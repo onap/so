558ac87b84f305face1c8cea424fff07c480c4ae
c00149107 <chenchuanyu@huawei.com>
2017-08-31 14:27:38 +0800

    Add VF-C Adapter
    Add VF-C Adapter to support VF-C APIs
    
    Change-Id: I4eb27755c8c5420343f457e0f9be37aa438361eb
    Issue-ID:SO-41
    Signed-off-by: c00149107 <chenchuanyu@huawei.com>

---
+++ adapters/mso-vfc-adapter/src/main/java/org/openecomp/mso/adapters/vfc/model/LocationConstraint.java
@@ -0,0 +1,72 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2017 Huawei Technologies Co., Ltd. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.openecomp.mso.adapters.vfc.model;
+
+import java.util.Map;
+
+/**
+ * <br>
+ * <p>
+ * </p>
+ * 
+ * @author
+ * @version GSO 0.5 2017/2/21
+ */
+public class LocationConstraint {
+
+    /**
+     * vnf profile id
+     */
+    private String vnfProfileId;
+
+    /**
+     * location constraints: vimId
+     */
+    private Map<String, String> locationConstraints;
+
+    /**
+     * @return Returns the vnfProfileId.
+     */
+    public String getVnfProfileId() {
+        return vnfProfileId;
+    }
+
+    /**
+     * @param vnfProfileId The vnfProfileId to set.
+     */
+    public void setVnfProfileId(String vnfProfileId) {
+        this.vnfProfileId = vnfProfileId;
+    }
+
+    /**
+     * @return Returns the locationConstraints.
+     */
+    public Map<String, String> getLocationConstraints() {
+        return locationConstraints;
+    }
+
+    /**
+     * @param locationConstraints The locationConstraints to set.
+     */
+    public void setLocationConstraints(Map<String, String> locationConstraints) {
+        this.locationConstraints = locationConstraints;
+    }
+
+}
---
+++ adapters/mso-vfc-adapter/src/main/java/org/openecomp/mso/adapters/vfc/util/JsonUtil.java
@@ -0,0 +1,129 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2017 Huawei Technologies Co., Ltd. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.openecomp.mso.adapters.vfc.util;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+import org.codehaus.jackson.type.TypeReference;
+import org.openecomp.mso.adapters.vfc.constant.HttpCode;
+import org.openecomp.mso.adapters.vfc.exceptions.ApplicationException;
+import org.openecomp.mso.logger.MessageEnum;
+import org.openecomp.mso.logger.MsoLogger;
+
+
+/**
+ * Interface for json analyzing.<br/>
+ * <p>
+ * </p>
+ * 
+ * @author
+ * @version GSO 0.5 2016/9/1
+ */
+public class JsonUtil {
+
+    /**
+     * Log service
+     */
+    private static final MsoLogger LOGGER = MsoLogger.getMsoLogger (MsoLogger.Catalog.RA);
+
+    /**
+     * Mapper.
+     */
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    static {
+        MAPPER.setDeserializationConfig(MAPPER.getDeserializationConfig()
+                .without(org.codehaus.jackson.map.DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES));
+        MAPPER.setSerializationInclusion(Inclusion.NON_NULL);
+    }
+
+    /**
+     * Constructor<br/>
+     * <p>
+     * </p>
+     * 
+     * @since GSO 0.5
+     */
+    private JsonUtil() {
+
+    }
+
+    /**
+     * Parse the string in form of json.<br/>
+     * 
+     * @param jsonstr json string.
+     * @param type that convert json string to
+     * @return model object
+     * @since GSO 0.5
+     */
+    public static <T> T unMarshal(String jsonstr, Class<T> type) {
+        try {
+            return MAPPER.readValue(jsonstr, type);
+        } catch(IOException e) {
+        	LOGGER.error(MessageEnum.RA_NS_EXC, "","", MsoLogger.ErrorCode.BusinessProcesssError, "fail to unMarshal json", e);
+            throw new ApplicationException(HttpCode.BAD_REQUEST, "fail to unMarshal json");
+        }
+    }
+
+    /**
+     * Parse the string in form of json.<br/>
+     * 
+     * @param jsonstr json string.
+     * @param type that convert json string to
+     * @return model object
+     * @since GSO 0.5
+     */
+    public static <T> T unMarshal(String jsonstr, TypeReference<T> type) {
+        try {
+            return MAPPER.readValue(jsonstr, type);
+        } catch(IOException e) {
+            LOGGER.error(MessageEnum.RA_NS_EXC, "","", MsoLogger.ErrorCode.BusinessProcesssError, "fail to unMarshal json", e);
+            throw new ApplicationException(HttpCode.BAD_REQUEST, "fail to unMarshal json");
+        }
+    }
+
+    /**
+     * Convert object to json string.<br/>
+     * 
+     * @param srcObj data object
+     * @return json string
+     * @since GSO 0.5
+     */
+    public static String marshal(Object srcObj) {
+        try {
+            return MAPPER.writeValueAsString(srcObj);
+        } catch(IOException e) {
+        	LOGGER.error(MessageEnum.RA_NS_EXC, "","", MsoLogger.ErrorCode.BusinessProcesssError, "fail to marshal json", e);
+            throw new ApplicationException(HttpCode.BAD_REQUEST, "srcObj marshal failed!");
+        }
+    }
+
+    /**
+     * Get mapper.<br/>
+     * 
+     * @return mapper
+     * @since GSO 0.5
+     */
+    public static ObjectMapper getMapper() {
+        return MAPPER;
+    }
+}
