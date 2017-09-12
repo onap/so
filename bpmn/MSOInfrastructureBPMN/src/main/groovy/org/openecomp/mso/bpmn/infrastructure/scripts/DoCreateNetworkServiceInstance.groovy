ad2a25bc95544769a58cbca9fdac3cb366cda479
jinxin1983 <j00101220@huawei.com>
2017-09-12 16:33:27 +0800

    Add the e2e service recipe
    Add the e2e service recipe BPMN file and groovy scripts
    
    Issue-ID: SO-46
    
    Change-Id: I5494823c3f93593a0145b330d38bfa50e975c306
    Signed-off-by: jinxin1983 <j00101220@huawei.com>

---
+++ bpmn/MSOInfrastructureBPMN/src/main/groovy/org/openecomp/mso/bpmn/infrastructure/scripts/DoCreateNetworkServiceInstance.groovy
@@ -0,0 +1,86 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * OPENECOMP - MSO
+ * ================================================================================
+ * Copyright (C) 2017 ONAP Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.openecomp.mso.bpmn.infrastructure.scripts;
+
+import static org.apache.commons.lang3.StringUtils.*;
+import groovy.xml.XmlUtil
+import groovy.json.*
+
+import org.openecomp.mso.bpmn.core.domain.ServiceDecomposition
+import org.openecomp.mso.bpmn.core.domain.ServiceInstance
+import org.openecomp.mso.bpmn.core.domain.ModelInfo
+import org.openecomp.mso.bpmn.core.json.JsonUtils
+import org.openecomp.mso.bpmn.common.scripts.AaiUtil
+import org.openecomp.mso.bpmn.common.scripts.AbstractServiceTaskProcessor
+import org.openecomp.mso.bpmn.common.scripts.ExceptionUtil
+import org.openecomp.mso.bpmn.common.scripts.SDNCAdapterUtils
+import org.openecomp.mso.bpmn.core.RollbackData
+import org.openecomp.mso.bpmn.core.WorkflowException
+import org.openecomp.mso.rest.APIResponse;
+import org.openecomp.mso.rest.RESTClient
+import org.openecomp.mso.rest.RESTConfig
+
+import java.util.UUID;
+
+import org.camunda.bpm.engine.delegate.BpmnError
+import org.camunda.bpm.engine.runtime.Execution
+import org.json.JSONObject;
+import org.apache.commons.lang3.*
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.web.util.UriUtils;
+
+/**
+ * This groovy class supports the <class>DoCreateNetworkServiceInstance.bpmn</class> process.
+ *
+ */
+public class DoCreateNetworkServiceInstance extends AbstractServiceTaskProcessor {
+
+	String Prefix="DCRESI_"
+	ExceptionUtil exceptionUtil = new ExceptionUtil()
+	JsonUtils jsonUtil = new JsonUtils()
+
+	public void prepareCreateNSRequest(Execution execution) {
+		
+	}
+	public void createNS(Execution execution) {
+		
+	}
+	public void instantiateNS(Execution execution) {
+		
+	}
+	public void queryOperationStatus(Execution execution) {
+		
+	}
+	public void preparePutServiceInsToAAI (Execution execution) {
+		
+	}
+	public void putServiceInsToAAI(Execution execution) {
+		
+	}
+	public void postPutServiceInsToAAI(Execution execution) {
+		
+	}
+	public void updateRequestDB(Execution execution) {
+		
+	}
+	
+	
+
+}
