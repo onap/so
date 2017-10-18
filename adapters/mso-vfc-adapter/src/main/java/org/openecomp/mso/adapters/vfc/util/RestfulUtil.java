bc1a1f7bad1fac18be6f790dd16ca49ce430d697
c00149107 <chenchuanyu@huawei.com>
2017-10-18 11:48:21 +0800

    Deal With Error Response from Vfc
    Deal With Error Response from Vfc
    
    Change-Id: Id5ce999d561099581b49524969c901ac6dac3f90
    Issue-ID:SO-233
    Signed-off-by: c00149107 <chenchuanyu@huawei.com>

--- adapters/mso-vfc-adapter/src/main/java/org/openecomp/mso/adapters/vfc/util/RestfulUtil.java
+++ adapters/mso-vfc-adapter/src/main/java/org/openecomp/mso/adapters/vfc/util/RestfulUtil.java
@@ -20,14 +20,8 @@
  */
 package org.openecomp.mso.adapters.vfc.util;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.SocketTimeoutException;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -143,21 +137,21 @@ public class RestfulUtil {
 
       method = null;
 
-      LOGGER.info(MessageEnum.RA_RESPONSE_FROM_SDNC, responseContent, "SDNC", "");
+      LOGGER.info(MessageEnum.RA_RESPONSE_FROM_SDNC, responseContent, "VFC", "");
       return createResponse(statusCode, responseContent);
 
     } catch (SocketTimeoutException e) {
-      String errMsg = "Request to SDNC timed out";
+      String errMsg = "Request to VFC timed out";
       logError(errMsg, e);
       return createResponse(HttpURLConnection.HTTP_CLIENT_TIMEOUT, errMsg);
 
     } catch (ConnectTimeoutException e) {
-      String errMsg = "Request to SDNC timed out";
+      String errMsg = "Request to VFC timed out";
       logError(errMsg, e);
       return createResponse(HttpURLConnection.HTTP_CLIENT_TIMEOUT, errMsg);
 
     } catch (Exception e) {
-      String errMsg = "Error processing request to SDNC";
+      String errMsg = "Error processing request to VFC";
       logError(errMsg, e);
       return createResponse(HttpURLConnection.HTTP_INTERNAL_ERROR, errMsg);
 
@@ -202,35 +196,35 @@ public class RestfulUtil {
    * @param request
    * @return
    */
-  public static String getRequestBody(HttpServletRequest request) {
-    String body = null;
-    StringBuilder stringBuilder = new StringBuilder();
-    BufferedReader bufferedReader = null;
-    try {
-      InputStream inputStream = request.getInputStream();
-      if (inputStream != null) {
-        bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        char[] charBuffer = new char[128];
-        int bytesRead = -1;
-        while ((bytesRead = bufferedReader.read(charBuffer)) > 0)
-          stringBuilder.append(charBuffer, 0, bytesRead);
-      }
-    } catch (IOException ex) {
-      LOGGER.error(MessageEnum.RA_NS_EXC, "VFC", "", MsoLogger.ErrorCode.AvailabilityError,
-          "read inputStream buffer catch exception:", ex);
-    } finally {
-      if (bufferedReader != null) {
-        try {
-          bufferedReader.close();
-        } catch (IOException ex) {
-          LOGGER.error(MessageEnum.RA_NS_EXC, "VFC", "", MsoLogger.ErrorCode.AvailabilityError,
-              "close buffer catch exception:", ex);
-        }
-      }
-    }
-
-    body = stringBuilder.toString();
-    return body;
-  }
+//  public static String getRequestBody(HttpServletRequest request) {
+//    String body = null;
+//    StringBuilder stringBuilder = new StringBuilder();
+//    BufferedReader bufferedReader = null;
+//    try {
+//      InputStream inputStream = request.getInputStream();
+//      if (inputStream != null) {
+//        bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+//        char[] charBuffer = new char[128];
+//        int bytesRead = -1;
+//        while ((bytesRead = bufferedReader.read(charBuffer)) > 0)
+//          stringBuilder.append(charBuffer, 0, bytesRead);
+//      }
+//    } catch (IOException ex) {
+//      LOGGER.error(MessageEnum.RA_NS_EXC, "VFC", "", MsoLogger.ErrorCode.AvailabilityError,
+//          "read inputStream buffer catch exception:", ex);
+//    } finally {
+//      if (bufferedReader != null) {
+//        try {
+//          bufferedReader.close();
+//        } catch (IOException ex) {
+//          LOGGER.error(MessageEnum.RA_NS_EXC, "VFC", "", MsoLogger.ErrorCode.AvailabilityError,
+//              "close buffer catch exception:", ex);
+//        }
+//      }
+//    }
+//
+//    body = stringBuilder.toString();
+//    return body;
+//  }
 
 }
