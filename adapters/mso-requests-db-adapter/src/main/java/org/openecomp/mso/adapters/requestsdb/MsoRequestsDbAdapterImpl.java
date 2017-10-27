a75282a875b6f1aa394733a3e65c0debdd2f01fc
c00149107 <chenchuanyu@huawei.com>
2017-10-27 14:53:10 +0800

    Add resource operation status oper
    Add resource operation status oper in requests db adapter
    
    Change-Id: I0c6a21178147c6c9cac9d1492cd1503e8f658157
    Issue-ID:SO-276
    Signed-off-by: c00149107 <chenchuanyu@huawei.com>

--- adapters/mso-requests-db-adapter/src/main/java/org/openecomp/mso/adapters/requestsdb/MsoRequestsDbAdapterImpl.java
+++ adapters/mso-requests-db-adapter/src/main/java/org/openecomp/mso/adapters/requestsdb/MsoRequestsDbAdapterImpl.java
@@ -285,6 +285,7 @@ public class MsoRequestsDbAdapterImpl implements MsoRequestsDbAdapter {
     public void updateServiceOperationStatus(String serviceId, String operationId, String serviceName,String operationType, String userId,
             String result, String operationContent, String progress, String reason) throws MsoRequestsDbException {
         OperationStatus operStatus = new OperationStatus();
+        operStatus.setResult(RequestsDbConstant.Status.PROCESSING);
         operStatus.setServiceId(serviceId);
         operStatus.setOperationId(operationId);
         operStatus.setServiceName(serviceName);
@@ -322,5 +323,56 @@ public class MsoRequestsDbAdapterImpl implements MsoRequestsDbAdapter {
             RequestsDatabase.getInstance().updateResOperStatus(resourceStatus);
         }     
     }
-
+    
+    /**
+     * get resource operation status
+     * <br>
+     * 
+     * @param serviceId
+     * @param operationId
+     * @param resourceUUID
+     * @return
+     * @throws MsoRequestsDbException
+     * @since   ONAP Amsterdam Release
+     */
+    @Override
+    public ResourceOperationStatus getResourceOperationStatus(String serviceId, String operationId, String resourceTemplateUUID)
+            throws MsoRequestsDbException {
+        return RequestsDatabase.getInstance().getResourceOperationStatus(serviceId, operationId, resourceTemplateUUID);
+    }
+    
+    /**
+     * update resource operation status
+     * <br>
+     * 
+     * @param serviceId
+     * @param operationId
+     * @param resourceUUID
+     * @param operationType
+     * @param resourceInstanceID
+     * @param jobId
+     * @param status
+     * @param progress
+     * @param errorCode
+     * @param statusDescription
+     * @throws MsoRequestsDbException
+     * @since   ONAP Amsterdam Release
+     */
+    @Override
+    public void updateResourceOperationStatus(String serviceId, String operationId, String resourceTemplateUUID,
+            String operType, String resourceInstanceID, String jobId, String status, String progress,
+            String errorCode, String statusDescription) throws MsoRequestsDbException {
+         ResourceOperationStatus resStatus = new ResourceOperationStatus();
+         resStatus.setServiceId(serviceId);
+         resStatus.setOperationId(operationId);
+         resStatus.setResourceTemplateUUID(resourceTemplateUUID);
+         resStatus.setOperType(operType);
+         resStatus.setResourceInstanceID(resourceInstanceID);
+         resStatus.setJobId(jobId);
+         resStatus.setStatus(status);
+         resStatus.setProgress(progress);
+         resStatus.setErrorCode(errorCode);
+         resStatus.setStatusDescription(statusDescription);
+         RequestsDatabase.getInstance().updateResOperStatus(resStatus);
+    }
 }
