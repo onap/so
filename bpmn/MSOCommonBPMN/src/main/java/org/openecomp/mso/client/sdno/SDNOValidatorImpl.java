ab95ee3b9fb4e1fcfe545e43c36c9b048cb60b22
Benjamin, Max (mb388a) <mb388a@us.att.com>
2017-11-06 15:01:36 -0500

    modified sample SDNO code for SO
    Issue-Id: SO-309
    Change-Id: I998d5573d60d99987bb99133967ca012ef8dba6f
    Signed-off-by: Benjamin, Max (mb388a) <mb388a@us.att.com>

--- bpmn/MSOCommonBPMN/src/main/java/org/openecomp/mso/client/sdno/SDNOValidatorImpl.java
+++ bpmn/MSOCommonBPMN/src/main/java/org/openecomp/mso/client/sdno/SDNOValidatorImpl.java
@@ -30,7 +30,7 @@ import org.openecomp.mso.client.dmaap.DmaapPublisher;
 import org.openecomp.mso.client.exceptions.SDNOException;
 import org.openecomp.mso.jsonpath.JsonPathUtil;
 
-public class SDNOValidatorImpl implements SDNOValidator, Consumer {
+public class SDNOValidatorImpl implements SDNOValidator {
 
 	private final static String aafUserName = "something";
 	private final static String clientName = "MSO";
@@ -51,103 +51,4 @@ public class SDNOValidatorImpl implements SDNOValidator, Consumer {
 		//block and continue to poll waiting for response
 	}
 
-	protected SDNO buildRequestDiagnostic(String vnfName, String uuid, String oamIp) {
-		
-		Input input = new Input();
-		SDNO parentRequest = new SDNO();
-		Body body = new Body();
-		parentRequest.setBody(body);
-		parentRequest.setNodeType("vPE");
-		parentRequest.setOperation("health-diagnostic");
-		
-		body.setInput(input);
-		
-		RequestHealthDiagnostic request = new RequestHealthDiagnostic();
-		request.setRequestClientName(clientName);
-		request.setRequestNodeName(vnfName);
-		request.setRequestNodeIp(oamIp); //generic-vnf oam ip
-		request.setRequestUserId(aafUserName); //mech id?
-		request.setRequestId(uuid); //something to identify this request by for polling
-		
-		input.setRequestHealthDiagnostic(request);
-		
-		return parentRequest;
-	}
-	protected void submitRequest(String json) throws FileNotFoundException, IOException, InterruptedException {
-		DmaapPublisher publisher = new DmaapPublisher(this.producerFilePath);
-		publisher.send(json);
-	}
-	protected boolean pollForResponse(DmaapConsumer consumer, String uuid) throws Exception {
-		this.uuid = uuid;
-		return consumer.consume(this);
-	}
-	
-	@Override
-	public boolean continuePolling() {
-		return continuePolling;
-	}
-	
-	@Override
-	public void stopProcessingMessages() {
-		continuePolling = false;
-	}
-	@Override
-	public void processMessage(String message) throws Exception {
-		if (isHealthDiagnostic(message, this.getRequestId())) {
-			if (!healthDiagnosticSuccessful(message)) {
-				Optional<String> statusMessage = this.getStatusMessage(message);
-				if (statusMessage.isPresent()) {
-					throw new SDNOException(statusMessage.get());
-				} else {
-					throw new SDNOException();
-				}
-			} else {
-				stopProcessingMessages();
-			}
-		}
-	}
-	
-	@Override
-	public boolean isAccepted(String message) {
-		if (isResultInfo(message)) {
-			Optional<String> code = isAccepted(message, this.getRequestId());
-			if (code.isPresent()) {
-				if ("202".equals(code.get())) {
-					return true;
-				} else {
-					//TODO check other statuses 400 and 500
-				}
-			} else {
-				//TODO throw error
-			}
-		}
-		
-		return false;
-	}
-	
-	@Override
-	public String getRequestId() {
-		return uuid;
-	}
-	
-	protected Optional<String> isAccepted(String json, String uuid) {
-		return JsonPathUtil.getInstance().locateResult(json, String.format("$.result-info[?(@.status=='ACCEPTED' && @.request-id=='%s')].code", uuid));
-	}
-	
-	protected boolean isResultInfo(String json) {
-		return JsonPathUtil.getInstance().pathExists(json, "$[?(@.result-info)]");
-	}
-	
-	protected boolean isHealthDiagnostic(String json, String uuid) {
-		return JsonPathUtil.getInstance().pathExists(json, String.format("$[?(@.result-info.request-id=='%s')].%s", uuid, healthDiagnosticPath));
-	}
-	
-	protected boolean healthDiagnosticSuccessful(String json) {
-		return JsonPathUtil.getInstance().pathExists(json, "$." + healthDiagnosticPath + "[?(@.response-status=='Success')]");
-	}
-	
-	protected Optional<String> getStatusMessage(String json) {
-		return JsonPathUtil.getInstance().locateResult(json, "$." + healthDiagnosticPath + ".response-details-json");
-	}
-	
 }
