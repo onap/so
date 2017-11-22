89ac95e4d148bf2b95a428893fbd3c448908d87e
c00149107 <chenchuanyu@huawei.com>
2017-11-22 09:11:53 +0800

    Fix Json Analyze failed issue
    Fix Json Analyze failed issue
    
    Issue-ID: SO-348
    Change-Id: Iffddccb2d8c15d954920dd1f6529e2e2b7656b87
    Signed-off-by: c00149107 <chenchuanyu@huawei.com>

--- bpmn/MSOCoreBPMN/src/main/java/org/openecomp/mso/bpmn/core/json/JsonUtils.java
+++ bpmn/MSOCoreBPMN/src/main/java/org/openecomp/mso/bpmn/core/json/JsonUtils.java
@@ -928,14 +928,14 @@ public class JsonUtils {
 		JSONObject obj = new JSONObject(stringListJson);
 		JSONArray arr = obj.getJSONArray("strings");
 		for (int i = 0; i < arr.length(); i++){
-			String s = arr.getString(i);
+		    String s = arr.get(i).toString();
 			list.add(s);
 		}
 		msoLogger.debug("Outgoing List is: " + list);
 		msoLogger.debug("Completed String Array To List Util Method");
 		return list;
 	}
-
+	
 	/**
 	 *
 	 * Invokes the getJsonRawValue() method to determine if the
