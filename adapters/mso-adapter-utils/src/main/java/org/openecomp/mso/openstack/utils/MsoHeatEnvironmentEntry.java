38ca2db35850385bc124a61917df5112e7aac523
Lukasz Muszkieta <lukasz.muszkieta@nokia.com>
2018-02-02 18:06:01 +0100

    add junit - MsoHeatEnvironmentEntry
    Change-Id: Ibc2b9562de8fbfdb60ee09add404bd3923431501
    Issue-ID: SO-360
    Signed-off-by: Lukasz Muszkieta <lukasz.muszkieta@nokia.com>

--- adapters/mso-adapter-utils/src/main/java/org/openecomp/mso/openstack/utils/MsoHeatEnvironmentEntry.java
+++ adapters/mso-adapter-utils/src/main/java/org/openecomp/mso/openstack/utils/MsoHeatEnvironmentEntry.java
@@ -21,54 +21,37 @@
 
 package org.openecomp.mso.openstack.utils;
 
-
-
-import java.util.HashSet;
-import java.util.ArrayList;
+import java.util.Optional;
 import java.util.Set;
-import org.openecomp.mso.db.catalog.beans.HeatTemplateParam;
 import org.openecomp.mso.logger.MsoLogger;
 
 public class MsoHeatEnvironmentEntry {
 
     private static final MsoLogger LOGGER = MsoLogger.getMsoLogger (MsoLogger.Catalog.RA);
-    
-	private Set<MsoHeatEnvironmentParameter> parameters = null;
-	private Set<MsoHeatEnvironmentResource> resources = null;
-	private StringBuilder rawEntry = null;
+    private Set<MsoHeatEnvironmentParameter> parameters;
+	private StringBuilder rawEntry;
 	private boolean valid = true;
-	private String errorString = null;
-	private StringBuilder resourceRegistryEntryRaw = null;
-	
-	public MsoHeatEnvironmentEntry() {
-		super();
-	}
-	
+	private String errorString;
+	private StringBuilder resourceRegistryEntryRaw;
+
 	public MsoHeatEnvironmentEntry(StringBuilder sb) {
-		this();
-		this.rawEntry = sb;
-		this.processRawEntry();
+		rawEntry = sb;
+		processRawEntry();
 	}
 	
 	private void processRawEntry() {
 		try {
-			if (this.rawEntry == null || "".equals(this.rawEntry))
+			if (rawEntry == null || rawEntry.toString().isEmpty()) {
 				return;
-			byte[] b = this.rawEntry.toString().getBytes();
-			MsoYamlEditorWithEnvt yaml = new MsoYamlEditorWithEnvt(b);
-			this.parameters = yaml.getParameterListFromEnvt();
-			//this.resources = yaml.getResourceListFromEnvt();
-			StringBuilder sb = this.getResourceRegistryRawEntry();
-			if (sb == null) {
-				this.resourceRegistryEntryRaw = new StringBuilder("");
-			} else {
-				this.resourceRegistryEntryRaw = sb;
 			}
+			byte[] b = this.rawEntry.toString().getBytes();
+			parameters = new MsoYamlEditorWithEnvt(b).getParameterListFromEnvt();
+			resourceRegistryEntryRaw = Optional.ofNullable(getResourceRegistryRawEntry())
+					.orElse(new StringBuilder(""));
 		} catch (Exception e) {
 		    LOGGER.debug("Exception:", e);
 			this.valid = false;
 			this.errorString = e.getMessage();
-			//e.printStackTrace();
 		}
 	}
 	
@@ -78,55 +61,8 @@ public class MsoHeatEnvironmentEntry {
 	public String getErrorString() {
 		return this.errorString;
 	}
-	
-	public Set<MsoHeatEnvironmentParameter> getParameters() {
-		return this.parameters;
-	}
-	public Set<MsoHeatEnvironmentResource> getResources() {
-		return this.resources;
-	}
-	public void setParameters(Set<MsoHeatEnvironmentParameter> paramSet) {
-		if (paramSet == null) {
-			this.parameters = null;
-		} else {
-			this.parameters = paramSet;
-		}
-	}
-	public void setResources(Set<MsoHeatEnvironmentResource> resourceSet) {
-		if (resourceSet == null) {
-			this.resources = null;
-		} else {
-			this.resources = resourceSet;
-		}
-	}
-	
-	public void addParameter(MsoHeatEnvironmentParameter hep) {
-		if (this.parameters == null) {
-			this.parameters = new HashSet<>();
-		}
-		this.parameters.add(hep);
-	}
-	public void addResource(MsoHeatEnvironmentResource her) {
-		if (this.resources == null) {
-			this.resources = new HashSet<>();
-		}
-		this.resources.add(her);
-	}
-	
-	public int getNumberOfParameters() {
-		return this.parameters.size();
-	}
-	public int getNumberOfResources() {
-		return this.resources.size();
-	}
-	
-	public boolean hasResources() {
-		if (this.resources != null && this.resources.size() > 0) {
-			return true;
-		} 
-		return false;
-	}
-	public boolean hasParameters() {
+
+	private boolean hasParameters() {
 		if (this.parameters != null && this.parameters.size() > 0) {
 			return true;
 		}
@@ -143,52 +79,7 @@ public class MsoHeatEnvironmentEntry {
 		}
 		return contains;
 	}
-	
-	public boolean containsParameter(String paramName, String paramAlias) {
-		if (this.containsParameter(paramName)) {
-			return true;
-		}
-		if (this.containsParameter(paramAlias)) {
-			return true;
-		}
-		return false;
-	}
-	
-	public StringBuilder toFullStringExcludeNonParams(Set<HeatTemplateParam> params) {
-		// Basically give back the envt - but exclude the params that aren't in the HeatTemplate 
-		
-		StringBuilder sb = new StringBuilder();
-		ArrayList<String> paramNameList = new ArrayList<String>(params.size());
-		for (HeatTemplateParam htp : params) {
-			paramNameList.add(htp.getParamName());
-		}
-		
-		if (this.hasParameters()) {
-			sb.append("parameters:\n");
-			for (MsoHeatEnvironmentParameter hep : this.parameters) {
-				String paramName = hep.getName();
-				if (paramNameList.contains(paramName)) {
-					// This parameter *is* in the Heat Template - so include it:
-					sb.append("  " + hep.getName() + ": " + hep.getValue() + "\n");
-					// New - 1607 - if any of the params mapped badly - JUST RETURN THE ORIGINAL ENVT!
-					if (hep.getValue().startsWith("_BAD")) {
-						return this.rawEntry;
-					}
-				} 
-			}
-			sb.append("\n");
-		}
-//		if (this.hasResources()) {
-//			sb.append("resource_registry:\n");
-//			for (MsoHeatEnvironmentResource her : this.resources) {
-//				sb.append("   \"" + her.getName() + "\": " + her.getValue() + "\n");
-//			}
-//		}
-		sb.append("\n");
-		sb.append(this.resourceRegistryEntryRaw);				
-		return sb;
-	}
-	
+
 	public StringBuilder toFullString() {
 		StringBuilder sb = new StringBuilder();
 		
@@ -199,14 +90,8 @@ public class MsoHeatEnvironmentEntry {
 			}
 			sb.append("\n");
 		}
-//		if (this.hasResources()) {
-//			sb.append("resource_registry:\n");
-//			for (MsoHeatEnvironmentResource her : this.resources) {
-//				sb.append("   \"" + her.getName() + "\": " + her.getValue() + "\n");
-//			}
-//		}
 		sb.append("\n");
-		sb.append(this.resourceRegistryEntryRaw);			
+		sb.append(this.resourceRegistryEntryRaw);
 		return sb;
 	}
 
@@ -215,11 +100,6 @@ public class MsoHeatEnvironmentEntry {
 	}
 	
 	private StringBuilder getResourceRegistryRawEntry() {
-		
-		if (this.rawEntry == null) {
-			return null;
-		}
-		
 		StringBuilder sb = new StringBuilder();
 		int indexOf = this.rawEntry.indexOf("resource_registry:");
 		if (indexOf < 0) { // no resource_registry:
@@ -228,5 +108,5 @@ public class MsoHeatEnvironmentEntry {
 		sb.append(this.rawEntry.substring(indexOf));
 		return sb;
 	}
-	
+
 }
