affcd9b499999f6456d27c4afae0b5fb02223c06
biniek <lukasz.biniek@nokia.com>
2018-02-15 11:44:11 +0100

    Added unittests
    Added unittests for ResponseExecptionMapperImpl and refactored one test for CompleteMsoProcessTest
    
    Issue-ID: SO-374
    Signed-off-by: biniek <lukasz.biniek@nokia.com>
    
    Change-Id: I359daf14d2a913829721fff3f2d340c6e893aa13
    Signed-off-by: biniek <lukasz.biniek@nokia.com>

---
+++ bpmn/MSOCommonBPMN/src/test/java/org/openecomp/mso/client/ResponseExceptionMapperImplTest.java
@@ -0,0 +1,111 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.mso.client;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.google.common.base.Charsets;
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.NotAcceptableException;
+import javax.ws.rs.NotAllowedException;
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.NotSupportedException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.ClientResponseContext;
+import javax.ws.rs.core.Response.Status;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitParamsRunner.class)
+public class ResponseExceptionMapperImplTest {
+
+    private static final ResponseExceptionMapperImpl mapper = new ResponseExceptionMapperImpl();
+
+    public static Object[][] statusesAndCorrespondingExceptions() {
+        return new Object[][]{
+                {Status.BAD_REQUEST, BadRequestException.class},
+                {Status.UNAUTHORIZED, NotAuthorizedException.class},
+                {Status.FORBIDDEN, ForbiddenException.class},
+                {Status.NOT_FOUND, NotFoundException.class},
+                {Status.METHOD_NOT_ALLOWED, NotAllowedException.class},
+                {Status.NOT_ACCEPTABLE, NotAcceptableException.class},
+                {Status.PRECONDITION_FAILED, PreconditionFailedException.class},
+                {Status.UNSUPPORTED_MEDIA_TYPE, NotSupportedException.class},
+                {Status.INTERNAL_SERVER_ERROR, InternalServerErrorException.class},
+                {Status.SERVICE_UNAVAILABLE, WebApplicationException.class},
+                {Status.BAD_GATEWAY, WebApplicationException.class},
+        };
+    }
+
+    @Test
+    @Parameters(method = "statusesAndCorrespondingExceptions")
+    public void shouldThrowExceptionWhenStatusIsNotOk(Status status, Class<Exception> expectedException) {
+        // given
+        ClientResponseContext responseContext = createMockResponseContext(status);
+        // when, then
+        assertThatThrownBy(() -> mapper.filter(null, responseContext)).isInstanceOf(expectedException);
+    }
+
+    @Test
+    public void shouldNotThrowExceptionWhenStatusIsOk() {
+        // given
+        ClientResponseContext responseContext = createMockResponseContext(Status.OK);
+        // when, then
+        assertThatCode(() -> mapper.filter(null, responseContext)).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void shouldThrowExceptionWithCustomMessageWhenResponseHasEntity() {
+        // given
+        ClientResponseContext responseContext = createMockResponseContext(Status.BAD_REQUEST);
+        when(responseContext.hasEntity()).thenReturn(true);
+        when(responseContext.getEntityStream()).thenReturn(IOUtils.toInputStream("test message", Charsets.UTF_8));
+        // when, then
+        assertThatThrownBy(() -> mapper.filter(null, responseContext)).isInstanceOf(BadRequestException.class)
+                .hasMessage("test message");
+    }
+
+    @Test
+    public void shouldThrowExceptionWithDefaultMessageWhenResponseHasNoEntity() {
+        // given
+        ClientResponseContext responseContext = createMockResponseContext(Status.BAD_REQUEST);
+        when(responseContext.hasEntity()).thenReturn(false);
+        // when, then
+        assertThatThrownBy(() -> mapper.filter(null, responseContext)).isInstanceOf(BadRequestException.class)
+                .hasMessage("empty message");
+    }
+
+    private static ClientResponseContext createMockResponseContext(Status status) {
+        ClientResponseContext responseContext = mock(ClientResponseContext.class);
+        when(responseContext.getStatusInfo()).thenReturn(status);
+        when(responseContext.getStatus()).thenReturn(status.getStatusCode());
+        return responseContext;
+    }
+}
