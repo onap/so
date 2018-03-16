f892a61367632c85fa8064d14b2f031e2cca9f51
AS00465059 <AS00465059@techmahindra.com>
2018-03-16 16:30:27 +0530

    Adding Junit
    Junit for:
    1.MsoRequestsDbExceptionBean.java
    2.MsoRequestsDbException.java
    3.UpdateInfraRequest.java
    
    Sonar Link:
    https://sonar.onap.org/code?id=org.onap.so%3Aso&selected=org.onap.so%3AMSOCommonBPMN%3Asrc%2Fmain%2Fjava%2Forg%2Fopenecomp%2Fmso%2Fclient%2Fadapter%2Frequests%2Fdb%2Fentities
    
    Change-Id: I7776846e4529bf5d9b6156606df6603f9b5ef79e
    Issue-ID: SO-492
    Signed-off-by: AS00465059 <AS00465059@techmahindra.com>

---
+++ bpmn/MSOCommonBPMN/src/test/java/org/openecomp/mso/client/adapter/requests/db/entities/MsoRequestsDbExceptionTest.java
@@ -0,0 +1,38 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : SO
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+
+package org.openecomp.mso.client.adapter.requests.db.entities;
+
+import org.junit.Test;
+
+public class MsoRequestsDbExceptionTest {
+
+	@Test
+	public void test() {
+		Throwable e = new Throwable();
+		MsoRequestsDbExceptionBean mredb=new MsoRequestsDbExceptionBean();
+		MsoRequestsDbException mrde=new MsoRequestsDbException("msg",e);
+		MsoRequestsDbException mrd=new MsoRequestsDbException("msg");
+		MsoRequestsDbException mrd1=new MsoRequestsDbException(e);
+		mrde.setFaultInfo(mredb);
+		assert(mrde.getFaultInfo().equals(mredb));
+	}
+
+}
---
+++ bpmn/MSOCommonBPMN/src/test/java/org/openecomp/mso/client/adapter/requests/db/entities/UpdateInfraRequestTest.java
@@ -0,0 +1,65 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : SO
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+
+package org.openecomp.mso.client.adapter.requests.db.entities;
+
+import org.junit.Test;
+
+public class UpdateInfraRequestTest {
+
+	@Test
+	public void test() {
+		UpdateInfraRequest uir=new UpdateInfraRequest();
+		RequestStatusType requestStatus=RequestStatusType.COMPLETE;
+		uir.setConfigurationId("configurationId");
+		uir.setConfigurationName("configurationName");
+		uir.setLastModifiedBy("lastModifiedBy");
+		uir.setNetworkId("networkId");
+		uir.setProgress("progress");
+		uir.setRequestId("requestId");
+		uir.setRequestStatus(requestStatus);
+		uir.setResponseBody("responseBody");
+		uir.setServiceInstanceId("serviceInstanceId");
+		uir.setServiceInstanceName("serviceInstanceName");
+		uir.setStatusMessage("statusMessage");
+		uir.setVfModuleId("vfModuleId");
+		uir.setVfModuleName("vfModuleName");
+		uir.setVnfId("vnfId");
+		uir.setVnfOutputs("vnfOutputs");
+		uir.setVolumeGroupId("volumeGroupId");
+		assert(uir.getConfigurationId().equals("configurationId"));
+		assert(uir.getConfigurationName().equals("configurationName"));
+		assert(uir.getLastModifiedBy().equals("lastModifiedBy"));
+		assert(uir.getNetworkId().equals("networkId"));
+		assert(uir.getProgress().equals("progress"));
+        assert(uir.getRequestId().equals("requestId"));
+        assert(uir.getRequestStatus().equals(requestStatus));
+        assert(uir.getResponseBody().equals("responseBody"));
+        assert(uir.getServiceInstanceId().equals("serviceInstanceId"));
+        assert(uir.getServiceInstanceName().equals("serviceInstanceName"));
+        assert(uir.getStatusMessage().equals("statusMessage"));
+        assert(uir.getVfModuleId().equals("vfModuleId"));
+        assert(uir.getVnfOutputs().equals("vnfOutputs"));
+        assert(uir.getVolumeGroupId().equals("volumeGroupId"));
+        assert(uir.getVfModuleName().equals("vfModuleName"));
+        assert(uir.getVnfId().equals("vnfId"));
+	}
+
+}
