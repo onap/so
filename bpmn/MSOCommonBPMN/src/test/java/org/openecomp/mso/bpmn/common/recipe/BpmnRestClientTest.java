54500b8f98f9fe7308e85c4d62b1069cf7f17870
AS00465059 <AS00465059@techmahindra.com>
2018-03-20 12:35:19 +0530

    Adding Junit
    Adding Junit for:
    1.BpmnParam.java
    2.BpmnRestClient.java
    3.ResourceInput.java
    4.ResourceRecipeRequest.java
    
    Sonar link:
    https://sonar.onap.org/code?id=org.onap.so%3Aso&selected=org.onap.so%3AMSOCommonBPMN%3Asrc%2Fmain%2Fjava%2Forg%2Fopenecomp%2Fmso%2Fbpmn%2Fcommon%2Frecipe
    
    
    Change-Id: Icb2c545cb62a49fdd84ce998aa43792436aeaf41
    Issue-ID: SO-491
    Signed-off-by: AS00465059 <AS00465059@techmahindra.com>

---
+++ bpmn/MSOCommonBPMN/src/test/java/org/openecomp/mso/bpmn/common/recipe/BpmnRestClientTest.java
@@ -0,0 +1,33 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : SO
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.openecomp.mso.bpmn.common.recipe;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class BpmnRestClientTest {
+	
+	@Test
+	public void test() {
+		BpmnRestClient.getEncryptedPropValue("prop", "123", "456");
+		BpmnRestClient.loadMsoProperties();
+		assertEquals(true,BpmnRestClient.getNoPropertiesState());
+	}
+}
