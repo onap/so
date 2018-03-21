fda871b58091fbd962d52ecac3b2aae6f3556274
Sumapriya <SS00493505@techmahindra.com>
2018-03-21 14:29:56 +0530

    Adding Junit
    Adding Junit for:
    1.AllotedResource.java
    2.ConfigResource.java
    3.Configuration.java
    4.Customer.java
    5.HomingSolution.java
    6.License.java
    7.ModelInfo.java
    8.ModuleResource.java
    9.NetworkResource.java
    10.OwningEntity.java
    11.Project.java
    12.Request.java
    13.ResourceDecomposition.java
    14.Resource.java
    15.ServiceDecomposition.java
    16.ServiceInstance.java
    17.Subscriber.java
    18.TunnelConnect.java
    19.VnfResource.java
    
    Sonar Link:
    https://sonar.onap.org/code?id=org.onap.so%3Aso&selected=org.onap.so%3AMSOCoreBPMN%3Asrc%2Fmain%2Fjava%2Forg%2Fopenecomp%2Fmso%2Fbpmn%2Fcore%2Fdomain
    
    Change-Id: I94c96ef50a0549ab20ea482db0c17e94c106b5d0
    Issue-ID: SO-509
    Signed-off-by: Sumapriya <SS00493505@techmahindra.com>

---
+++ bpmn/MSOCoreBPMN/src/test/java/org/openecomp/mso/bpmn/core/domain/ModuleResourceTest.java
@@ -0,0 +1,48 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : SO
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.openecomp.mso.bpmn.core.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ModuleResourceTest {
+	private ModuleResource moduleresource = new ModuleResource();
+
+	@Test
+	public void testModuleResource() {
+		
+		moduleresource.setVfModuleName("vfModuleName");
+		moduleresource.setHeatStackId("heatStackId");
+		moduleresource.setIsBase(true);
+		moduleresource.setVfModuleLabel("vfModuleLabel");
+		moduleresource.setInitialCount(0);
+		moduleresource.setVfModuleType("vfModuleType");
+		moduleresource.setHasVolumeGroup(true);
+		assertEquals(moduleresource.getVfModuleName(), "vfModuleName");
+		assertEquals(moduleresource.getHeatStackId(), "heatStackId");
+		assertEquals(moduleresource.getIsBase(), true);
+		assertEquals(moduleresource.getVfModuleLabel(), "vfModuleLabel");
+		assertEquals(moduleresource.getInitialCount(), 0);
+		assertEquals(moduleresource.getVfModuleType(), "vfModuleType");
+		assertEquals(moduleresource.isHasVolumeGroup(), true);
+			}
+
+}
---
+++ bpmn/MSOCoreBPMN/src/test/java/org/openecomp/mso/bpmn/core/domain/ResourceDecompositionTest.java
@@ -0,0 +1,50 @@
+/*
+* ============LICENSE_START=======================================================
+* ONAP : SO
+* ================================================================================
+* Copyright 2018 TechMahindra
+*=================================================================================
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* ============LICENSE_END=========================================================
+*/
+package org.openecomp.mso.bpmn.core.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ResourceDecompositionTest {
+	
+	private ResourceDecomposition rd = new ResourceDecomposition() {
+		private static final long serialVersionUID = 1L;
+	};
+	ModelInfo model = new ModelInfo();
+	ResourceInstance ri = new ResourceInstance();
+	
+	
+
+	@Test
+	public void testResourceDecomposition() {
+		rd.setModelInfo(model);
+		rd.setInstanceData(ri);
+		rd.setResourceType("resourceType");
+		rd.setResourceInstanceId("newInstanceId");
+		rd.setResourceInstanceName("newInstanceName");
+		assertEquals(rd.getResourceModel(), model);
+		assertEquals(rd.getModelInfo(), model);
+		assertEquals(rd.getInstanceData(), ri);
+		assertEquals(rd.getResourceInstanceId(), "newInstanceId");
+		assertEquals(rd.getResourceInstanceName(), "newInstanceName");
+	}
+
+}
