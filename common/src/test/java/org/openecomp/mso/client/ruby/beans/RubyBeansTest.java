bafee3947938674ea1099780fd1b1764d6ad91cb
Benjamin, Max (mb388a) <mb388a@us.att.com>
2018-03-22 09:41:02 -0400

    Added openpojo tests for ruby beans
    Change-Id: Id30228ee7b02d9200ca3c02b73e9b0cb9541df46
    Issue-ID: SO-515
    Signed-off-by: Benjamin, Max (mb388a) <mb388a@us.att.com>

---
+++ common/src/test/java/org/openecomp/mso/client/ruby/beans/RubyBeansTest.java
@@ -0,0 +1,39 @@
+package org.openecomp.mso.client.ruby.beans;
+
+import org.junit.Test;
+
+import com.openpojo.reflection.PojoClass;
+import com.openpojo.reflection.PojoClassFilter;
+import com.openpojo.reflection.filters.FilterEnum;
+import com.openpojo.reflection.filters.FilterPackageInfo;
+import com.openpojo.validation.Validator;
+import com.openpojo.validation.ValidatorBuilder;
+import com.openpojo.validation.rule.impl.GetterMustExistRule;
+import com.openpojo.validation.rule.impl.SetterMustExistRule;
+import com.openpojo.validation.test.impl.GetterTester;
+import com.openpojo.validation.test.impl.SetterTester;
+
+public class RubyBeansTest {
+
+	private PojoClassFilter filterTestClasses = new FilterTestClasses();
+
+	@Test
+	public void pojoStructure() {
+		test("org.openecomp.mso.client.ruby.beans");
+	}
+
+	private void test(String pojoPackage) {
+		Validator validator = ValidatorBuilder.create()
+				.with(new GetterMustExistRule())
+				.with(new SetterMustExistRule())
+				.with(new SetterTester())
+				.with(new GetterTester())
+				.build();
+		validator.validate(pojoPackage, new FilterPackageInfo(), new FilterEnum(), filterTestClasses);
+	}
+	private static class FilterTestClasses implements PojoClassFilter {
+		public boolean include(PojoClass pojoClass) {
+			return !pojoClass.getSourcePath().contains("/test-classes/");
+		}
+	}
+}
