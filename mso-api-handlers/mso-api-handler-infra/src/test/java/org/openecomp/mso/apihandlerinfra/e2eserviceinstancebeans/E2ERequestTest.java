97b6536a1e0111585b125369691b91d2e8d59665
Sindhu A <arcot.sindhuri@huawei.com>
2018-03-22 05:09:02 +0000

    UT Coverage
    UT Coverage for SO
    
    Issue-ID: SO-369
    Change-Id: I1708ed331d07386ce85bf1f448085acdc2915fea
    Signed-off-by: Sindhu A <arcot.sindhuri@huawei.com>

---
+++ mso-api-handlers/mso-api-handler-infra/src/test/java/org/openecomp/mso/apihandlerinfra/e2eserviceinstancebeans/E2ERequestTest.java
@@ -0,0 +1,54 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2018 Huawei Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.mso.apihandlerinfra.e2eserviceinstancebeans;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class E2ERequestTest {
+
+    E2ERequest test = new E2ERequest();
+
+    @Test
+    public void verifyE2ERequest() throws Exception {
+
+        test.setOperationId("operationId");
+        assertEquals(test.getOperationId(),"operationId");
+        test.setOperation("operation");
+        assertEquals(test.getOperation(),"operation");
+        test.setResult("result");
+        assertEquals(test.getResult(),"result");
+        test.setReason("test");
+        assertEquals(test.getReason(),"test");
+        test.setUserId("userId");
+        assertEquals(test.getUserId(),"userId");
+        test.setOperationContent("operation");
+        assertEquals(test.getOperationContent(),"operation");
+        test.setProgress(123);
+        assertEquals(test.getProgress(),123);
+        test.setOperateAt("operate");
+        assertEquals(test.getOperateAt(),"operate");
+        test.setFinishedAt("finished");
+        assertEquals(test.getFinishedAt(),"finished");
+    }
+
+}
---
+++ mso-api-handlers/mso-api-handler-infra/src/test/java/org/openecomp/mso/apihandlerinfra/e2eserviceinstancebeans/E2EUserParamTest.java
@@ -0,0 +1,48 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2018 Huawei Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.mso.apihandlerinfra.e2eserviceinstancebeans;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class E2EUserParamTest {
+
+    E2EUserParam test = new E2EUserParam();
+
+    @Test
+    public void verifyE2EParam() throws Exception {
+
+        test.setName("name");
+        assertEquals(test.getName(),"name");
+        test.setValue("value");
+        assertEquals(test.getValue(),"value");
+        Map<String, Object> additionalProperties = new HashMap<>();
+        additionalProperties.put("name", test);
+        test.setAdditionalProperty("name",test);
+        assertEquals(test.getAdditionalProperties(),additionalProperties);
+
+    }
+
+}
