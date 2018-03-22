646b61c12bae2996fa5b9eeeb4e0f104fbf2a7a1
Smokowski, Steve (ss835w) <ss835w@us.att.com>
2018-03-22 09:18:29 -0400

    Added OpenPojo Validations to beans
    Change-Id: I34d8922a55b1ba35610ad0856dcd3363fa016785
    Issue-ID: SO-516
    Signed-off-by: Smokowski, Steve (ss835w) <ss835w@us.att.com>

--- bpmn/MSOCommonBPMN/src/main/java/org/openecomp/mso/bpmn/appc/payload/beans/StartStopAction.java
+++ bpmn/MSOCommonBPMN/src/main/java/org/openecomp/mso/bpmn/appc/payload/beans/StartStopAction.java
@@ -26,20 +26,30 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-" AICIdentity "
+	" AICIdentity "
 })
 public class StartStopAction {
 
-@JsonProperty(" AICIdentity ")
-private String aICIdentity;
+	@JsonProperty(" AICIdentity ")
+	private String aICIdentity;
 
-@JsonProperty(" AICIdentity ")
-public String getAICIdentity() {
-return aICIdentity;
-}
+	@JsonProperty(" AICIdentity ")
+	public String getAICIdentity() {
+		return aICIdentity;
+	}
 
-@JsonProperty(" AICIdentity ")
-public void setAICIdentity(String aICIdentity) {
-this.aICIdentity = aICIdentity;
-}
+	@JsonProperty(" AICIdentity ")
+	public void setAICIdentity(String aICIdentity) {
+		this.aICIdentity = aICIdentity;
+	}
+
+	public String getaICIdentity() {
+		return aICIdentity;
+	}
+
+	public void setaICIdentity(String aICIdentity) {
+		this.aICIdentity = aICIdentity;
+	}
+
+
 }
