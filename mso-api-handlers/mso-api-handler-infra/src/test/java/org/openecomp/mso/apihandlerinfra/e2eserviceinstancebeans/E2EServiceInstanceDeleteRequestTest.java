f256779b9cab2a0d289ff65c714810d5ce700491
Sindhu A <arcot.sindhuri@huawei.com>
2018-03-22 05:09:02 +0000

    UT Coverage
    UT Coverage for SO
    
    Issue-ID: SO-369
    Change-Id: I1708ed331d07386ce85bf1f448085acdc2915fea
    Signed-off-by: Sindhu A <arcot.sindhuri@huawei.com>

---
+++ mso-api-handlers/mso-api-handler-infra/src/test/java/org/openecomp/mso/apihandlerinfra/e2eserviceinstancebeans/E2EServiceInstanceDeleteRequestTest.java
@@ -0,0 +1,51 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2018 Huawei Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.mso.apihandlerinfra.e2eserviceinstancebeans;
+
+import org.junit.Test;
+
+public class E2EServiceInstanceDeleteRequestTest {
+
+    // TODO: currently test case is done for coverage
+    // later, it should be modified properly.
+
+    E2EServiceInstanceDeleteRequest test = new E2EServiceInstanceDeleteRequest();
+
+    @Test
+    public void getGlobalSubscriberIdTest() throws Exception {
+        test.getGlobalSubscriberId();
+    }
+
+    @Test
+    public void setGlobalSubscriberIdTest() throws Exception {
+        test.setGlobalSubscriberId("id");
+    }
+
+    @Test
+    public void getServiceTypeTest() throws Exception {
+        test.getServiceType();
+    }
+
+    @Test
+    public void setServiceTypeTest() throws Exception {
+        test.setServiceType("type");
+    }
+}
