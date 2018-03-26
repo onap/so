ee00c794c1f1b0264b41d07c536900742dcccb97
subhash kumar singh <subhash.kumar.singh@huawei.com>
2018-03-26 23:57:15 +0530

    Implement resouce sequence
    Define resource sequence from config file.
    
    Change-Id: Icc01001139b4b07c38fccc509e0bd5b7c23e56a5
    Issue-ID: SO-422
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

---
+++ bpmn/MSOInfrastructureBPMN/src/main/java/org/openecomp/mso/bpmn/infrastructure/properties/ResourceSequence.java
@@ -0,0 +1,10 @@
+package org.openecomp.mso.bpmn.infrastructure.properties;
+
+public final class ResourceSequence {
+
+    public static final String VNF_RESOURCE = "vnf-resource";
+    public static final String NETWORK_RESOURCE = "network-resource";
+    public static final String ALLOTTED_RESOURCE = "allotted-resource";
+
+    private ResourceSequence() {}
+}
