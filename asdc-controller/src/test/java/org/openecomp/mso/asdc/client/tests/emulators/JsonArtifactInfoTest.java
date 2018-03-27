e6ef3039bb364d3b5ab0750557d3eb55715a9999
Anjali Walsatwar <anjali.walsatwar@huawei.com>
2018-03-27 16:06:14 +0000

    UT Coverage for asdc-controller emulators
    UT Coverage for asdc-controller emulators
    
    Issue-ID: SO-369
    
    Change-Id: I7b557ceffa5a9677eabc2fa67797ef543c40034e
    Signed-off-by: Anjali Walsatwar <anjali.walsatwar@huawei.com>

---
+++ asdc-controller/src/test/java/org/openecomp/mso/asdc/client/tests/emulators/JsonArtifactInfoTest.java
@@ -0,0 +1,65 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2018 Huawei Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.mso.asdc.client.tests.emulators;
+import org.junit.Test;
+
+import org.openecomp.mso.asdc.client.test.emulators.JsonArtifactInfo;
+import org.openecomp.sdc.api.notification.IArtifactInfo;
+
+import java.util.List;
+
+public class JsonArtifactInfoTest {
+    JsonArtifactInfo jsonArtifactInfo = new JsonArtifactInfo();
+
+    List<JsonArtifactInfo> artifactList;
+
+    @Test
+    public final void addArtifactToUUIDMap()
+    {
+        jsonArtifactInfo.addArtifactToUUIDMap(artifactList);
+    }
+
+    @Test
+    public final void setAttribute()
+    {
+        jsonArtifactInfo.setAttribute("artifactName", "test");
+    }
+
+
+    @Test
+    public final void getArtifactDescription()
+    {
+        final String artifactDescription = jsonArtifactInfo.getArtifactDescription();
+        final String artifactName = jsonArtifactInfo.getArtifactName();
+        final String artifactChecksumfinal = jsonArtifactInfo.getArtifactChecksum();
+        final String artifactChecksum = jsonArtifactInfo.getArtifactChecksum();
+        final Integer artifactTimeout = jsonArtifactInfo.getArtifactTimeout();
+        final String artifactType =  jsonArtifactInfo.getArtifactType();
+        final String artifactURL = jsonArtifactInfo.getArtifactURL();
+        final String artifactUUID = jsonArtifactInfo.getArtifactUUID();
+        final String artifactVersion = jsonArtifactInfo.getArtifactVersion();
+        IArtifactInfo generatedArtifact = jsonArtifactInfo.getGeneratedArtifact();
+        final List<IArtifactInfo> relatedArtifacts = jsonArtifactInfo.getRelatedArtifacts();
+
+    }
+
+
+}
