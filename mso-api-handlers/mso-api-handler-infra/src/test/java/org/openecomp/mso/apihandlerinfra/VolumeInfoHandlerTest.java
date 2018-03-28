cf0790e8a1f804a10761bec64914da7fabb2ca63
Abhishek Shekhar <abhishek.shekhar1@amdocs.com>
2018-03-28 13:27:18 +0530

    UT Coverage for API Handler
    1. NetworkRequestHandler
    2. VolumeInfoHandler
    
    Change-Id: I2b04effcdc9cd8e5ff0af5a73c2fcbfba580f645
    Issue-ID: SO-369
    Signed-off-by: Abhishek Shekhar <abhishek.shekhar1@amdocs.com>

--- mso-api-handlers/mso-api-handler-infra/src/test/java/org/openecomp/mso/apihandlerinfra/VolumeInfoHandlerTest.java
+++ mso-api-handlers/mso-api-handler-infra/src/test/java/org/openecomp/mso/apihandlerinfra/VolumeInfoHandlerTest.java
@@ -22,14 +22,78 @@ package org.openecomp.mso.apihandlerinfra;
 
 import static org.junit.Assert.assertTrue;
 
+import mockit.Mock;
+import mockit.MockUp;
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolVersion;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.message.BasicHttpResponse;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openecomp.mso.apihandler.common.CamundaClient;
+import org.openecomp.mso.apihandlerinfra.volumebeans.ActionType;
+import org.openecomp.mso.apihandlerinfra.volumebeans.RequestStatusType;
 import org.openecomp.mso.apihandlerinfra.volumebeans.VolumeRequest;
+import org.openecomp.mso.db.catalog.CatalogDatabase;
+import org.openecomp.mso.db.catalog.beans.NetworkRecipe;
+import org.openecomp.mso.db.catalog.beans.VfModule;
+import org.openecomp.mso.db.catalog.beans.VnfResource;
+import org.openecomp.mso.properties.MsoPropertiesFactory;
+import org.openecomp.mso.requestsdb.InfraActiveRequests;
 import org.openecomp.mso.requestsdb.InfraRequests;
+import org.openecomp.mso.requestsdb.RequestsDatabase;
+
+import javax.ws.rs.core.Response;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
 
 public class VolumeInfoHandlerTest {
 
 	VolumeInfoHandler handler = new VolumeInfoHandler();
-	
+
+	private static MockUp<RequestsDatabase> mockRDB;
+
+	@BeforeClass
+	public static void setUp() throws Exception {
+		MsoPropertiesFactory msoPropertiesFactory = new MsoPropertiesFactory();
+		msoPropertiesFactory.removeAllMsoProperties();
+		msoPropertiesFactory.initializeMsoProperties(Constants.MSO_PROP_APIHANDLER_INFRA, "src/test/resources/mso.apihandler-infra.properties");
+
+		mockRDB = new MockUp<RequestsDatabase>() {
+			@Mock
+			public List<InfraActiveRequests> getRequestListFromInfraActive (String queryAttributeName,
+																			String queryValue,
+																			String requestType) {
+				final InfraActiveRequests requests = new InfraActiveRequests();
+				requests.setAction(ActionType.CREATE.name());
+				requests.setRequestStatus(RequestStatusType.IN_PROGRESS.name());
+				requests.setStartTime(Timestamp.valueOf(LocalDateTime.now()));
+				return Collections.singletonList(requests);
+			}
+			@Mock
+			public InfraActiveRequests getRequestFromInfraActive (String requestId, String requestType) {
+				final InfraActiveRequests requests = new InfraActiveRequests();
+				requests.setAction(ActionType.CREATE.name());
+				requests.setRequestStatus(RequestStatusType.IN_PROGRESS.name());
+				requests.setStartTime(Timestamp.valueOf(LocalDateTime.now()));
+				return requests;
+			}
+		};
+
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		mockRDB.tearDown();
+	}
+
 	@Test
 	public void fillVnfRequestTestV3(){
 		VolumeRequest qr = new VolumeRequest();
@@ -50,5 +114,22 @@ public class VolumeInfoHandlerTest {
 		handler.fillVolumeRequest(qr, ar, "v2");
 		String vnfid = (String)qr.getVolumeParams();
 		assertTrue(vnfid.equals("test"));
+	}
+
+	@Test
+	public void queryFilters() {
+		final Response response = handler.queryFilters("vnf-type", "svc-type", "aicNode", "tenant-id",
+				"vg-id", "vg-name", "v3");
+	}
+
+	@Test
+	public void queryFilters2() {
+		final Response response = handler.queryFilters(null, "svc-type", "aicNode", "tenant-id",
+				"vg-id", "vg-name", "v3");
+	}
+
+	@Test
+	public void getRequest() {
+		final Response response = handler.getRequest("request-id", "v3");
 	}
 }
