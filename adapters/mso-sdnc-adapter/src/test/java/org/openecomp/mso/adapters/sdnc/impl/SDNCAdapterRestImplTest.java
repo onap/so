800ecc737265bad39451af9f7e285ea22235a74c
Sindhu A <arcot.sindhuri@huawei.com>
2018-03-28 21:02:55 +0000

    UT for sdnc adapter rest impl
    UT for sdnc-adapter SDNCAdapterRestImpl
    
    Issue-ID: SO-369
    
    Change-Id: I1378458de4a47e561c6b3b99222133e3551924ae
    Signed-off-by: Sindhu A <arcot.sindhuri@huawei.com>

---
+++ adapters/mso-sdnc-adapter/src/test/java/org/openecomp/mso/adapters/sdnc/impl/SDNCAdapterRestImplTest.java
@@ -0,0 +1,61 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2018 Huawei Technologies Co., Ltd. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.openecomp.mso.adapters.sdnc.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SDNCAdapterRestImplTest {
+
+    SDNCAdapterRestImpl test = new SDNCAdapterRestImpl();
+
+    @Test(expected = ClassFormatError.class)
+    public void testMSORequestException() {
+
+        test.MSORequest("reqXML");
+        Assert.assertFalse(true);
+               
+    }
+
+    @Test(expected = NoClassDefFoundError.class)
+    public void testHealthCheckException() {
+
+        test.healthcheck("1a25a7c0-4c91-4f74-9a78-8c11b7a57f1a");
+        Assert.assertFalse(true);
+    }
+
+    @Test(expected = ClassFormatError.class)
+    public void testglobalHealthcheckException() {
+
+        test.globalHealthcheck(true);
+        Assert.assertFalse(true);
+    }
+
+
+    @Test(expected = NoClassDefFoundError.class)
+    public void testNodeHealthCheckException() {
+
+        test.nodeHealthcheck();
+        Assert.assertFalse(true);
+    }
+
+}
+
