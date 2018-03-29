9341028f3a1a29ad3db9ded01424d821fa2f572f
Anjali Walsatwar <anjali.walsatwar@huawei.com>
2018-03-29 06:26:14 +0000

    UT Coverage for asdc-controller
    UT Coverage for asdc-controller
    
    Issue-ID: SO-369
    
    Change-Id: Id38747b83f41218024592eaf225eb52de91fd4c2
    Signed-off-by: Anjali Walsatwar <anjali.walsatwar@huawei.com>

--- asdc-controller/src/test/java/org/openecomp/mso/asdc/client/YamlTest.java
+++ asdc-controller/src/test/java/org/openecomp/mso/asdc/client/YamlTest.java
@@ -2,7 +2,7 @@
  * ============LICENSE_START=======================================================
  * ONAP - SO
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2018 Huawei Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
