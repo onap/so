9218d80e65bf3eaa01e7942730bac8c72dcb7862
Rob Daugherty <rd472p@att.com>
2018-04-18 16:57:11 -0400

    AAIRestClient support for Basic Auth
    Adding support for Basic Auth in the AAI Rest Clients because
    ONAP uses this (whereas ECOMP uses 2-way SSL).
    
    In general, each AAI client will allow the user to configure
    properties called "aai.auth" and "mso.msoKey".  If these are
    set, then the client will add the Authorization header to every
    request.
    
    Change-Id: I7c81ec05d2ec4a7dca131f2e9e19d341ac89b09f
    Issue-ID: SO-576
    Signed-off-by: Rob Daugherty <rd472p@att.com>

--- bpmn/MSOCommonBPMN/src/main/java/org/openecomp/mso/client/adapter/vnf/AdapterRestClient.java
+++ bpmn/MSOCommonBPMN/src/main/java/org/openecomp/mso/client/adapter/vnf/AdapterRestClient.java
@@ -21,7 +21,6 @@
 package org.openecomp.mso.client.adapter.vnf;
 
 import java.net.URI;
-import java.security.GeneralSecurityException;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
@@ -29,8 +28,6 @@ import java.util.UUID;
 import javax.ws.rs.client.ClientResponseFilter;
 import javax.ws.rs.ext.ContextResolver;
 
-import org.apache.commons.codec.binary.Base64;
-import org.openecomp.mso.bpmn.common.util.CryptoUtils;
 import org.openecomp.mso.client.ResponseExceptionMapperImpl;
 import org.openecomp.mso.client.policy.JettisonStyleMapperProvider;
 import org.openecomp.mso.client.policy.RestClient;
@@ -52,8 +49,7 @@ public class AdapterRestClient extends RestClient {
 
 	@Override
 	protected void initializeHeaderMap(Map<String, String> headerMap) {
-		headerMap.put("Authorization",
-				this.getBasicAuth(props.getAuth(), props.getKey()));
+		addBasicAuthHeader(props.getAuth(), props.getKey());
 	}
 
 	@Override
@@ -69,21 +65,5 @@ public class AdapterRestClient extends RestClient {
 	@Override
 	protected ContextResolver<ObjectMapper> getMapper() {
 		return new JettisonStyleMapperProvider();
-	}
-	
-	private String getBasicAuth(String encryptedAuth, String msoKey) {
-		if ((encryptedAuth == null || encryptedAuth.isEmpty()) || (msoKey == null || msoKey.isEmpty())) {
-			return null;
-		}
-		try {
-			String auth = CryptoUtils.decrypt(encryptedAuth, msoKey);
-			byte[] encoded = Base64.encodeBase64(auth.getBytes());
-			String encodedString = new String(encoded);
-			encodedString = "Basic " + encodedString;
-			return encodedString;
-		} catch (GeneralSecurityException e) {
-			this.logger.warn(e.getMessage(), e);
-			return null;
-		}
 	}
 }
