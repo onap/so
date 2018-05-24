e012d750d0702ef68324f232d7b361b177c778ca
Tomasz Switon <tomasz.switon@nokia.com>
2018-05-24 13:11:16 +0200

    Fix potential error problems with streams
    Change-Id: I212ce25ead07746bf050f4db2aeb0fedd325c439
    Issue-ID: SO-644
    Signed-off-by: Switon Tomasz <tomasz.switon@nokia.com>

--- adapters/mso-adapter-utils/src/main/java/org/openecomp/mso/aria/CSAR.java
+++ adapters/mso-adapter-utils/src/main/java/org/openecomp/mso/aria/CSAR.java
@@ -14,17 +14,17 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  * ============LICENSE_END====================================================
-*/
+ */
 package org.openecomp.mso.aria;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
-import java.util.Map;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -38,151 +38,159 @@ import com.google.common.io.Files;
 /**
  * The purpose of this class is to create a CSAR byte array from Vdu inputs for the purpose
  * of forwarding to a TOSCA orchestrator.
- * 
- * @author DeWayne
  *
+ * @author DeWayne
  */
 public class CSAR {
-	private static final String MANIFEST_FILENAME = "MANIFEST.MF";
-	private VduModelInfo vduModel;
+    private static final String MANIFEST_FILENAME = "MANIFEST.MF";
+    private VduModelInfo vduModel;
 
-	public CSAR(VduModelInfo model){
-	   this.vduModel = model;
-	}
+    public CSAR(VduModelInfo model) {
+        this.vduModel = model;
+    }
 
-	/**
-	 * Creates a byte array representation of a CSAR corresponding to the VduBlueprint arg in the
-	 * constructor.  
-	 * 
-	 * @return
-	 */
-	public byte[] create() {
-		File dir = Files.createTempDir();
+    /**
+     * Creates a byte array representation of a CSAR corresponding to the VduBlueprint arg in the
+     * constructor.
+     *
+     * @return
+     */
+    public byte[] create() {
+        File dir = Files.createTempDir();
 
-		/**
-		 * Create subdir
-		 */
-		File metadir = new File(dir.getAbsolutePath() + "/" + "TOSCA-Metadata");
-		if (!metadir.mkdir()) {
-			throw new RuntimeException("CSAR TOSCA-Metadata directory create failed");
-		}
+        /**
+         * Create subdir
+         */
+        File metadir = new File(dir.getAbsolutePath() + "/" + "TOSCA-Metadata");
+        if (!metadir.mkdir()) {
+            throw new RuntimeException("CSAR TOSCA-Metadata directory create failed");
+        }
 
-		/**
-		 * Organize model info for consumption
-		 */
-		VduArtifact mainTemplate = null;
-		List<VduArtifact> extraFiles = null;
-		for(VduArtifact artifact: vduModel.getArtifacts()){
-				  if( artifact.getType() == ArtifactType.MAIN_TEMPLATE ){
-					 mainTemplate = artifact;
-				  }
-				  else{
-				    extraFiles.add(artifact);
-				  }
-		}	  
+        /**
+         * Organize model info for consumption
+         */
+        VduArtifact mainTemplate = null;
+        List<VduArtifact> extraFiles = null;
+        for (VduArtifact artifact : vduModel.getArtifacts()) {
+            if (artifact.getType() == ArtifactType.MAIN_TEMPLATE) {
+                mainTemplate = artifact;
+            } else {
+                extraFiles.add(artifact);
+            }
+        }
 
-		/**
-		 * Write template files
-		 */
-		OutputStream ofs = null;
-		try {
-			ofs = new FileOutputStream(new File(dir, mainTemplate.getName()));
-			ofs.write(mainTemplate.getContent());
-			ofs.close();
+        try {
+            writeMainTemplateFile(dir, mainTemplate);
 
-			/**
-			 * Write other files
-			 */
-			if (extraFiles != null) {
-				for (VduArtifact artifact: extraFiles){
-					ofs = new FileOutputStream(new File(dir, artifact.getName()));
-					ofs.write(artifact.getContent());
-					ofs.close();
-				}
-			}
+            writeExtraFiles(dir, extraFiles);
 
+            createManifest(metadir, mainTemplate);
 
-			/**
-			 * Create manifest
-			 */
-			PrintStream mfstream = new PrintStream(new File(metadir.getAbsolutePath() + "/" + MANIFEST_FILENAME));
-			mfstream.println("TOSCA-Meta-File-Version: 1.0");
-			mfstream.println("CSAR-Version: 1.1");
-			mfstream.println("Created-by: ONAP");
-			mfstream.println("Entry-Definitions: " + mainTemplate.getName());
-			mfstream.close();
+            return getZipbytesOfTree(dir).toByteArray();
 
-			/**
-			 * ZIP it up
-			 */
-			ByteArrayOutputStream zipbytes = new ByteArrayOutputStream();
-			ZipOutputStream zos = new ZipOutputStream(zipbytes);
-			compressTree(zos, "", dir, dir);
-			zos.close();
-			return zipbytes.toByteArray();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create CSAR: " + e.getMessage());
+        } finally {
 
-		} catch (Exception e) {
-			throw new RuntimeException("Failed to create CSAR: " + e.getMessage());
-		} finally {
+            /**
+             * Clean up tmpdir
+             */
+            deleteDirectory(dir);
+        }
+    }
 
-			/**
-			 * Clean up tmpdir
-			 */
-			deleteDirectory(dir);
-		}
-	}
+    /**
+     * Private methods
+     */
 
-	/**
-	 * Private methods
-	 */
+    private ByteArrayOutputStream getZipbytesOfTree(File dir) throws IOException {
+        ByteArrayOutputStream zipbytes = new ByteArrayOutputStream();
+        try (ZipOutputStream zos = new ZipOutputStream(zipbytes)) {
+            compressTree(zos, "", dir, dir);
+        }
+        return zipbytes;
+    }
 
-	/**
-	 * Compresses (ZIPs) a directory tree
-	 * 
-	 * @param dir
-	 * @throws IOException
-	 */
-	private void compressTree(ZipOutputStream zos, String path, File basedir, File dir) throws IOException {
-		if (!dir.isDirectory())
-			return;
+    private void writeExtraFiles(File dir, List<VduArtifact> extraFiles) throws IOException {
+        if (extraFiles != null) {
+            for (VduArtifact artifact : extraFiles) {
+                writeFile(dir, artifact);
+            }
+        }
+    }
 
-		for (File f : dir.listFiles()) {
-			if (f.isDirectory()) {
-				String newpath = path + f.getName() + "/";
-				ZipEntry entry = new ZipEntry(newpath);
-				zos.putNextEntry(entry);
-				zos.closeEntry();
-				compressTree(zos, newpath, basedir, f);
-			} else {
-				ZipEntry ze = new ZipEntry(
-						f.getAbsolutePath().substring(basedir.getAbsolutePath().length() + 1).replaceAll("\\\\", "/"));
-				zos.putNextEntry(ze);
-				// read the file and write to ZipOutputStream
-				FileInputStream fis = new FileInputStream(f);
-				byte[] buffer = new byte[1024];
-				int len;
-				while ((len = fis.read(buffer)) > 0) {
-					zos.write(buffer, 0, len);
-				}
-				zos.closeEntry();
-				fis.close();
-			}
-		}
-	}
+    private void writeMainTemplateFile(File dir, VduArtifact mainTemplate) throws IOException {
+        if (mainTemplate != null) {
+            writeFile(dir, mainTemplate);
+        }
+    }
 
-	private boolean deleteDirectory(File directory) {
-		if (directory.exists()) {
-			File[] files = directory.listFiles();
-			if (null != files) {
-				for (int i = 0; i < files.length; i++) {
-					if (files[i].isDirectory()) {
-						deleteDirectory(files[i]);
-					} else {
-						files[i].delete();
-					}
-				}
-			}
-		}
-		return (directory.delete());
-	}
+    private void createManifest(File metadir, VduArtifact mainTemplate) {
+        if (mainTemplate != null) {
+            try (PrintStream mfstream = new PrintStream(new File(metadir.getAbsolutePath() + "/" + MANIFEST_FILENAME))) {
+                mfstream.println("TOSCA-Meta-File-Version: 1.0");
+                mfstream.println("CSAR-Version: 1.1");
+                mfstream.println("Created-by: ONAP");
+                mfstream.println("Entry-Definitions: " + mainTemplate.getName());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void writeFile(File dir, VduArtifact artifact) throws IOException {
+        try (OutputStream fos = new FileOutputStream(new File(dir, artifact.getName()))) {
+            fos.write(artifact.getContent());
+        }
+    }
+
+    /**
+     * Compresses (ZIPs) a directory tree
+     *
+     * @param dir
+     * @throws IOException
+     */
+    private void compressTree(ZipOutputStream zos, String path, File basedir, File dir) throws IOException {
+        if (!dir.isDirectory())
+            return;
+
+        for (File f : dir.listFiles()) {
+            if (f.isDirectory()) {
+                String newpath = path + f.getName() + "/";
+                ZipEntry entry = new ZipEntry(newpath);
+                zos.putNextEntry(entry);
+                zos.closeEntry();
+                compressTree(zos, newpath, basedir, f);
+            } else {
+                ZipEntry ze = new ZipEntry(
+                        f.getAbsolutePath().substring(basedir.getAbsolutePath().length() + 1).replaceAll("\\\\", "/"));
+                zos.putNextEntry(ze);
+                // read the file and write to ZipOutputStream
+                FileInputStream fis = new FileInputStream(f);
+                byte[] buffer = new byte[1024];
+                int len;
+                while ((len = fis.read(buffer)) > 0) {
+                    zos.write(buffer, 0, len);
+                }
+                zos.closeEntry();
+                fis.close();
+            }
+        }
+    }
+
+    private boolean deleteDirectory(File directory) {
+        if (directory.exists()) {
+            File[] files = directory.listFiles();
+            if (null != files) {
+                for (int i = 0; i < files.length; i++) {
+                    if (files[i].isDirectory()) {
+                        deleteDirectory(files[i]);
+                    } else {
+                        files[i].delete();
+                    }
+                }
+            }
+        }
+        return (directory.delete());
+    }
 }
