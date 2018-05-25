f922823ee62c1ebda6b555c29a5a836917f3bf6b
Adam Wudzinski <adam.wudzinski@nokia.com>
2018-05-25 12:46:03 +0200

    AriaRestClient sonar issues fix
    Change-Id: I76c5d0b0461ec75542469d4f85d7ff1e7e85d7c2
    Issue-ID: SO-651
    Signed-off-by: awudzins <adam.wudzinski@nokia.com>

---
+++ aria/aria-rest-java-client/src/main/java/com/gigaspaces/aria/rest/client/exceptions/RequestProcessingException.java
@@ -0,0 +1,10 @@
+package com.gigaspaces.aria.rest.client.exceptions;
+
+/**
+ * Created by adam on 25.05.18.
+ */
+public class RequestProcessingException extends Exception{
+    public RequestProcessingException(String message) {
+        super(message);
+    }
+}
