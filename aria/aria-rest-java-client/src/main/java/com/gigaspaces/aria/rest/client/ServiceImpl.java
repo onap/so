d5d522eb39199ea753dff19392eab8536bb2e594
Justyna Czestochowska <justyna.czestochowsa@nokia.com>
2018-05-25 10:47:57 +0200

    Fix sonar issues in aria rest client ServiceImpl
    Change-Id: I7a0f6a9026b90b189df3c987ba74401f3abcef06
    Issue-ID: SO-652
    Signed-off-by: jczestochowska <justyna.czestochowska@nokia.com>

--- aria/aria-rest-java-client/src/main/java/com/gigaspaces/aria/rest/client/ServiceImpl.java
+++ aria/aria-rest-java-client/src/main/java/com/gigaspaces/aria/rest/client/ServiceImpl.java
@@ -17,7 +17,6 @@
 */
 package com.gigaspaces.aria.rest.client;
 
-import java.net.URI;
 import java.util.Date;
 
 /**
@@ -27,8 +26,11 @@ import java.util.Date;
  */
 public class ServiceImpl implements Service {
     private int id;
-    private String description, name, template;
-    private Date created, updated;
+    private String description;
+    private String name;
+    private String template;
+    private Date created;
+    private Date updated;
 
     public int getId(){
         return id;
