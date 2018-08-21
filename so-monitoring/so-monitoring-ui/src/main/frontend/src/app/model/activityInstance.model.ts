6b425dc28fd78e48fc2028e5cb06552455cf7609
waqas.ikram <waqas.ikram@ericsson.com>
2018-08-21 17:35:35 +0100

    SO Monitoring UI
    Change-Id: I6f1c13119d18b8731a02c8559bb1484ec84d6041
    Issue-ID: SO-726
    Signed-off-by: waqas.ikram <waqas.ikram@ericsson.com>
    Signed-off-by: Ronan Kenny <ronan.kenny@ericsson.com>

---
+++ so-monitoring/so-monitoring-ui/src/main/frontend/src/app/model/activityInstance.model.ts
@@ -0,0 +1,32 @@
+/**
+============LICENSE_START=======================================================
+ Copyright (C) 2018 Ericsson. All rights reserved.
+================================================================================
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+ limitations under the License.
+
+SPDX-License-Identifier: Apache-2.0
+============LICENSE_END=========================================================
+
+@authors: ronan.kenny@ericsson.com, waqas.ikram@ericsson.com
+*/
+
+export interface ACTINST {
+  activityId: string;
+  activityName: string;
+  activityType: string;
+  processInstanceId: string;
+  calledProcessInstanceId: string;
+  startTime: string;
+  endTime: string;
+  durationInMillis: string;
+}
