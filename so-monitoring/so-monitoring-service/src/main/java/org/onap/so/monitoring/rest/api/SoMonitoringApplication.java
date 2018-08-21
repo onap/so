2f71ef7bd72d2a2ccc1a8e408058a46d20e5caff
waqas.ikram <waqas.ikram@ericsson.com>
2018-08-21 17:35:35 +0100

    SO Monitoring UI
    Change-Id: I6f1c13119d18b8731a02c8559bb1484ec84d6041
    Issue-ID: SO-726
    Signed-off-by: waqas.ikram <waqas.ikram@ericsson.com>

--- so-monitoring/so-monitoring-service/src/main/java/org/onap/so/monitoring/rest/api/SoMonitoringApplication.java
+++ so-monitoring/so-monitoring-service/src/main/java/org/onap/so/monitoring/rest/api/SoMonitoringApplication.java
@@ -22,12 +22,6 @@ package org.onap.so.monitoring.rest.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 /**
  * @author waqas.ikram@ericsson.com
@@ -38,20 +32,6 @@ public class SoMonitoringApplication {
     public static void main(String[] args) {
         SpringApplication.run(SoMonitoringApplication.class, args);
 
-    }
-
-    @Bean
-    public FilterRegistrationBean corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        final FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return bean;
     }
 
 }
