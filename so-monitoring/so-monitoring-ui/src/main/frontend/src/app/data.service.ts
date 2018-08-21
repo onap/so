29b0e6f06dff3b4f5d13a6cbd8d3fbb234d742f9
waqas.ikram <waqas.ikram@ericsson.com>
2018-08-21 17:35:35 +0100

    SO Monitoring UI
    Change-Id: I6f1c13119d18b8731a02c8559bb1484ec84d6041
    Issue-ID: SO-726
    Signed-off-by: waqas.ikram <waqas.ikram@ericsson.com>

---
+++ so-monitoring/so-monitoring-ui/src/main/frontend/src/app/data.service.ts
@@ -0,0 +1,96 @@
+/**
+============LICENSE_START=======================================================
+ Copyright (C) 2018 Ericsson. All rights reserved.
+================================================================================
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+ limitations under the License.
+
+SPDX-License-Identifier: Apache-2.0
+============LICENSE_END=========================================================
+
+@authors: ronan.kenny@ericsson.com, waqas.ikram@ericsson.com
+*/
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Process } from './model/process.model';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { ProcessInstanceId } from './model/processInstanceId.model';
+import { environment } from '../environments/environment';
+import { HttpResponse } from '@angular/common/http';
+import { PII } from './model/processInstance.model';
+import { HttpErrorHandlerService } from './http-error-handler.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient, private httpErrorHandlerService: HttpErrorHandlerService) { }
+
+  // HTTP POST call to running Spring Boot application
+  retrieveInstance(servInstId: {}, from: number, to: number) {
+    var url = environment.soMonitoringBackendURL + 'v1/search?from=' + from + "&to=" + to;
+    return this.http.post<Process[]>(url, servInstId)
+      .pipe(
+        catchError(this.httpErrorHandlerService.handleError("POST", url))
+      );
+  }
+
+  // HTTP GET to return Process Instance using RequestID
+  getProcessInstanceId(requestId): Observable<HttpResponse<ProcessInstanceId>> {
+    var url = environment.soMonitoringBackendURL + 'process-instance-id/' + requestId;
+    console.log(requestId);
+    console.log(url);
+    return this.http.get<ProcessInstanceId>(url, { observe: 'response' })
+      .pipe(
+        catchError(this.httpErrorHandlerService.handleError("GET", url))
+      );
+  }
+
+  // HTTP GET to return Activity instancs using ProcessInstanceID
+  getActivityInstance(processInstanceId) {
+    var url = environment.soMonitoringBackendURL + 'activity-instance/' + processInstanceId;
+    return this.http.get(url)
+      .pipe(
+        catchError(this.httpErrorHandlerService.handleError("GET", url))
+      );
+  }
+
+  // HTTP GET to return Activity Instance using ProcessInstanceID
+  async getProcessInstance(processInstanceId): Promise<PII> {
+    var url = environment.soMonitoringBackendURL + 'process-instance/' + processInstanceId;
+    return await (this.http.get<PII>(url)
+      .pipe(
+        catchError(this.httpErrorHandlerService.handleError("GET", url))))
+      .toPromise();
+  }
+
+  // HTTP GET to return Process Definition using processDefinitionId
+  getProcessDefinition(processDefinitionId) {
+    var url = environment.soMonitoringBackendURL + 'process-definition/' + processDefinitionId;
+    return this.http.get(url).pipe(
+      catchError(this.httpErrorHandlerService.handleError("GET", url))
+    );
+  }
+
+  // HTTP GET to return Variable Instance using ProcessInstanceID
+  getVariableInstance(processDefinitionId) {
+    var url = environment.soMonitoringBackendURL + 'variable-instance/' + processDefinitionId;
+    return this.http.get(url).pipe(
+      catchError(this.httpErrorHandlerService.handleError("GET", url))
+    );
+  }
+
+}
---
+++ so-monitoring/so-monitoring-ui/src/main/frontend/src/app/model/searchData.model.ts
@@ -0,0 +1,105 @@
+import { ToastrNotificationService } from "../toastr-notification-service.service";
+
+/**
+============LICENSE_START=======================================================
+ Copyright (C) 2018 Ericsson. All rights reserved.
+================================================================================
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+ limitations under the License.
+
+SPDX-License-Identifier: Apache-2.0
+============LICENSE_END=========================================================
+
+@authors: ronan.kenny@ericsson.com, waqas.ikram@ericsson.com
+*/
+
+import { Observable, throwError, of } from 'rxjs';
+import { SearchRequest } from "./SearchRequest.model";
+export class SearchData {
+
+  public selectedValueSII = "EQ";
+  public selectedValueRI = "EQ";
+  public selectedValueSN = "EQ";
+  public selectedValueSTATUS = "ALL";
+
+  private now = Date.now();
+  // Minus 1 hour from current time for start date
+  public startDate = new Date(this.now - (1 * 60 * 60 * 1000));
+  public selectedStartHour = this.getNumberAsString(this.startDate.getHours());
+  public selectedStartMinute = this.getNumberAsString(this.startDate.getMinutes());
+
+  public endDate = new Date(this.now);
+  public selectedEndHour = this.getNumberAsString(this.endDate.getHours());
+  public selectedEndMinute = this.getNumberAsString(this.endDate.getMinutes());
+
+
+  public serviceInstanceId: string;
+  public requestId: string;
+  public serviceInstanceName: string;
+
+  private startTimeInMilliseconds: number;
+  private endTimeInMilliseconds: number;
+
+  constructor() {
+  }
+
+  public getSearchRequest(): Observable<SearchRequest> {
+    var searchFields = {};
+    if ((!this.startDate || this.startDate === null) || (!this.endDate || this.endDate === null)) {
+      console.error("Found either start time or end time null or undefined");
+      return throwError("Found end or start date empty, Please enter start and end date");
+    }
+
+    this.startDate.setHours(parseInt(this.selectedStartHour));
+    this.startDate.setMinutes(parseInt(this.selectedStartMinute));
+
+    this.endDate.setHours(parseInt(this.selectedEndHour));
+    this.endDate.setMinutes(parseInt(this.selectedEndMinute));
+
+    this.startTimeInMilliseconds = this.startDate.getTime();
+    this.endTimeInMilliseconds = this.endDate.getTime();
+
+    if (this.startTimeInMilliseconds > this.endTimeInMilliseconds) {
+      console.error("End time: " + this.endDate + " can not be greater then start time: " + this.startDate);
+      return throwError("End time: " + this.endDate + " can not be greater then start time: " + this.startDate);
+    }
+
+
+    if (!this.isEmpty(this.selectedValueSII) && !this.isEmpty(this.serviceInstanceId)) {
+      searchFields["serviceInstanceId"] = [this.selectedValueSII, this.serviceInstanceId]
+    }
+    if (!this.isEmpty(this.selectedValueRI) && !this.isEmpty(this.requestId)) {
+      searchFields["requestId"] = [this.selectedValueRI, this.requestId]
+    }
+    if (!this.isEmpty(this.selectedValueSN) && !this.isEmpty(this.serviceInstanceName)) {
+      searchFields["serviceInstanceName"] = [this.selectedValueSN, this.serviceInstanceName]
+    }
+
+    if (!this.isEmpty(this.selectedValueSTATUS) && this.selectedValueSTATUS !== "ALL") {
+      searchFields["requestStatus"] = ["EQ", this.selectedValueSTATUS]
+    }
+
+    return of(new SearchRequest(searchFields, this.startTimeInMilliseconds, this.endTimeInMilliseconds));
+  }
+
+  private isEmpty(str) {
+    return (!str || 0 === str.length);
+  }
+
+  private getNumberAsString(num: number) {
+    if (num <= 9) {
+      return "0" + num;
+    }
+    return "" + num;
+  }
+
+}
