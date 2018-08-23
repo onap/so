8443f1c9d8efb9c601d4849b5d001d05a67e0c71
waqas.ikram <waqas.ikram@ericsson.com>
2018-08-23 13:26:09 +0100

    Adding rest service for so monitoring
    Change-Id: I9dac918998901d54b3cbc5477cc9c057c3019cb3
    Issue-ID: SO-724
    Signed-off-by: waqas.ikram <waqas.ikram@ericsson.com>

---
+++ so-monitoring/so-monitoring-handler/src/main/java/org/onap/so/montoring/rest/service/HttpRestServiceProviderImpl.java
@@ -0,0 +1,117 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2018 Ericsson. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.so.montoring.rest.service;
+
+import org.onap.so.montoring.exception.InvalidRestRequestException;
+import org.onap.so.montoring.exception.RestProcessingException;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import com.google.common.base.Optional;
+
+/**
+ * @author waqas.ikram@ericsson.com
+ */
+@Service
+public class HttpRestServiceProviderImpl implements HttpRestServiceProvider {
+
+    private static final XLogger LOGGER = XLoggerFactory.getXLogger(HttpRestServiceProviderImpl.class);
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public HttpRestServiceProviderImpl(final RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public <T> Optional<T> getHttpResponse(final String url, final Class<T> clazz) {
+        LOGGER.trace("Will invoke HTTP GET using URL: {}", url);
+        try {
+            final ResponseEntity<T> response = restTemplate.exchange(url, HttpMethod.GET, null, clazz);
+            if (!response.getStatusCode().equals(HttpStatus.OK)) {
+                LOGGER.error("Unable to invoke HTTP GET using URL: {}, Response Code: {}", url,
+                        response.getStatusCode());
+                return Optional.absent();
+            }
+
+            if (response.hasBody()) {
+                return Optional.of(response.getBody());
+            }
+        } catch (final HttpClientErrorException httpClientErrorException) {
+            LOGGER.error("Unable to invoke HTTP GET using url: {}, Response: {}", url,
+                    httpClientErrorException.getRawStatusCode(), httpClientErrorException);
+            final int rawStatusCode = httpClientErrorException.getRawStatusCode();
+            if (rawStatusCode == HttpStatus.BAD_REQUEST.value() || rawStatusCode == HttpStatus.NOT_FOUND.value()) {
+                throw new InvalidRestRequestException("No result found for given url: " + url);
+            }
+            throw new RestProcessingException("Unable to invoke HTTP GET using URL: " + url);
+
+        } catch (final RestClientException restClientException) {
+            LOGGER.error("Unable to invoke HTTP GET using url: {}", url, restClientException);
+            throw new RestProcessingException("Unable to invoke HTTP GET using URL: " + url, restClientException);
+        }
+
+        return Optional.absent();
+    }
+
+    @Override
+    public <T> Optional<T> postHttpRequest(final Object object, final String url, final Class<T> clazz) {
+        try {
+            final HttpEntity<?> request = new HttpEntity<>(object);
+            final ResponseEntity<T> response = restTemplate.exchange(url, HttpMethod.POST, request, clazz);
+            if (!response.getStatusCode().equals(HttpStatus.OK)) {
+                LOGGER.error("Unable to invoke HTTP GET using URL: {}, Response Code: {}", url,
+                        response.getStatusCode());
+                return Optional.absent();
+            }
+
+            if (response.hasBody()) {
+                return Optional.of(response.getBody());
+            }
+
+        } catch (final HttpClientErrorException httpClientErrorException) {
+            LOGGER.error("Unable to invoke HTTP POST using url: {}, Response: {}", url,
+                    httpClientErrorException.getRawStatusCode(), httpClientErrorException);
+            final int rawStatusCode = httpClientErrorException.getRawStatusCode();
+            if (rawStatusCode == HttpStatus.BAD_REQUEST.value() || rawStatusCode == HttpStatus.NOT_FOUND.value()) {
+                throw new InvalidRestRequestException("No result found for given url: " + url);
+            }
+            throw new RestProcessingException("Unable to invoke HTTP POST using URL: " + url);
+
+        } catch (final RestClientException restClientException) {
+            LOGGER.error("Unable to invoke HTTP POST using url: {}", url, restClientException);
+            throw new RestProcessingException("Unable to invoke HTTP POST using URL: " + url, restClientException);
+        }
+
+        return Optional.absent();
+    }
+
+
+}
