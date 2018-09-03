aa10706da87c0111092a40f2d7b4f0b8295360e1
Sandeep J <sandeejh@in.ibm.com>
2018-09-03 19:47:28 +0530

    fixed sonar issue in UpdateNetworkRequest.java
    fixed sonar issue as detailed in SO-960
    
    Issue-ID: SO-960
    Change-Id: Iebf2fc563fea2aef70c8f7db75d104efeeb54ff5
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- adapters/mso-adapters-rest-interface/src/main/java/org/onap/so/adapters/nwrest/UpdateNetworkRequest.java
+++ adapters/mso-adapters-rest-interface/src/main/java/org/onap/so/adapters/nwrest/UpdateNetworkRequest.java
@@ -4,6 +4,8 @@
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Modifications Copyright (C) 2018 IBM.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -58,7 +60,7 @@ public class UpdateNetworkRequest extends NetworkRequestCommon {
 	private Boolean backout = true;
 	private Map<String,String> networkParams = new HashMap<>();
 	private MsoRequest msoRequest = new MsoRequest();
-	private static final MsoLogger LOGGER = MsoLogger.getMsoLogger (MsoLogger.Catalog.RA, UpdateNetworkRequest.class);
+	
 	@JsonProperty
 	private boolean contrailRequest;
 	public UpdateNetworkRequest() {
