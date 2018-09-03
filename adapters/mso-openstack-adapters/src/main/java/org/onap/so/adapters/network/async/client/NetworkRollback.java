28aed9df507b6b65b3cbce608a8298500e0e40e0
Sandeep J <sandeejh@in.ibm.com>
2018-09-03 18:47:52 +0530

    fixed sonar issue in NetworkRollback.java
    fixed sonar issue as detailed in SO-954
    
    Issue-ID: SO-954
    Change-Id: I89d1e082820f76bcbe282bba35fd9144283afe74
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- adapters/mso-openstack-adapters/src/main/java/org/onap/so/adapters/network/async/client/NetworkRollback.java
+++ adapters/mso-openstack-adapters/src/main/java/org/onap/so/adapters/network/async/client/NetworkRollback.java
@@ -4,6 +4,8 @@
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Modifications Copyright (C) 2018 IBM.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -362,7 +364,7 @@ public class NetworkRollback {
      */
     public List<Integer> getVlans() {
         if (vlans == null) {
-            vlans = new ArrayList<Integer>();
+            vlans = new ArrayList<>();
         }
         return this.vlans;
     }
