d49ae53e3e95fd831cf123be2f32a9fe597024c8
Sandeep J <sandeejh@in.ibm.com>
2018-09-03 19:19:49 +0530

    fixed sonar issue in NetworkException.java
    fix sonar issue as detailed in SO-957
    
    Issue-ID: SO-957
    Change-Id: Ifbf83713015e71bfec8c45e77a466cd7cc908dd9
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- adapters/mso-openstack-adapters/src/main/java/org/onap/so/adapters/network/exceptions/NetworkException.java
+++ adapters/mso-openstack-adapters/src/main/java/org/onap/so/adapters/network/exceptions/NetworkException.java
@@ -4,6 +4,8 @@
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Modifications Copyright (C) 2018 IBM.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -38,7 +40,7 @@ public class NetworkException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
-	private NetworkExceptionBean faultInfo;
+	private final NetworkExceptionBean faultInfo;
 	
 	public NetworkException (String msg) {
 		super(msg);
