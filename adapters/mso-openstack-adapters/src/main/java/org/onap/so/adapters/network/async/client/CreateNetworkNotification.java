8bfd669ad19b7a0a3dd6747f12d7ba4f4f469619
Sandeep J <sandeejh@in.ibm.com>
2018-09-04 15:29:13 +0530

    fixed major sonar isue in CreatNetworkNotification
    fixed major sonar issue as detailed in SO-968
    
    Issue-ID: SO-968
    Change-Id: Ib1b846a262e0426db3a0c16b28881befb22d7a52
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- adapters/mso-openstack-adapters/src/main/java/org/onap/so/adapters/network/async/client/CreateNetworkNotification.java
+++ adapters/mso-openstack-adapters/src/main/java/org/onap/so/adapters/network/async/client/CreateNetworkNotification.java
@@ -4,6 +4,8 @@
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Modifications Copyright (C) 2018 IBM.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -346,7 +348,7 @@ public class CreateNetworkNotification {
          */
         public List<CreateNetworkNotification.SubnetIdMap.Entry> getEntry() {
             if (entry == null) {
-                entry = new ArrayList<CreateNetworkNotification.SubnetIdMap.Entry>();
+                entry = new ArrayList<>();
             }
             return this.entry;
         }
