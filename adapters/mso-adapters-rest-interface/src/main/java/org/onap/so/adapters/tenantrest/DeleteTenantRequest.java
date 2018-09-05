6a318fcd6ea52a4741a5decdd0c12903bf2c340a
Sandeep J <sandeejh@in.ibm.com>
2018-09-05 17:26:51 +0530

    fixed sonar issue in DeleteTenantRequest.java
    fixed sonar issue as detailed in SO-979
    
    Issue-ID: SO-979
    Change-Id: I4c0aa6380e734088ba6fb68414d4391f9b6d6344
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- adapters/mso-adapters-rest-interface/src/main/java/org/onap/so/adapters/tenantrest/DeleteTenantRequest.java
+++ adapters/mso-adapters-rest-interface/src/main/java/org/onap/so/adapters/tenantrest/DeleteTenantRequest.java
@@ -4,6 +4,8 @@
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Modifications Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -34,7 +36,9 @@ public class DeleteTenantRequest extends TenantRequestCommon {
 
 	private MsoRequest msoRequest = new MsoRequest();
 
-	public DeleteTenantRequest() {}
+	public DeleteTenantRequest() {
+		//empty constructor
+	}
 	
 	public String getCloudSiteId() {
 		return cloudSiteId;
