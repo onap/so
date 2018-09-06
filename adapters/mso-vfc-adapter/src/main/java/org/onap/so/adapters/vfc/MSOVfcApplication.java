2ea0596cc493541f8b9e27a86bf7f9eba6beb5b8
Sandeep J <sandeejh@in.ibm.com>
2018-09-07 00:26:31 +0530

    fixed sonar issue in MSOVfcApplication.java
    fixed sonar issue as detailed in SO-990
    
    Issue-ID: SO-990
    Change-Id: Ie27a1040d64b53b699a14cae95e5641a70c18ec0
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- adapters/mso-vfc-adapter/src/main/java/org/onap/so/adapters/vfc/MSOVfcApplication.java
+++ adapters/mso-vfc-adapter/src/main/java/org/onap/so/adapters/vfc/MSOVfcApplication.java
@@ -4,6 +4,8 @@
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Modifications Copyright (C) 2018 IBM.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -23,9 +25,6 @@ package org.onap.so.adapters.vfc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 
