f25c614cb273c65433f55e1c99e9dd7c039ea424
Chandan Ghosh <cghosh12@in.ibm.com>
2018-09-12 11:24:48 +0530

    Added log in case of exception in servicepluginfac
    Added error log in servicepluginfactory.java class
    
    Issue-ID: SO-1027
    Change-Id: Ib9ecd078554357b715914e56dc6994f75e59afb6
    Signed-off-by: Chandan Ghosh <cghosh12@in.ibm.com>

--- bpmn/so-bpmn-infrastructure-common/src/main/java/org/onap/so/bpmn/infrastructure/workflow/service/ServicePluginFactory.java
+++ bpmn/so-bpmn-infrastructure-common/src/main/java/org/onap/so/bpmn/infrastructure/workflow/service/ServicePluginFactory.java
@@ -399,7 +399,6 @@ public class ServicePluginFactory {
 			jsonStr = mapper.writeValueAsString(srcObj);
 		} catch (JsonProcessingException e) {
 			LOGGER.debug("SdcToscaParserException", e);
-			e.printStackTrace();
 		}
 		return jsonStr;
 	}
@@ -452,9 +451,9 @@ public class ServicePluginFactory {
 				try {
 					responseContent = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
 				} catch (ParseException e) {
-					e.printStackTrace();
+					LOGGER.debug("ParseException in sendrequest", e);
 				} catch (IOException e) {
-					e.printStackTrace();
+					LOGGER.debug("IOException in sendrequest", e);
 				}
 			}
 			if (null != method) {
