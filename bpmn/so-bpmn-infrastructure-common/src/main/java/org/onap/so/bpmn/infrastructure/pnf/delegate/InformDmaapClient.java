4605db63cab2a9cb6739f89a6307cd86ab39c129
Lukasz Muszkieta <lukasz.muszkieta@nokia.com>
2018-10-04 16:15:13 +0200

    Pnf Spring Environment correction
    Change-Id: Ifd33f4ba0f62b15605ea21eb3d9256c3e70a1d2f
    Issue-ID: SO-1102
    Signed-off-by: Lukasz Muszkieta <lukasz.muszkieta@nokia.com>

--- bpmn/so-bpmn-infrastructure-common/src/main/java/org/onap/so/bpmn/infrastructure/pnf/delegate/InformDmaapClient.java
+++ bpmn/so-bpmn-infrastructure-common/src/main/java/org/onap/so/bpmn/infrastructure/pnf/delegate/InformDmaapClient.java
@@ -25,7 +25,9 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.onap.so.bpmn.infrastructure.pnf.dmaap.DmaapClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component("informDmaapClient")
 public class InformDmaapClient implements JavaDelegate {
 
     private DmaapClient dmaapClient;
