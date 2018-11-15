ddc23f4fcc6e7a7522909b2426a37d194ac4daff
Sandeep J <sandeejh@in.ibm.com>
2018-11-15 20:16:31 +0530

    fixed sonar issue in ContrailSubnetHostRoute.java
    fixed sonar issue as detailed in SO-1217
    
    Issue-ID: SO-1217
    Change-Id: Ifa0e75eef67df10faf3cb7720570167564b99a90
    Signed-off-by: Sandeep J <sandeejh@in.ibm.com>

--- adapters/mso-openstack-adapters/src/main/java/org/onap/so/adapters/network/ContrailSubnetHostRoute.java
+++ adapters/mso-openstack-adapters/src/main/java/org/onap/so/adapters/network/ContrailSubnetHostRoute.java
@@ -4,6 +4,8 @@
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Modifications Copyright (C) 2018 IBM.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -32,6 +34,7 @@ public class ContrailSubnetHostRoute {
 	private String nextHop;
 
 	public ContrailSubnetHostRoute() {
+		//this method does nothing
 	}
 
 	public String getPrefix() {
