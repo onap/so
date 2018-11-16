383f5f079fd59bfa9399563a83ef8a67981125f3
Brittany Plummer (bp896r) <bp896r@att.com>
2018-11-16 08:15:26 -0500

    Added script with table data
    Issue-ID: SO-1224
    Change-Id: I38dfb86ae866aa510e16b95f2c86619bc663fe2f
    Signed-off-by: Brittany Plummer (bp896r) <bp896r@att.com>

---
+++ adapters/mso-catalog-db-adapter/src/main/resources/db/migration/V4.18__ControllerSelectionReferenceData.sql
@@ -0,0 +1,4 @@
+use catalogdb;
+
+INSERT INTO controller_selection_reference (VNF_TYPE, CONTROLLER_NAME, ACTION_CATEGORY) VALUES ('vLoadBalancerMS/vLoadBalancerMS 0', 'APPC', 'HealthCheck');
+INSERT INTO controller_selection_reference (VNF_TYPE, CONTROLLER_NAME, ACTION_CATEGORY) VALUES ('vLoadBalancerMS/vLoadBalancerMS 0', 'APPC', 'ConfigScaleOut');
