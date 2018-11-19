ffce4c64a808efa9f979aadac578f2dad511195e
subhash kumar singh <subhash.kumar.singh@huawei.com>
2018-11-19 16:39:09 +0530

    Fix namespace for requestdb update
    Fix namespace for requestdb update.
    
    Change-Id: Ib13384e669100ff6db837819a1ba025300d66f0c
    Issue-ID: SO-689
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

--- bpmn/so-bpmn-infrastructure-common/src/main/groovy/org/onap/so/bpmn/infrastructure/scripts/Delete3rdONAPE2EServiceInstance.groovy
+++ bpmn/so-bpmn-infrastructure-common/src/main/groovy/org/onap/so/bpmn/infrastructure/scripts/Delete3rdONAPE2EServiceInstance.groovy
@@ -184,7 +184,7 @@ public class Delete3rdONAPE2EServiceInstance extends AbstractServiceTaskProcesso
 
 		String body = """
                 <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
-                        xmlns:ns="http://org.openecomp.mso/requestsdb">
+                        xmlns:ns="http://org.onap.so/requestsdb">
                         <soapenv:Header/>
                 <soapenv:Body>
                     <ns:updateResourceOperationStatus>
