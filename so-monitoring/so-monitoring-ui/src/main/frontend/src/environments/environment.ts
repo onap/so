738dcb656042681d6b552000103a7541cc6c3c93
eHanan <eoin.hanan@ericsson.com>
2018-11-20 16:31:28 +0000

    Changed nodeport from 9091 to 30224
    Change-Id: I8305c88572611e0283e699fce62a415ae7586ffe
    Issue-ID: SO-1164
    Signed-off-by: eHanan <eoin.hanan@ericsson.com>

--- so-monitoring/so-monitoring-ui/src/main/frontend/src/environments/environment.ts
+++ so-monitoring/so-monitoring-ui/src/main/frontend/src/environments/environment.ts
@@ -24,5 +24,5 @@ SPDX-License-Identifier: Apache-2.0
 export const environment = {
   production: false,
 
-  soMonitoringBackendURL: 'http://so-monitoring:9091/so/monitoring/'
+  soMonitoringBackendURL: 'http://so-monitoring:30224/so/monitoring/'
 };
