e10ca0ba99bb5bbdc10f095aae8933e3a5e61112
Yulian Han <elaine.hanyulian@huawei.com>
2018-11-21 15:51:18 +0800

    insert E2E svc progress into db bug fix
    Change-Id: Idaa55084f5ecb0dd3636c232cebc14fa5f064409
    Issue-ID: SO-1235
    Signed-off-by: Yulian Han <elaine.hanyulian@huawei.com>

--- adapters/mso-requests-db-adapter/src/main/java/org/onap/so/adapters/requestsdb/MsoRequestsDbAdapterImpl.java
+++ adapters/mso-requests-db-adapter/src/main/java/org/onap/so/adapters/requestsdb/MsoRequestsDbAdapterImpl.java
@@ -201,7 +201,7 @@ public class MsoRequestsDbAdapterImpl implements MsoRequestsDbAdapter {
 			String error = "Entity not found. Unable to retrieve OperationStatus Object ServiceId: " + serviceId + " operationId: "
 					+ operationId;
 			logger.error(error);
-			throw new MsoRequestsDbException(error,MsoLogger.ErrorCode.BusinessProcesssError);
+//			throw new MsoRequestsDbException(error,MsoLogger.ErrorCode.BusinessProcesssError);
 		}
 
 		operStatus.setUserId(userId);
