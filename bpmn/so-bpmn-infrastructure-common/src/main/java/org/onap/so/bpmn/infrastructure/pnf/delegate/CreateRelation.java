0c3dd6e321e0ccc5dd4548f621a80b50869242d5
Lukasz Muszkieta <lukasz.muszkieta@nokia.com>
2019-01-16 11:29:46 +0100

    Associate PNF instance with Service
    Change-Id: I19e25e0daae042b30138bbfc074b0e651b8b1c01
    Issue-ID: SO-1274
    Signed-off-by: Lukasz Muszkieta <lukasz.muszkieta@nokia.com>

---
+++ bpmn/so-bpmn-infrastructure-common/src/main/java/org/onap/so/bpmn/infrastructure/pnf/delegate/CreateRelation.java
@@ -0,0 +1,67 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Modifications Copyright 2019 Nokia
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.bpmn.infrastructure.pnf.delegate;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.onap.so.bpmn.common.scripts.ExceptionUtil;
+import org.onap.so.bpmn.infrastructure.pnf.implementation.AaiConnection;
+import org.onap.so.client.aai.AAIObjectType;
+import org.onap.so.client.aai.entities.uri.AAIResourceUri;
+import org.onap.so.client.aai.entities.uri.AAIUriFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateRelation implements JavaDelegate {
+
+    private static final Logger logger = LoggerFactory.getLogger(CreateRelation.class);
+
+    private AaiConnection aaiConnectionImpl;
+
+    @Autowired
+    public CreateRelation(AaiConnection aaiConnectionImpl) {
+        this.aaiConnectionImpl = aaiConnectionImpl;
+    }
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) {
+        String serviceInstanceId = (String) delegateExecution.getVariable("serviceInstanceId");
+        String pnfName = (String) delegateExecution.getVariable("correlationId");
+        AAIResourceUri serviceInstanceURI = AAIUriFactory.createResourceUri(AAIObjectType.SERVICE_INSTANCE,
+                serviceInstanceId);
+        AAIResourceUri pnfUri = AAIUriFactory
+                .createResourceUri(AAIObjectType.PNF, pnfName);
+        try {
+            aaiConnectionImpl.createRelation(serviceInstanceURI, pnfUri);
+        } catch (Exception e) {
+            new ExceptionUtil().buildAndThrowWorkflowException(delegateExecution, 9999,
+                    "An exception occurred when making service and pnf relation. Exception: "+ e.getMessage());
+        }
+        logger.debug("The relation has been made between service with id: {} and pnf with name: {}",
+                serviceInstanceId, pnfName);
+    }
+
+}
