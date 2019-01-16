3c2d50c011b3a48e3e40daa31dedf10f26757b93
Shubhada Ramkrishna Vaze <SV00449682@TechMahindra.com>
2019-01-16 15:04:13 +0530

    Either log or rethrow this exception.
    Location:DistributionClientEmulator.java
    
    Change-Id: I38b1b3fed9db0d24a6045352127e48b98cce6b63
    Issue-ID: SO-1397
    Signed-off-by: Shubhada Ramkrishna Vaze <SV00449682@TechMahindra.com>

--- asdc-controller/src/main/java/org/onap/so/asdc/client/test/emulators/DistributionClientEmulator.java
+++ asdc-controller/src/main/java/org/onap/so/asdc/client/test/emulators/DistributionClientEmulator.java
@@ -90,8 +90,9 @@ public class DistributionClientEmulator implements IDistributionClient {
 		byte[] inputStream=null;
 		try {
 			inputStream = getData(filename);
-		} catch (IOException e) {			
-			logger.debug("InputStream is NULL for:"+ resourcePath + filename);
+		} catch (IOException e) {	
+			e.printStackTrace();
+			logger.error("InputStream is NULL for:"+ resourcePath + filename);
 		}		
 	
 		return new DistributionClientDownloadResultImpl(DistributionActionResultEnum.SUCCESS, DistributionActionResultEnum.SUCCESS.name(),arg0.getArtifactName(),inputStream);		
