b4d2686b364d60cb95f1ef0c7996617dbacec13a
subhash kumar singh <subhash.kumar.singh@huawei.com>
2019-01-17 14:28:43 +0530

    Change size for resource input
    Change size for resource input.
    
    Change-Id: Ic6856e6baa2623867d0ae688c98ed24dbb245036
    Issue-ID: SO-1400
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

--- adapters/mso-catalog-db-adapter/src/main/resources/db/migration/V4.22__AddResourceInput.sql
+++ adapters/mso-catalog-db-adapter/src/main/resources/db/migration/V4.22__AddResourceInput.sql
@@ -4,10 +4,10 @@ ALTER TABLE service
 ADD RESOURCE_ORDER varchar(255);
 
 ALTER TABLE vnf_resource_customization
-ADD RESOURCE_INPUT varchar(2000);
+ADD RESOURCE_INPUT varchar(20000);
 
 ALTER TABLE network_resource_customization
-ADD RESOURCE_INPUT varchar(2000);
+ADD RESOURCE_INPUT varchar(20000);
 
 ALTER TABLE allotted_resource_customization
-ADD RESOURCE_INPUT varchar(2000);
+ADD RESOURCE_INPUT varchar(20000);
