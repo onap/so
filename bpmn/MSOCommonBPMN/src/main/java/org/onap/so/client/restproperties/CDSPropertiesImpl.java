f88b17705190f4ad7efebbaa59d3cecf82f7514a
Alexis de Talhouët <adetalhouet89@gmail.com>
2019-02-26 11:26:03 -0500

    Add CDS client
    Change-Id: I3f77a2c9e8341239b97675f5897cecf28c7dfb6f
    Issue-ID: SO-1483
    Signed-off-by: Alexis de Talhouët <adetalhouet89@gmail.com>

---
+++ bpmn/MSOCommonBPMN/src/main/java/org/onap/so/client/restproperties/CDSPropertiesImpl.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2019 Bell Canada.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.so.client.restproperties;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Objects;
+import org.onap.so.bpmn.core.UrnPropertiesReader;
+import org.onap.so.client.cds.CDSProperties;
+
+public class CDSPropertiesImpl implements CDSProperties {
+
+    private static final String ENDPOINT = "cds.endpoint";
+    private static final String PORT = "cds.port";
+
+    public CDSPropertiesImpl() {
+        // NEEDED for service loader
+    }
+
+    @Override
+    public URL getEndpoint() {
+        try {
+            return new URL(Objects.requireNonNull(UrnPropertiesReader.getVariable(ENDPOINT)));
+        } catch (MalformedURLException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public String getSystemName() {
+        return "MSO";
+    }
+
+    @Override
+    public int getPort() {
+        return Integer.parseInt(Objects.requireNonNull(UrnPropertiesReader.getVariable(PORT)));
+    }
+}
