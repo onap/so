d06d2ffb9f555485bb3fa2839e03d02f7d8c127e
Benjamin, Max (mb388a) <mb388a@us.att.com>
2019-03-04 09:43:34 -0500

    flow validators can be skipped via an annotation
    flow validators can now be skipped via an annotation
    
    Change-Id: I7796d5829aacee466419d558dfc95a761125577a
    Issue-ID: SO-1589
    Signed-off-by: Benjamin, Max (mb388a) <mb388a@us.att.com>

---
+++ bpmn/MSOCommonBPMN/src/main/java/org/onap/so/bpmn/common/validation/Skip.java
@@ -0,0 +1,9 @@
+package org.onap.so.bpmn.common.validation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Skip {
+
+}
