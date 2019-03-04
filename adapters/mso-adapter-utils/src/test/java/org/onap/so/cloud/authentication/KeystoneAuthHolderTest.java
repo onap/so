437e5cb32ce5ca85c617c73ce9f9b2336ddc4a5a
Sudarshan Kumar <sudars19@in.ibm.com>
2019-03-04 17:21:45 +0530

    Junit  added for KeystoneAuthHolder.java
    Junit  added for KeystoneAuthHolder.java
    
    Issue-ID: SO-1576
    Change-Id: Ib313d6a3aa01da3fc7292b2cae4ba7b0a03ebce6
    Signed-off-by: Sudarshan Kumar <sudars19@in.ibm.com>

---
+++ adapters/mso-adapter-utils/src/test/java/org/onap/so/cloud/authentication/KeystoneAuthHolderTest.java
@@ -0,0 +1,59 @@
+/*
+ * ============LICENSE_START==========================================
+ *  org.onap.music
+ * ===================================================================
+ *  Copyright (c) 2019 IBM.
+ * ===================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * 
+ * ============LICENSE_END=============================================
+ * ====================================================================
+ */
+package org.onap.so.cloud.authentication;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Calendar;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class KeystoneAuthHolderTest {
+
+    private KeystoneAuthHolder keystoneAuthHolder;
+    private Calendar calendar = Calendar.getInstance();
+
+    @Before
+    public void setUp() {
+        keystoneAuthHolder = new KeystoneAuthHolder();
+    }
+
+    @Test
+    public void testGetId() {
+        keystoneAuthHolder.setId("001");
+        assertEquals("001", keystoneAuthHolder.getId());
+    }
+
+    @Test
+    public void testGetexpiration() {
+        keystoneAuthHolder.setexpiration(calendar);
+        assertEquals(calendar, keystoneAuthHolder.getexpiration());
+    }
+
+    @Test
+    public void testGetServiceUrl() {
+        keystoneAuthHolder.setHeatUrl("testURL");
+        assertEquals("testURL", keystoneAuthHolder.getServiceUrl());
+    }
+
+}
