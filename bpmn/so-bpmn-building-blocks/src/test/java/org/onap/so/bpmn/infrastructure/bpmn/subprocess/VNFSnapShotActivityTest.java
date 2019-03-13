0fd928074d9121bbde367475f0fd4019a2dd2258
Elena Kuleshov <evn@att.com>
2019-03-13 13:26:54 -0400

    Add APPC-related Activities for WFD
    Add APP-C related Activities for WFD with JUnits
    
    Change-Id: I1a7142f692945843e7942b43b6fe68e23ea21e96
    Issue-ID: SO-1518
    Signed-off-by: Kuleshov, Elena <evn@att.com>

---
+++ bpmn/so-bpmn-building-blocks/src/test/java/org/onap/so/bpmn/infrastructure/bpmn/subprocess/VNFSnapShotActivityTest.java
@@ -0,0 +1,56 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2019 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.bpmn.infrastructure.bpmn.subprocess;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.junit.Test;
+import org.onap.so.bpmn.common.BuildingBlockExecution;
+import org.onap.so.bpmn.BaseBPMNTest;
+import org.onap.appc.client.lcm.model.Action;
+
+public class VNFSnapShotActivityTest extends BaseBPMNTest{
+	@Test
+	public void sunnyDayVNFSnapShotActivity_Test() throws InterruptedException {
+		ProcessInstance pi = runtimeService.startProcessInstanceByKey("VNFSnapShotActivity", variables);
+		assertThat(pi).isNotNull();
+		assertThat(pi).isStarted().hasPassedInOrder("VNFSnapShotActivity_Start",
+				"TaskPreProcessActivity",
+				"TaskSnapShot",								   
+				"VNFSnapShotActivity_End");
+		assertThat(pi).isEnded();
+	}
+	
+	@Test	
+	public void rainyDayVNFSnapShotActivity_Test() throws Exception {
+		doThrow(new BpmnError("7000", "TESTING ERRORS")).when(appcRunTasks)
+				.runAppcCommand(any(BuildingBlockExecution.class), any(Action.class));
+		ProcessInstance pi = runtimeService.startProcessInstanceByKey("VNFSnapShotActivity", variables);
+		assertThat(pi).isNotNull().isStarted().hasPassedInOrder("VNFSnapShotActivity_Start",
+				"TaskPreProcessActivity",
+				"TaskSnapShot").hasNotPassed(								   
+				"VNFSnapShotActivity_End");		
+	}
+	
+}
---
+++ bpmn/so-bpmn-building-blocks/src/test/java/org/onap/so/bpmn/infrastructure/bpmn/subprocess/VNFStartActivityTest.java
@@ -0,0 +1,56 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.bpmn.infrastructure.bpmn.subprocess;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.junit.Test;
+import org.onap.so.bpmn.common.BuildingBlockExecution;
+import org.onap.so.bpmn.BaseBPMNTest;
+import org.onap.appc.client.lcm.model.Action;
+
+public class VNFStartActivityTest extends BaseBPMNTest{
+	@Test
+	public void sunnyDayVNFStartActivity_Test() throws InterruptedException {
+		ProcessInstance pi = runtimeService.startProcessInstanceByKey("VNFStartActivity", variables);
+		assertThat(pi).isNotNull();
+		assertThat(pi).isStarted().hasPassedInOrder("VNFStartActivity_Start",
+				"TaskPreProcessActivity",
+				"TaskStart",								   
+				"VNFStartActivity_End");
+		assertThat(pi).isEnded();
+	}
+	
+	@Test	
+	public void rainyDayVNFStartActivity_Test() throws Exception {
+		doThrow(new BpmnError("7000", "TESTING ERRORS")).when(appcRunTasks)
+				.runAppcCommand(any(BuildingBlockExecution.class), any(Action.class));
+		ProcessInstance pi = runtimeService.startProcessInstanceByKey("VNFStartActivity", variables);
+		assertThat(pi).isNotNull().isStarted().hasPassedInOrder("VNFStartActivity_Start",
+				"TaskPreProcessActivity",
+				"TaskStart").hasNotPassed(								   
+				"VNFStartActivity_End");		
+	}
+	
+}
