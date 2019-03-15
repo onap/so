324ff751baa40c1cc416c0b62744f219c4a9b7e1
Bonkur, Venkat (vb8416) <vb8416@att.com>
2019-03-15 16:16:21 -0400

    Add VnfInPlaceSoftwareUpdate for WFD
    Added  CheckPserversLocked,SetClosedLoopDisabled,UnsetClosedLoopDisabled, CheckClosedLoopDisabled, CheckInMain
    
    Change-Id: I6405b2c88d2109d952d452648bef2f99be6993df
    Issue-ID: SO-1518
    Signed-off-by: Bonkur, Venkat (vb8416) <vb8416@att.com>

--- bpmn/so-bpmn-tasks/src/main/java/org/onap/so/bpmn/infrastructure/aai/tasks/AAIFlagTasks.java
+++ bpmn/so-bpmn-tasks/src/main/java/org/onap/so/bpmn/infrastructure/aai/tasks/AAIFlagTasks.java
@@ -73,4 +73,45 @@ public class AAIFlagTasks {
 		}
 	}	
 	
+	public void checkVnfClosedLoopDisabledFlag(BuildingBlockExecution execution) {
+		boolean isClosedLoopDisabled = false;
+		try {
+			GenericVnf vnf = extractPojosForBB.extractByKey(execution, ResourceKey.GENERIC_VNF_ID, execution.getLookupMap().get(ResourceKey.GENERIC_VNF_ID));
+			String vnfId = vnf.getVnfId();
+			isClosedLoopDisabled = aaiVnfResources.checkVnfClosedLoopDisabledFlag(vnfId);
+		} catch (Exception ex) {
+			exceptionUtil.buildAndThrowWorkflowException(execution, 7000, ex);
+		}
+		if (isClosedLoopDisabled) {
+			exceptionUtil.buildAndThrowWorkflowException(execution, 7000, "VNF Close Loop Disabled in A&AI");
+		}
+	}	
+		
+	public void modifyVnfClosedLoopDisabledFlag(BuildingBlockExecution execution, boolean closedLoopDisabled) {
+		try {
+			GenericVnf genericVnf = extractPojosForBB.extractByKey(execution, ResourceKey.GENERIC_VNF_ID, execution.getLookupMap().get(ResourceKey.GENERIC_VNF_ID));
+
+			GenericVnf copiedGenericVnf = genericVnf.shallowCopyId();
+			copiedGenericVnf.setClosedLoopDisabled(closedLoopDisabled);
+			genericVnf.setClosedLoopDisabled(closedLoopDisabled);
+
+			aaiVnfResources.updateObjectVnf(copiedGenericVnf);
+		} catch(Exception ex) {
+			exceptionUtil.buildAndThrowWorkflowException(execution, 7000, ex);
+		}
+	}	
+	
+	public void checkVnfPserversLockedFlag(BuildingBlockExecution execution) {
+		boolean inPserversLocked = false;
+		try {
+			GenericVnf vnf = extractPojosForBB.extractByKey(execution, ResourceKey.GENERIC_VNF_ID, execution.getLookupMap().get(ResourceKey.GENERIC_VNF_ID));
+			String vnfId = vnf.getVnfId();
+			inPserversLocked = aaiVnfResources.checkVnfPserversLockedFlag(vnfId);
+		} catch (Exception ex) {
+			exceptionUtil.buildAndThrowWorkflowException(execution, 7000, ex);
+		}
+		if (inPserversLocked) {
+			exceptionUtil.buildAndThrowWorkflowException(execution, 7000, "VNF PServers in Locked in A&AI");
+		}
+	}	
 }
