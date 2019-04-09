b67bc0a413783ce9bbe607ded701d73a7c6731fc
Bonkur, Venkat (vb8416) <vb8416@att.com>
2019-04-09 00:41:32 -0400

    Add SO Populate Activity BB Data in Catalog DB
    Added  SO Populate Activity BB Data in Catalog DB. Make the ID columns
    auto increment activity_spec_categories, activity_spec_to_activity_spec_categories
    Issue-ID: SO-1724
    Signed-off-by: Bonkur, Venkat (vb8416) <vb8416@att.com>
    Change-Id: Ic59727cd8aa13133d6b5ebde98a249cda180cc9d

--- adapters/mso-catalog-db-adapter/src/main/resources/db/migration/R__MacroData.sql
+++ adapters/mso-catalog-db-adapter/src/main/resources/db/migration/R__MacroData.sql
@@ -773,3 +773,30 @@ INSERT INTO rainy_day_handler_macro (FLOW_NAME, SERVICE_TYPE, VNF_TYPE, ERROR_CO
 VALUES
 ('GenericVnfHealthCheckBB', '*', '*', '*', "*" , 'Retry'),
 ('ConfigurationScaleOutBB', '*', '*', '*', "*" , 'Retry');
+
+
+DELETE FROM activity_spec_to_activity_spec_parameters;
+DELETE FROM activity_spec_to_activity_spec_categories;
+DELETE FROM activity_spec;
+DELETE FROM activity_spec_categories;
+DELETE FROM activity_spec_parameters;
+
+INSERT INTO activity_spec (NAME, DESCRIPTION, VERSION) 
+VALUES ('VNFSetInMaintFlagActivity','Activity to Set InMaint Flag in A&AI',1.0);
+
+INSERT INTO activity_spec_categories (NAME)
+VALUES ('VNF');
+
+INSERT INTO activity_spec_parameters (NAME, TYPE, DIRECTION, DESCRIPTION) 
+VALUES('WorkflowException','WorkflowException','outputParameters','Description');
+
+INSERT INTO activity_spec_to_activity_spec_categories(ACTIVITY_SPEC_ID, ACTIVITY_SPEC_CATEGORIES_ID) 
+VALUES
+(
+(select ID from ACTIVITY_SPEC where NAME='VNFSetInMaintFlagActivity' and VERSION=1.0),
+(select ID from activity_spec_categories where NAME='VNF'));
+
+INSERT INTO activity_spec_to_activity_spec_parameters( ACTIVITY_SPEC_ID, ACTIVITY_SPEC_PARAMETERS_ID) 
+VALUES(
+(select ID from ACTIVITY_SPEC where NAME='VNFSetInMaintFlagActivity' and VERSION=1.0),
+(select ID from activity_spec_parameters where NAME='WorkflowException' and DIRECTION='outputParameters'));
