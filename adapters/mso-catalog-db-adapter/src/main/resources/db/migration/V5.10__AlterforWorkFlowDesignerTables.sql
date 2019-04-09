d15557dbc49107d7113d38921d7cdc1df75f0066
Bonkur, Venkat (vb8416) <vb8416@att.com>
2019-04-09 00:41:32 -0400

    Add SO Populate Activity BB Data in Catalog DB
    Added  SO Populate Activity BB Data in Catalog DB. Make the ID columns
    auto increment activity_spec_categories, activity_spec_to_activity_spec_categories
    Issue-ID: SO-1724
    Signed-off-by: Bonkur, Venkat (vb8416) <vb8416@att.com>
    Change-Id: Ic59727cd8aa13133d6b5ebde98a249cda180cc9d

---
+++ adapters/mso-catalog-db-adapter/src/main/resources/db/migration/V5.10__AlterforWorkFlowDesignerTables.sql
@@ -0,0 +1,14 @@
+use catalogdb;
+
+ALTER TABLE activity_spec_to_activity_spec_categories DROP FOREIGN KEY fk_activity_spec_to_activity_spec_categories__activity_spec_c1;
+
+ALTER TABLE activity_spec_categories modify id INT(11) not null auto_increment;
+
+ALTER TABLE activity_spec_to_activity_spec_categories ADD 
+ CONSTRAINT `fk_activity_spec_to_activity_spec_categories__activity_spec_c1` 
+    FOREIGN KEY (`ACTIVITY_SPEC_CATEGORIES_ID`) 
+    REFERENCES `activity_spec_categories` (`ID`) 
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE; 
+    
+ALTER TABLE activity_spec_to_activity_spec_categories modify id INT(11) not null auto_increment; 
