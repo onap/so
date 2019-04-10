c523be8011adad928b9e1f0a2e68a9b45fd66b58
r.bogacki <r.bogacki@samsung.com>
2019-04-10 08:33:09 +0200

    Fixed latest Sonar blocker issues
    Fixed blockers according to Sonar analysis.
    
    Change-Id: I2c2d053b66019b74ee20b0a56fb7b93bf931bb61
    Issue-ID: SO-1734
    Signed-off-by: Robert Bogacki <r.bogacki@samsung.com>

--- vnfm-simulator/vnfm-service/src/main/java/org/onap/svnfm/simulator/notifications/VnfmAdapterDeletionNotification.java
+++ vnfm-simulator/vnfm-service/src/main/java/org/onap/svnfm/simulator/notifications/VnfmAdapterDeletionNotification.java
@@ -2,6 +2,8 @@
  * ============LICENSE_START=======================================================
  *  Copyright (C) 2019 Nordix Foundation.
  * ================================================================================
+ *  Modifications Copyright (c) 2019 Samsung
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -33,6 +35,7 @@ public class VnfmAdapterDeletionNotification implements Runnable {
             Thread.sleep(10000);
         } catch (final InterruptedException e) {
             e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
         System.out.println("Call to VNFM Adapter-Delete");
     }
