9f2c9ffcea16a8157a4637420b99277bccf3297b
Elena Kuleshov <evn@att.com>
2019-04-10 09:26:02 -0400

    Deploy activities to SDC from Catalog
    Deploy activities to SDC from Catalog
    
    Change-Id: I1c91b79a060889f14cf0b653b3ed1a60e934c8bf
    Issue-ID: SO-1725
    Signed-off-by: Kuleshov, Elena <evn@att.com>

--- asdc-controller/src/test/resources/data.sql
+++ asdc-controller/src/test/resources/data.sql
@@ -62,3 +62,21 @@ insert into requestdb.watchdog_per_component_distribution_status(DISTRIBUTION_ID
 insert into requestdb.watchdog_service_mod_ver_id_lookup(DISTRIBUTION_ID, SERVICE_MODEL_VERSION_ID, DISTRIBUTION_NOTIFICATION, CONSUMER_ID) values
 ('watchdogTestStatusSuccess', '5df8b6de-2083-11e7-93ae-92361f002671', NULL, NULL),
 ('watchdogTestStatusNull', '00000000-0000-0000-0000-000000000000', NULL, NULL);
+
+INSERT INTO activity_spec (NAME, DESCRIPTION, VERSION) 
+VALUES ('VNFSetInMaintFlagActivity','Activity to Set InMaint Flag in A&AI',1.0);
+
+INSERT INTO activity_spec_categories (NAME)
+VALUES ('VNF');
+
+INSERT INTO activity_spec_parameters (NAME, TYPE, DIRECTION, DESCRIPTION) 
+VALUES('WorkflowException','WorkflowException','outputParameters','Description');
+
+INSERT INTO activity_spec_to_activity_spec_categories(ACTIVITY_SPEC_ID, ACTIVITY_SPEC_CATEGORIES_ID) 
+VALUES
+(
+(select ID from ACTIVITY_SPEC where NAME='VNFSetInMaintFlagActivity' and VERSION=1.0),
+(select ID from activity_spec_categories where NAME='VNF'));
+
+INSERT INTO activity_spec_to_activity_spec_parameters( ACTIVITY_SPEC_ID, ACTIVITY_SPEC_PARAMETERS_ID) VALUES( (select ID from ACTIVITY_SPEC where NAME='VNFSetInMaintFlagActivity' and VERSION=1.0),
+(select ID from activity_spec_parameters where NAME='WorkflowException' and DIRECTION='outputParameters'));
