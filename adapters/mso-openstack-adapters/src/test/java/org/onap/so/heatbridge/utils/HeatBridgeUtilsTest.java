ae9b288acb55589259f8f5ab4fda85a40c752f96
Lukasz Muszkieta <lukasz.muszkieta@nokia.com>
2019-04-15 14:08:38 +0200

    add junit coverage for HeatBridgeUtils
    Change-Id: Ibea7e60ac8e8b511e2a6a6ab54b2072e7e8d4008
    Issue-ID: SO-414
    Signed-off-by: Lukasz Muszkieta <lukasz.muszkieta@nokia.com>

---
+++ adapters/mso-openstack-adapters/src/test/java/org/onap/so/heatbridge/utils/HeatBridgeUtilsTest.java
@@ -0,0 +1,32 @@
+package org.onap.so.heatbridge.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import org.junit.Test;
+
+public class HeatBridgeUtilsTest {
+
+    @Test(expected = IllegalStateException.class)
+    public void matchServerName_canNotBeNull() {
+        HeatBridgeUtils.getMatchingPserverPifName(null);
+    }
+
+    @Test
+    public void matchServerName_isDedicated() {
+        Optional<String> serverName = HeatBridgeUtils.getMatchingPserverPifName("dedicated-testServer");
+        assertThat(serverName).isNotEmpty().hasValue("sriov-d-testServer");
+    }
+
+    @Test
+    public void matchServerName_isShared() {
+        Optional<String> serverName = HeatBridgeUtils.getMatchingPserverPifName("shared-testServer");
+        assertThat(serverName).isNotEmpty().hasValue("sriov-s-testServer");
+    }
+
+    @Test
+    public void matchServerName_unknown() {
+        Optional<String> serverName = HeatBridgeUtils.getMatchingPserverPifName("differentServerName");
+        assertThat(serverName).isEmpty();
+    }
+}
