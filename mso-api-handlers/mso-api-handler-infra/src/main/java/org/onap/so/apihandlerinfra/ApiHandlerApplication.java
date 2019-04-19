522c6fc381d59bf8026468695117db9291636ea2
Isaac Manuel Raj <isaac.manuelraj@huawei.com>
2019-04-19 11:39:35 +0530

    Remove unused code or class import.
    Remove unused code or class import.
    
    Issue-ID: SO-1490
    
    Change-Id: I2d7bdf82b47181c2a77029c9a17fc49c4a0aff18
    Signed-off-by: Isaac Manuel Raj <isaac.manuelraj@huawei.com>

--- mso-api-handlers/mso-api-handler-infra/src/main/java/org/onap/so/apihandlerinfra/ApiHandlerApplication.java
+++ mso-api-handlers/mso-api-handler-infra/src/main/java/org/onap/so/apihandlerinfra/ApiHandlerApplication.java
@@ -21,7 +21,7 @@
 package org.onap.so.apihandlerinfra;
 
 import java.util.concurrent.Executor;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
