eb183afca8c959d2ed855ba3323e2bba92ca37ee
Isaac Manuel Raj <isaac.manuelraj@huawei.com>
2019-04-19 12:44:54 +0530

    Remove hard coded string litrals and making it as final
    Remove hard coded string litrals and making it as final
    
    Issue-ID: SO-1490
    
    Change-Id: Icbee0bd15fda83d28db182a5b2cc079ccf779ad1
    Signed-off-by: Isaac Manuel Raj <isaac.manuelraj@huawei.com>

--- mso-api-handlers/mso-api-handler-infra/src/main/java/org/onap/so/apihandlerinfra/RequestActionMap.java
+++ mso-api-handlers/mso-api-handler-infra/src/main/java/org/onap/so/apihandlerinfra/RequestActionMap.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,22 +28,27 @@ import java.util.Map;
  */
 public class RequestActionMap {
     private static final Map<String, String> actionMap;
+    private static final String CREATE_INSTANCE = "createInstance";
+    private static final String DELETE_INSTANCE = "deleteInstance";
+    private static final String UPDATE_INSTANCE = "updateInstance";
+    private static final String REPLACE_INSTANCE = "replaceInstance";
+
 
     static {
         actionMap = new HashMap<>();
-        actionMap.put("CREATE_VF_MODULE", "createInstance");
-        actionMap.put("DELETE_VF_MODULE", "deleteInstance");
-        actionMap.put("UPDATE_VF_MODULE", "updateInstance");
-        actionMap.put("CREATE_VF_MODULE_VOL", "createInstance");
-        actionMap.put("DELETE_VF_MODULE_VOL", "deleteInstance");
-        actionMap.put("UPDATE_VF_MODULE_VOL", "updateInstance");
-        actionMap.put("CREATE", "createInstance");
-        actionMap.put("DELETE", "deleteInstance");
-        actionMap.put("UPDATE", "updateInstance");
-        actionMap.put("createInstance", "createInstance");
-        actionMap.put("deleteInstance", "deleteInstance");
-        actionMap.put("updateInstance", "updateInstance");
-        actionMap.put("replaceInstance", "replaceInstance");
+        actionMap.put("CREATE_VF_MODULE", CREATE_INSTANCE);
+        actionMap.put("DELETE_VF_MODULE", DELETE_INSTANCE);
+        actionMap.put("UPDATE_VF_MODULE", UPDATE_INSTANCE);
+        actionMap.put("CREATE_VF_MODULE_VOL", CREATE_INSTANCE);
+        actionMap.put("DELETE_VF_MODULE_VOL", DELETE_INSTANCE);
+        actionMap.put("UPDATE_VF_MODULE_VOL", UPDATE_INSTANCE);
+        actionMap.put("CREATE", CREATE_INSTANCE);
+        actionMap.put("DELETE", DELETE_INSTANCE);
+        actionMap.put("UPDATE", UPDATE_INSTANCE);
+        actionMap.put("createInstance", CREATE_INSTANCE);
+        actionMap.put("deleteInstance", DELETE_INSTANCE);
+        actionMap.put("updateInstance", UPDATE_INSTANCE);
+        actionMap.put("replaceInstance", REPLACE_INSTANCE);
 
     }
 
