1f8e0230da3c2a38472f994da7329e1ecc9c3f4d
subhash kumar singh <subhash.kumar.singh@huawei.com>
2019-04-20 15:41:18 +0530

    WIP: Enhancement for query
    Enhancemenv for query to include group and vnfccustomization.
    
    {
        "serviceResources": {
            "modelInfo": {
                "modelName": "NSService",
                "modelUuid": "0bad8c92-7d22-49f0-b092-b64e6ca564a7",
                "modelInvariantUuid": "69161960-515b-4bf3-91f1-313b813f5e1d",
                "modelVersion": "1.0"
            },
            "serviceType": "",
            "serviceRole": "",
            "environmentContext": "General_Revenue-Bearing",
            "resourceOrder": "NF",
            "workloadContext": "Production",
            "serviceVnfs": [
                {
                    "modelInfo": {
                        "modelName": "",
                        "modelUuid": "123",
                        "modelInvariantUuid": "",
                        "modelVersion": "",
                        "modelCustomizationUuid": "1234",
                        "modelInstanceName": "test"
                    },
                    "toscaNodeType": "",
                    "nfFunction": "",
                    "nfType": "",
                    "nfRole": "",
                    "nfNamingCode": "",
                    "multiStageDesign": "",
                    "resourceInput": "",
                    "vfModules": [],
                    "groups": [
                        {
                            "modelInfo": {
                                "modelName": "test",
                                "modelUuid": "11",
                                "modelInvariantUuid": "11",
                                "modelVersion": "2"
                            },
                            "vnfcs": [
                                {
                                    "modelInfo": {
                                        "modelName": "xfs",
                                        "modelUuid": "22",
                                        "modelInvariantUuid": "2222",
                                        "modelVersion": "22222",
                                        "modelCustomizationUuid": "2222"
                                    }
                                }
                            ]
                        }
                    ]
                }
            ],
            "serviceNetworks": [],
            "serviceAllottedResources": []
        }
    }
    
    Issue-ID: SO-1393
    Change-Id: Iee7f79337f73276f857d6d42b5b2416e4e8f4536
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

---
+++ adapters/mso-catalog-db-adapter/src/test/java/org/onap/so/adapters/catalogdb/catalogrest/QueryVnfcsTest.java
@@ -0,0 +1,46 @@
+package org.onap.so.adapters.catalogdb.catalogrest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import org.onap.so.db.catalog.beans.VnfcCustomization;
+import org.onap.so.jsonpath.JsonPathUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QueryVnfcsTest {
+
+    @Test
+    public void convertToJson_successful() {
+        QueryVnfcs queryVnfcs = new QueryVnfcs(createList());
+        String jsonResult = queryVnfcs.JSON2(true, false);
+        System.out.println(jsonResult);
+        assertThat(JsonPathUtil.getInstance().locateResult(jsonResult, "$.vnfcs[0].modelInfo.modelName"))
+                .contains("model1");
+        assertThat(JsonPathUtil.getInstance().locateResult(jsonResult, "$.vnfcs[1].modelInfo.modelName"))
+                .contains("model2");
+
+    }
+
+    private List<VnfcCustomization> createList() {
+        List<VnfcCustomization> customizations = new ArrayList();
+
+        VnfcCustomization c1 = new VnfcCustomization();
+        c1.setModelName("model1");
+        c1.setModelUUID("uuid1");
+        c1.setModelInvariantUUID("inv1");
+        c1.setModelVersion("v1");
+        c1.setModelCustomizationUUID("cust1");
+
+        VnfcCustomization c2 = new VnfcCustomization();
+        c2.setModelName("model2");
+        c2.setModelUUID("uuid2");
+        c2.setModelInvariantUUID("inv2");
+        c2.setModelVersion("v2");
+        c2.setModelCustomizationUUID("cust2");
+
+        customizations.add(c1);
+        customizations.add(c2);
+        return customizations;
+    }
+}
