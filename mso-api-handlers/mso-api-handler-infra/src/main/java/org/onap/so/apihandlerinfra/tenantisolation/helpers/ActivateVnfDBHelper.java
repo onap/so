80462c509ef9ce3241b4cdf3e7f8fe5f6ab74974
Subhosree Pan <subhosree.pan1@huawei.com>
2019-04-25 09:44:50 +0000

    comments updated String constant added as per the guidance, commented out codes has been removed Issue-ID: SO-1490
    Change-Id: Ia7a531580eaafd80900200b1cf5122c1a8b9f511
    Signed-off-by: Subhosree Pan <subhosree.pan1@huawei.com>

--- mso-api-handlers/mso-api-handler-infra/src/main/java/org/onap/so/apihandlerinfra/tenantisolation/helpers/ActivateVnfDBHelper.java
+++ mso-api-handlers/mso-api-handler-infra/src/main/java/org/onap/so/apihandlerinfra/tenantisolation/helpers/ActivateVnfDBHelper.java
@@ -31,7 +31,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class ActivateVnfDBHelper {
 
-    private static Logger logger = LoggerFactory.getLogger(ActivateVnfDBHelper.class);
+
 
     /**
      * Insert record to OperationalEnvServiceModelStatus table
