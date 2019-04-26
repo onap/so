06466b18b5e5878b85d6bf3e5c7747ee2df56120
subhash kumar singh <subhash.kumar.singh@huawei.com>
2019-04-26 17:08:40 +0530

    Add new resource model
    Add new resource model to handle group and vnfc resource.
    
    Issue-ID: SO-1393
    Change-Id: I243916d2ed2b9b8aa852613f936e70fc9b841001
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

--- bpmn/MSOCoreBPMN/src/main/java/org/onap/so/bpmn/core/domain/GroupResource.java
+++ bpmn/MSOCoreBPMN/src/main/java/org/onap/so/bpmn/core/domain/GroupResource.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,10 +17,21 @@
  * limitations under the License.
  * ============LICENSE_END=========================================================
  */
-
 package org.onap.so.bpmn.core.domain;
 
-public enum ResourceType {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-    VNF, NETWORK, MODULE, ALLOTTED_RESOURCE, CONFIGURATION, GROUP, VNFC // etc.
+import java.util.List;
+import java.util.UUID;
+
+public class GroupResource extends Resource {
+    private static final long serialVersionUID = 1L;
+
+    @JsonProperty("vnfcs")
+    private List<VnfcResource> vnfcs;
+
+    public GroupResource() {
+        resourceType = ResourceType.GROUP;
+        setResourceId(UUID.randomUUID().toString());
+    }
 }
--- bpmn/MSOCoreBPMN/src/main/java/org/onap/so/bpmn/core/domain/VnfcResource.java
+++ bpmn/MSOCoreBPMN/src/main/java/org/onap/so/bpmn/core/domain/VnfcResource.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,10 +17,14 @@
  * limitations under the License.
  * ============LICENSE_END=========================================================
  */
-
 package org.onap.so.bpmn.core.domain;
 
-public enum ResourceType {
+import java.util.UUID;
 
-    VNF, NETWORK, MODULE, ALLOTTED_RESOURCE, CONFIGURATION, GROUP, VNFC // etc.
+public class VnfcResource extends Resource{
+
+    public VnfcResource() {
+        resourceType = ResourceType.VNFC;
+        setResourceId(UUID.randomUUID().toString());
+    }
 }
