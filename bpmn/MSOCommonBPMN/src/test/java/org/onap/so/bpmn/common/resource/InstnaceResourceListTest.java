340e7b852f27e549f9c796ea5b29a1c1846c4436
subhash kumar singh <subhash.kumar.singh@huawei.com>
2019-05-07 12:40:41 +0530

    Improve test cases for instance resource list
    Improve test cases for instance resource list.
    
    Change-Id: Icb7f01e7d12a16b623180678ab0ac0a1186ea511
    Issue-ID: SO-1393
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

--- bpmn/MSOCommonBPMN/src/test/java/org/onap/so/bpmn/common/resource/InstnaceResourceListTest.java
+++ bpmn/MSOCommonBPMN/src/test/java/org/onap/so/bpmn/common/resource/InstnaceResourceListTest.java
@@ -7,6 +7,7 @@ import org.onap.so.bpmn.core.domain.Resource;
 import org.onap.so.bpmn.core.domain.ResourceType;
 import org.onap.so.bpmn.core.domain.VnfResource;
 import org.onap.so.bpmn.core.domain.VnfcResource;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -15,25 +16,55 @@ import java.util.Arrays;
 import java.util.List;
 
 public class InstnaceResourceListTest {
-
     public static String RESOURCE_PATH = "src/test/resources/__files/InstanceResourceList/";
 
     @Test
-    public void testInstanceResourceList() throws IOException {
+    public void testInstanceResourceTwoVF() throws IOException {
         String uuiRequest = new String(Files.readAllBytes(Paths.get(RESOURCE_PATH + "InstanceResourceList" + ".json")));
         List<Resource> instanceResourceList =
-                InstanceResourceList.getInstanceResourceList(createResourceSequence(), uuiRequest);
+                InstanceResourceList.getInstanceResourceList(createResourceSequence1(), uuiRequest);
+        Assert.assertEquals(2, instanceResourceList.size());
+        Assert.assertEquals(ResourceType.VNF, instanceResourceList.get(0).getResourceType());
+        Assert.assertEquals(ResourceType.VNF, instanceResourceList.get(1).getResourceType());
+    }
+
+
+    @Test
+    public void testInstanceResourceListWithOneVFOneGrp() throws IOException {
+        String uuiRequest = new String(Files.readAllBytes(Paths.get(RESOURCE_PATH + "InstanceResourceList" + ".json")));
+        List<Resource> instanceResourceList =
+                InstanceResourceList.getInstanceResourceList(createResourceSequence2(), uuiRequest);
         Assert.assertEquals(4, instanceResourceList.size());
         Assert.assertEquals(ResourceType.VNF, instanceResourceList.get(0).getResourceType());
         Assert.assertEquals(ResourceType.GROUP, instanceResourceList.get(1).getResourceType());
         Assert.assertEquals(ResourceType.VNF, instanceResourceList.get(2).getResourceType());
         Assert.assertEquals(ResourceType.GROUP, instanceResourceList.get(3).getResourceType());
+
     }
 
-    private List<Resource> createResourceSequence() {
-        List<Resource> resourceList = new ArrayList<>();
+    @Test
+    public void testInstanceResourceList3() throws IOException {
+        String uuiRequest = new String(Files.readAllBytes(Paths.get(RESOURCE_PATH + "InstanceResourceList" + ".json")));
+        List<Resource> instanceResourceList =
+                InstanceResourceList.getInstanceResourceList(createResourceSequence3(), uuiRequest);
+        Assert.assertEquals(6, instanceResourceList.size());
+        Assert.assertEquals(ResourceType.VNF, instanceResourceList.get(0).getResourceType());
+        Assert.assertEquals(ResourceType.GROUP, instanceResourceList.get(1).getResourceType());
+        Assert.assertEquals(ResourceType.GROUP, instanceResourceList.get(2).getResourceType());
+        Assert.assertEquals(ResourceType.VNF, instanceResourceList.get(3).getResourceType());
+        Assert.assertEquals(ResourceType.GROUP, instanceResourceList.get(4).getResourceType());
+        Assert.assertEquals(ResourceType.GROUP, instanceResourceList.get(5).getResourceType());
+
+    }
+
+    private List<Resource> createResourceSequence1() {
         VnfResource vnfResource = new VnfResource();
         vnfResource.setResourceInput("{\"a\":\"[sdwansiteresource_list,INDEX,sdwansiteresource_list]\"}");
+
+        return Arrays.asList(vnfResource);
+    }
+
+    private List<Resource> createResourceSequence2() {
 
         VnfcResource vnfcResource = new VnfcResource();
         vnfcResource.setResourceInput("{\"a\":\"[sdwansitewan_list,INDEX,test]\"}");
@@ -41,6 +72,22 @@ public class InstnaceResourceListTest {
         GroupResource groupResource = new GroupResource();
         groupResource.setVnfcs(Arrays.asList(vnfcResource));
 
-        return Arrays.asList(vnfResource, groupResource);
+        ArrayList<Resource> seq = new ArrayList<>();
+        seq.addAll(createResourceSequence1());
+        seq.add(groupResource);
+        return seq;
+    }
+
+    private List<Resource> createResourceSequence3() {
+        VnfcResource vnfcResource = new VnfcResource();
+        vnfcResource.setResourceInput("{\"b\":\"[sdwandevice_list,INDEX,test]\"}");
+
+        GroupResource groupResource = new GroupResource();
+        groupResource.setVnfcs(Arrays.asList(vnfcResource));
+
+        ArrayList<Resource> seq = new ArrayList<>();
+        seq.addAll(createResourceSequence2());
+        seq.add(groupResource);
+        return seq;
     }
 }
