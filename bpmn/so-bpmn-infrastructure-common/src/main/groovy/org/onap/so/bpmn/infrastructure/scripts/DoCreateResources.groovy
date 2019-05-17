7e19e0aa3ad2fff8f8593579192a417932b36004
subhash kumar singh <subhash.kumar.singh@huawei.com>
2019-05-17 19:01:40 +0530

    Add model type to resource model
    Add model type to resource model.
    
    Change-Id: Ic1937d859cebca1b515d9c069c51c9811d6874ab
    Issue-ID: SO-1393
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

--- bpmn/so-bpmn-infrastructure-common/src/main/groovy/org/onap/so/bpmn/infrastructure/scripts/DoCreateResources.groovy
+++ bpmn/so-bpmn-infrastructure-common/src/main/groovy/org/onap/so/bpmn/infrastructure/scripts/DoCreateResources.groovy
@@ -90,7 +90,7 @@ public class DoCreateResources extends AbstractServiceTaskProcessor{
     }
 
     // this method will convert resource list to instance_resource_list
-    void prepareInstanceResourceList(DelegateExecution execution) {
+    public void prepareInstanceResourceList(DelegateExecution execution) {
 
         String uuiRequest = execution.getVariable("uuiRequest")
         List<Resource> sequencedResourceList = execution.getVariable("sequencedResourceList")
@@ -251,9 +251,11 @@ public class DoCreateResources extends AbstractServiceTaskProcessor{
         def currentIndex = execution.getVariable("currentResourceIndex")
         List<Resource> sequencedResourceList = execution.getVariable("instanceResourceList")
         Resource currentResource = sequencedResourceList.get(currentIndex)
-        resourceInput.setResourceModelInfo(currentResource.getModelInfo());
+        resourceInput.setResourceModelInfo(currentResource.getModelInfo())
+        resourceInput.getResourceModelInfo().setModelType(currentResource.getResourceType().toString())
         ServiceDecomposition serviceDecomposition = execution.getVariable("serviceDecomposition")
-        resourceInput.setServiceModelInfo(serviceDecomposition.getModelInfo());
+
+        resourceInput.setServiceModelInfo(serviceDecomposition.getModelInfo())
         def String resourceCustomizationUuid = currentResource.getModelInfo().getModelCustomizationUuid();
 
         String incomingRequest = execution.getVariable("uuiRequest")
