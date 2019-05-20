4a3f40a5721cf80baca63b60bf9c8097314d47e7
Bonkur, Venkat (vb8416) <vb8416@att.com>
2019-05-20 14:43:33 -0400

    Add SO WFD activity BBs to building_block_detail
    Added dublin-WFD activity BBs to building_block_detaili tbl
    Also updated the resource_type to NO_VALIDATE where target_action is custom
    
    Issue-ID: SO-1901
    Signed-off-by: Bonkur, Venkat (vb8416) <vb8416@att.com>
    Change-Id: I24e0c7159b71215914d3ef09fcfdb23928ea2aca

--- adapters/mso-catalog-db-adapter/src/main/resources/db/migration/R__WorkflowDesignerData.sql
+++ adapters/mso-catalog-db-adapter/src/main/resources/db/migration/R__WorkflowDesignerData.sql
@@ -182,3 +182,4 @@ VALUES
 (select ID from user_parameters where NAME='existing_software_version')),
 ((select ID from activity_spec where NAME='VNFUpgradeSoftwareActivity' and VERSION=1.0),
 (select ID from user_parameters where NAME='new_software_version'));
+
