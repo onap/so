969333e6374d3e2259ab5fbbdbcbb3f4c12a134e
Elena Kuleshov <evn@att.com>
2019-05-20 11:45:55 -0400

    Check for null requestInfo
    Check for null requestInfo
    
    Issue-ID: SO-1900
    Signed-off-by: Kuleshov, Elena <evn@att.com>
    Change-Id: I2a3eb7ddbee396b19bcc71a2f647b9cf90082ed4

--- bpmn/MSOCommonBPMN/src/main/java/org/onap/so/bpmn/servicedecomposition/tasks/BBInputSetupMapperLayer.java
+++ bpmn/MSOCommonBPMN/src/main/java/org/onap/so/bpmn/servicedecomposition/tasks/BBInputSetupMapperLayer.java
@@ -378,7 +378,11 @@ public class BBInputSetupMapperLayer {
 
     protected OrchestrationContext mapOrchestrationContext(RequestDetails requestDetails) {
         OrchestrationContext context = new OrchestrationContext();
-        context.setIsRollbackEnabled(!(requestDetails.getRequestInfo().getSuppressRollback()));
+        if (requestDetails.getRequestInfo() != null) {
+            context.setIsRollbackEnabled(!(requestDetails.getRequestInfo().getSuppressRollback()));
+        } else {
+            context.setIsRollbackEnabled(false);
+        }
         return context;
     }
 
--- bpmn/MSOCommonBPMN/src/test/java/org/onap/so/bpmn/servicedecomposition/tasks/BBInputSetupMapperLayerTest.java
+++ bpmn/MSOCommonBPMN/src/test/java/org/onap/so/bpmn/servicedecomposition/tasks/BBInputSetupMapperLayerTest.java
@@ -571,6 +571,19 @@ public class BBInputSetupMapperLayerTest {
     }
 
     @Test
+    public void testMapOrchestrationContextNoRequestInfo() throws IOException {
+        OrchestrationContext expected = new OrchestrationContext();
+        expected.setIsRollbackEnabled(false);
+
+        RequestDetails requestDetails = mapper.readValue(
+                new File(RESOURCE_PATH + "RequestDetailsInput_mapReqContextNoRequestInfo.json"), RequestDetails.class);
+
+        OrchestrationContext actual = bbInputSetupMapperLayer.mapOrchestrationContext(requestDetails);
+
+        assertThat(actual, sameBeanAs(expected));
+    }
+
+    @Test
     public void testMapLocationContext() {
         CloudRegion expected = new CloudRegion();
         expected.setCloudOwner("test-owner-name");
