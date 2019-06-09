b7cde4474b9688a1527254dcae6114123ea00c9c
Plummer, Brittany <brittany.plummer@att.com>
2019-06-09 10:38:55 -0400

    Added ext_system_error_source column to requestdb
    Added ext_system_error_source column to requestdb
    
    Change-Id: Iad9ad0178546aad2b64014987ff3da60f01f8bd4
    Issue-ID: SO-2001
    Signed-off-by: Benjamin, Max (mb388a) <mb388a@us.att.com>

--- mso-api-handlers/mso-requests-db/src/main/java/org/onap/so/db/request/beans/InfraActiveRequests.java
+++ mso-api-handlers/mso-requests-db/src/main/java/org/onap/so/db/request/beans/InfraActiveRequests.java
@@ -88,6 +88,7 @@ public class InfraActiveRequests extends InfraRequests {
                 .append("requestorId", getRequestorId()).append("configurationId", getConfigurationId())
                 .append("configurationName", getConfigurationName()).append("operationalEnvId", getOperationalEnvId())
                 .append("operationalEnvName", getOperationalEnvName()).append("requestUrl", getRequestUrl())
-                .append("originalRequestId", getOriginalRequestId()).toString();
+                .append("originalRequestId", getOriginalRequestId())
+                .append("extSystemErrorSource", getExtSystemErrorSource()).toString();
     }
 }
