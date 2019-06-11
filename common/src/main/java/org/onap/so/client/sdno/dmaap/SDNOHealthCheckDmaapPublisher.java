e30f4a24c4db0657cd4a8f7d76e0fd0167983c63
Boslet, Cory <cory.boslet@att.com>
2019-06-11 10:23:58 -0400

    Updated sdno to use msokey config property
    Updated sdno to use msokey config property instead of one that doesnt
    exist
    
    Change-Id: I7f1bc39ae1dae3f6b0cab8c05c97950f45c83f71
    Issue-ID: SO-2009
    Signed-off-by: Benjamin, Max (mb388a) <mb388a@us.att.com>

--- common/src/main/java/org/onap/so/client/sdno/dmaap/SDNOHealthCheckDmaapPublisher.java
+++ common/src/main/java/org/onap/so/client/sdno/dmaap/SDNOHealthCheckDmaapPublisher.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,7 +38,7 @@ public class SDNOHealthCheckDmaapPublisher extends DmaapPublisher {
 
     @Override
     public String getKey() {
-        return msoProperties.get("sdno.health-check.dmaap.msoKey");
+        return msoProperties.get("mso.msoKey");
     }
 
     @Override
