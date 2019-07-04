1f97e7f053dbf64e91c2215785f954399c5a6f09
k.kazak <k.kazak@samsung.com>
2019-07-04 16:26:42 +0200

    basic auth for so-monitoring
    Add basic auth for so-monitoring app
    Issue submitted in pentest report
    
    Change-Id: I8e826da9b9f66e893826fd9b40b3b26623b2ab8d
    Issue-ID: OJSI-169
    Signed-off-by: k.kazak <k.kazak@samsung.com>

--- so-monitoring/so-monitoring-service/src/main/java/org/onap/so/monitoring/rest/api/WebSecurityConfigImpl.java
+++ so-monitoring/so-monitoring-service/src/main/java/org/onap/so/monitoring/rest/api/WebSecurityConfigImpl.java
@@ -1,37 +1,40 @@
 /*-
  * ============LICENSE_START=======================================================
- *  Copyright (C) 2018 Ericsson. All rights reserved.
+ *  Copyright (C) 2019 Samsung
  * ================================================================================
- * Modifications Copyright (c) 2019 Samsung
- * ================================================================================
-  * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
+
 package org.onap.so.monitoring.rest.api;
 
+import org.onap.so.security.WebSecurityConfig;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.util.StringUtils;
 
-/**
- * @author waqas.ikram@ericsson.com
- */
-@Configuration
-public class WebApplicationConfig implements WebMvcConfigurer {
+@EnableWebSecurity
+@Configuration("att-security-config")
+@Order(2)
+public class WebSecurityConfigImpl extends WebSecurityConfig {
+
     @Override
-    public void addViewControllers(final ViewControllerRegistry registry) {
-        registry.addViewController("/details/**").setViewName("forward:/");
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/actuator", "/actuator/*").permitAll().antMatchers("/**")
+                .hasAnyRole(StringUtils.collectionToDelimitedString(getRoles(), ",")).and().httpBasic();
     }
 }
