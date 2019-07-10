4cf8b93e7a359074e107639f58a16835d8419ff5
jegadeeshbabu1 <jegabab1@in.ibm.com>
2019-07-10 10:24:42 +0530

    Added null check to avoid null pointer exception
    Added null check
    
    Issue-ID: SO-2104
    Change-Id: Ic1d35314b4037fbe88c12caa64fd0cd2c4eb15de
    Signed-off-by: jegadeeshbabu1 <jegabab1@in.ibm.com>

--- bpmn/so-bpmn-tasks/src/main/java/org/onap/so/bpmn/infrastructure/workflow/tasks/WorkflowAction.java
+++ bpmn/so-bpmn-tasks/src/main/java/org/onap/so/bpmn/infrastructure/workflow/tasks/WorkflowAction.java
@@ -6,6 +6,8 @@
  * ================================================================================
  * Modifications Copyright (c) 2019 Samsung
  * ================================================================================
+ * Modifications Copyright (c) 2019 IBM
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -358,10 +360,10 @@ public class WorkflowAction {
                     }
                 }
             }
-
-            if (flowsToExecute.isEmpty()) {
+            
+            if(flowsToExecute!= null && flowsToExecute.isEmpty()) {	
                 throw new IllegalStateException("Macro did not come up with a valid execution path.");
-            }
+            }           
             List<String> flowNames = new ArrayList<>();
             logger.info("List of BuildingBlocks to execute:");
             for (ExecuteBuildingBlock ebb : flowsToExecute) {
