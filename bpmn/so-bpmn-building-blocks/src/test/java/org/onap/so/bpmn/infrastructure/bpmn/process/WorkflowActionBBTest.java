89604fd38922a99d7566526562cebf7406907c18
r.bogacki <r.bogacki@samsung.com>
2019-07-10 11:29:37 +0200

    Fixes in WorkflowAction
    Fixes in WorkflowAction according to the Sonar analysis:
    -Fixed String comparison.
    -Removed unnecessary method parameters.
    -Updated junit test.
    
    Issue-ID: SO-2107
    Signed-off-by: Robert Bogacki <r.bogacki@samsung.com>
    Change-Id: I7522269e237f736521a627bcd1c8f77b0c776a70

--- bpmn/so-bpmn-building-blocks/src/test/java/org/onap/so/bpmn/infrastructure/bpmn/process/WorkflowActionBBTest.java
+++ bpmn/so-bpmn-building-blocks/src/test/java/org/onap/so/bpmn/infrastructure/bpmn/process/WorkflowActionBBTest.java
@@ -4,6 +4,8 @@
  * ================================================================================
  * Copyright (C) 2017 - 2018 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Modifications Copyright (c) 2019 Samsung
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -93,8 +95,7 @@ public class WorkflowActionBBTest extends BaseBPMNTest {
         Map<String, String> map = new HashMap<>();
         map.put("handlingCode", "Abort");
 
-        doThrow(new BpmnError("7000", "TESTING ERRORS")).when(workflowActionBBFailure)
-                .abortCallErrorHandling(any(DelegateExecution.class));
+        doThrow(new BpmnError("7000", "TESTING ERRORS")).when(workflowActionBBFailure).abortCallErrorHandling();
         mockSubprocess("ExecuteBuildingBlock", "Mocked ExecuteBuildingBlock", "GenericStub", map);
 
         ProcessInstance pi = runtimeService.startProcessInstanceByKey("WorkflowActionBB", variables);
