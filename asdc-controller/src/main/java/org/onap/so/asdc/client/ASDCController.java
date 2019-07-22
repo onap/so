865b8d7819dba99ad59bcc5d396af827035419a1
sarada prasad sahoo <sarada.prasad.sahoo@huawei.com>
2019-07-22 16:55:12 +0530

    Enhanced List Level flow with backward support
    Modified the e2e service instance flow to
    support both new list types groups along with
    backward compatibility to support old types
    alloted resources, network etc.
    
    Change-Id: I1cb128e259c54b80009840914b2c1cee8cf8a4df
    Issue-ID: SO-1393
    Signed-off-by: sarada prasad sahoo <sarada.prasad.sahoo@huawei.com>

--- asdc-controller/src/main/java/org/onap/so/asdc/client/ASDCController.java
+++ asdc-controller/src/main/java/org/onap/so/asdc/client/ASDCController.java
@@ -736,8 +736,7 @@ public class ASDCController {
 
                 logger.info("Processing Resource Type: {}, Model UUID: {}", resourceType, resource.getResourceUUID());
 
-                if ("VF".equals(resourceType) && resource.getArtifacts() != null
-                        && !resource.getArtifacts().isEmpty()) {
+                if ("VF".equals(resourceType)) {
                     resourceStructure = new VfResourceStructure(iNotif, resource);
                 } else if ("PNF".equals(resourceType)) {
                     resourceStructure = new PnfResourceStructure(iNotif, resource);
@@ -755,8 +754,7 @@ public class ASDCController {
                         logger.debug("Processing Resource Type: " + resourceType + " and Model UUID: "
                                 + resourceStructure.getResourceInstance().getResourceUUID());
 
-                        if ("VF".equals(resourceType) && resource.getArtifacts() != null
-                                && !resource.getArtifacts().isEmpty()) {
+                        if ("VF".equals(resourceType)) {
                             hasVFResource = true;
                             for (IArtifactInfo artifact : resource.getArtifacts()) {
                                 IDistributionClientDownloadResult resultArtifact =
