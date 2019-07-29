c3e3bcbcc4516f4b2afa10edcb7e98c25c6b04b4
Plummer, Brittany <brittany.plummer@att.com>
2019-07-29 14:33:20 -0400

    apih allowing requests with the same requestid
    Updated filters to throw error on duplicate requestId
    Added message indicating exception will be thrown
    InfraActivRequest check added to ATT filter
    Added unit tests for requestIdFilters
    Added access modifier in filter test
    Updated CloudApiRequests for successful deletion when
    InfraActiveRequest is deleted
    Removed changes from CloudApiRequests bean
    Updated repository to include deleteByRequestId
    Removed unused import from cloudApiRequests
    Removed deleteByRequestId from interface
    Updated to change type of extended jparepository
    Removed repository added for CloudApiRequests
    Updated uri check to remove '/' from checked string
    Updated returned on failing junit test
    Updated RequestIdFilterATT to call RequestIdFilter
    Updated RequestIdFilterATTTest returning RequestError
    Updated access modifiers for logger and createRequestError
    
    Issue-ID: SO-2166
    Signed-off-by: Benjamin, Max (mb388a) <mb388a@att.com>
    Change-Id: Id0cee672567b15e0a3eb1acfbdfb967945494500

--- mso-api-handlers/mso-api-handler-common/src/test/java/org/onap/so/apihandler/filters/RequestIdFilterTest.java
+++ mso-api-handlers/mso-api-handler-common/src/test/java/org/onap/so/apihandler/filters/RequestIdFilterTest.java
@@ -20,67 +20,85 @@
 
 package org.onap.so.apihandler.filters;
 
-import static org.junit.Assert.assertEquals;
+import static com.shazam.shazamcrest.matcher.Matchers.sameBeanAs;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.doReturn;
 import java.io.IOException;
+import java.util.Collections;
 import javax.ws.rs.container.ContainerRequestContext;
-import org.apache.http.HttpStatus;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
-import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.junit.MockitoRule;
 import org.onap.logging.ref.slf4j.ONAPLogConstants;
+import org.onap.so.apihandlerinfra.exceptions.DuplicateRequestIdException;
 import org.onap.so.db.request.beans.InfraActiveRequests;
 import org.onap.so.db.request.client.RequestsDbClient;
+import org.onap.so.serviceinstancebeans.RequestError;
+import org.onap.so.serviceinstancebeans.ServiceException;
 import org.slf4j.MDC;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RequestIdFilterTest {
 
     @Mock
-    ContainerRequestContext mockContext;
+    private ContainerRequestContext mockContext;
 
     @Mock
-    protected RequestsDbClient requestsDbClient;
+    private RequestsDbClient requestsDbClient;
 
     @InjectMocks
     @Spy
-    RequestIdFilter requestIdFilter;
+    private RequestIdFilter requestIdFilter;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-    @Rule
-    public MockitoRule mockitoRule = MockitoJUnit.rule();
+    private static final String REQUEST_ID = "32807a28-1a14-4b88-b7b3-2950918aa769";
+    private ObjectMapper mapper = new ObjectMapper();
+
+    private RequestError getRequestError() throws IOException {
+        RequestError requestError = new RequestError();
+        mapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, false);
+        ServiceException serviceException = new ServiceException();
+        serviceException.setMessageId("SVC0002");
+        serviceException.setText(
+                "RequestId: 32807a28-1a14-4b88-b7b3-2950918aa769 already exists in the RequestDB InfraActiveRequests table");
+        serviceException.setVariables(Collections.emptyList());
+        requestError.setServiceException(serviceException);
+        return requestError;
+    }
 
     @Test
-    public void filterTest() throws IOException {
-
-        String requestId = "32807a28-1a14-4b88-b7b3-2950918aa769";
+    public void filterTestInfra() throws IOException {
+        String error = mapper.writeValueAsString(getRequestError());
+        String requestId = REQUEST_ID;
         MDC.put(ONAPLogConstants.MDCs.REQUEST_ID, requestId);
 
         // ExpectedRecord InfraActiveRequests
         InfraActiveRequests infraActiveRequests = new InfraActiveRequests();
-        infraActiveRequests.setRequestStatus("FAILED");
-        infraActiveRequests.setProgress(100L);
-        infraActiveRequests.setLastModifiedBy("APIH");
-        infraActiveRequests.setRequestScope("network");
-        infraActiveRequests.setRequestAction("deleteInstance");
-        infraActiveRequests.setRequestId("32807a28-1a14-4b88-b7b3-2950918aa769");
+        infraActiveRequests.setRequestId(REQUEST_ID);
 
         doReturn(infraActiveRequests).when(requestsDbClient).getInfraActiveRequestbyRequestId(requestId);
+        doReturn(error).when(requestIdFilter).createRequestError(REQUEST_ID, "InfraActiveRequests");
 
+        thrown.expect(DuplicateRequestIdException.class);
+        thrown.expectMessage("HTTP 400 Bad Request");
         requestIdFilter.filter(mockContext);
+    }
 
-        Mockito.verify(requestIdFilter, Mockito.times(1)).filter(mockContext);
-        assertEquals(MDC.get(ONAPLogConstants.MDCs.RESPONSE_CODE), String.valueOf(HttpStatus.SC_BAD_REQUEST));
+    @Test
+    public void createRequestErrorTest() throws IOException {
+        RequestError requestError = getRequestError();
+        String result = requestIdFilter.createRequestError(REQUEST_ID, "InfraActiveRequests");
+        RequestError resultingError = mapper.readValue(result, RequestError.class);
 
+        assertThat(resultingError, sameBeanAs(requestError));
     }
 }
