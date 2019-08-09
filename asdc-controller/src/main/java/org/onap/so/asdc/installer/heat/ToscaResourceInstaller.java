2b9ae350d8a75af9c6c6c095a8c12c3c2c0409b2
sindhu3672 <arcot.sindhuri@huawei.com>
2019-08-09 20:45:41 +0530

    Avoid null pointer exception
    Avoid null pointer ex ToscaResourceInstaller
    
    Issue-ID: SO-2187
    
    Signed-off-by: sindhu3672 <arcot.sindhuri@huawei.com>
    Change-Id: I68745ec0ac6df197369d5882e0602f31fb7ad5e6

--- asdc-controller/src/main/java/org/onap/so/asdc/installer/heat/ToscaResourceInstaller.java
+++ asdc-controller/src/main/java/org/onap/so/asdc/installer/heat/ToscaResourceInstaller.java
@@ -10,9 +10,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -2017,8 +2017,8 @@ public class ToscaResourceInstaller {
                     }
 
                     // Only Add Abstract VNFC's to our DB, ignore all others
-                    if (existingVnfcCustomization == null && vfcEntity.getMetadata()
-                            .getValue(SdcPropertyNames.PROPERTY_NAME_SUBCATEGORY).equalsIgnoreCase("Abstract")) {
+                    if (existingVnfcCustomization == null && ("Abstract").equalsIgnoreCase(
+                            vfcEntity.getMetadata().getValue(SdcPropertyNames.PROPERTY_NAME_SUBCATEGORY))) {
 
                         vnfcCustomization.setModelCustomizationUUID(
                                 vfcEntity.getMetadata().getValue(SdcPropertyNames.PROPERTY_NAME_CUSTOMIZATIONUUID));
@@ -2148,7 +2148,7 @@ public class ToscaResourceInstaller {
 
                         String policyType = getLeafPropertyValue(policyEntity, "type");
 
-                        if (policyType != null && policyType.equalsIgnoreCase("Fabric Policy")) {
+                        if (policyType != null && ("Fabric Policy").equalsIgnoreCase(policyType)) {
                             cvnfcConfigurationCustomization.setPolicyName(getLeafPropertyValue(policyEntity, "name"));
                         }
                     }
