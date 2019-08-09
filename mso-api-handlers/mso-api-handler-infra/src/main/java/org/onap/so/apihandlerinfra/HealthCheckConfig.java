2a6e3dca63c0cb67db3b65b0e5bf6085b5458e9b
Benjamin, Max <max.benjamin@att.com>
2019-08-09 13:29:22 -0400

    health check now entirely config based
    health check now entirely config based
    api handler now lists itself as an endpoint
    updated robot test to verify new format
    fixed formatting issues with modified files
    updated test case to reflect new statuses
    fixed duplicate bean issue with converter
    ignore unit test that never worked
    added missing class to context configuration
    removed extraneous spaces from robot file
    update robot files as well as fix compile issues
    config format must be correct on startup
    
    Issue-ID: SO-2216
    Signed-off-by: Benjamin, Max (mb388a) <mb388a@att.com>
    Change-Id: I86770922c7b7f36e4a4d59615f7336cda7cbe629

---
+++ mso-api-handlers/mso-api-handler-infra/src/main/java/org/onap/so/apihandlerinfra/HealthCheckConfig.java
@@ -0,0 +1,83 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.so.apihandlerinfra;
+
+import java.net.URI;
+import java.util.List;
+import javax.validation.constraints.NotNull;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
+
+@Configuration
+@ConfigurationProperties(prefix = "mso.health")
+@Validated
+public class HealthCheckConfig {
+
+    @NotNull
+    private List<Endpoint> endpoints;
+
+    public List<Endpoint> getEndpoints() {
+        return endpoints;
+    }
+
+    public void setEndpoints(List<Endpoint> endpoints) {
+        this.endpoints = endpoints;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("endpoints", this.endpoints).toString();
+    }
+
+    @Validated
+    public static class Endpoint {
+        @NotNull
+        private Subsystem subsystem;
+        @NotNull
+        private URI uri;
+
+        public Endpoint() {
+
+        }
+
+        public Endpoint(Subsystem subsystem, URI uri) {
+            this.subsystem = subsystem;
+            this.uri = uri;
+        }
+
+        public Subsystem getSubsystem() {
+            return subsystem;
+        }
+
+        public void setSubsystem(Subsystem subsystem) {
+            this.subsystem = subsystem;
+        }
+
+        public URI getUri() {
+            return uri;
+        }
+
+        public void setUri(URI uri) {
+            this.uri = uri;
+        }
+    }
+}
--- mso-api-handlers/mso-api-handler-infra/src/test/java/org/onap/so/apihandlerinfra/GlobalHealthcheckHandlerTest.java
+++ mso-api-handlers/mso-api-handler-infra/src/test/java/org/onap/so/apihandlerinfra/GlobalHealthcheckHandlerTest.java
@@ -20,70 +20,67 @@
 
 package org.onap.so.apihandlerinfra;
 
-import static org.junit.Assert.assertArrayEquals;
+import static com.shazam.shazamcrest.matcher.Matchers.sameBeanAs;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyObject;
+import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import java.net.URI;
-import java.util.Collections;
-import java.util.List;
-import org.springframework.test.util.ReflectionTestUtils;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
-import org.springframework.core.ParameterizedTypeReference;
+import org.json.JSONException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.onap.so.apihandlerinfra.HealthCheckConfig.Endpoint;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.json.JSONException;
-import org.junit.Rule;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.Spy;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {GenericStringConverter.class, HealthCheckConverter.class},
+        initializers = {ConfigFileApplicationContextInitializer.class})
+@ActiveProfiles("test")
+@EnableConfigurationProperties({HealthCheckConfig.class})
 public class GlobalHealthcheckHandlerTest {
-    @Mock
-    RestTemplate restTemplate;
+    @MockBean
+    private RestTemplate restTemplate;
 
-    @Mock
-    ContainerRequestContext requestContext;
+    @MockBean
+    private ContainerRequestContext requestContext;
 
-    @InjectMocks
-    @Spy
-    GlobalHealthcheckHandler globalhealth;
-
-    @Rule
-    public MockitoRule mockitoRule = MockitoJUnit.rule();
+    @SpyBean
+    private GlobalHealthcheckHandler globalhealth;
 
     @Test
     public void testQuerySubsystemHealthNullResult() {
         ReflectionTestUtils.setField(globalhealth, "actuatorContextPath", "/manage");
-        ReflectionTestUtils.setField(globalhealth, "endpointBpmn", "http://localhost:8080");
 
         Mockito.when(restTemplate.exchange(ArgumentMatchers.any(URI.class), ArgumentMatchers.any(HttpMethod.class),
                 ArgumentMatchers.<HttpEntity<?>>any(), ArgumentMatchers.<Class<Object>>any())).thenReturn(null);
 
-        String result = globalhealth.querySubsystemHealth(MsoSubsystems.BPMN);
-        System.out.println(result);
-        assertEquals(HealthcheckStatus.DOWN.toString(), result);
+        HealthCheckSubsystem result = globalhealth
+                .querySubsystemHealth(new Endpoint(SoSubsystems.BPMN, UriBuilder.fromPath("http://localhost").build()));
+        assertEquals(HealthCheckStatus.DOWN, result.getStatus());
     }
 
     @Test
     public void testQuerySubsystemHealthNotNullResult() {
         ReflectionTestUtils.setField(globalhealth, "actuatorContextPath", "/manage");
-        ReflectionTestUtils.setField(globalhealth, "endpointAsdc", "http://localhost:8080");
 
         SubsystemHealthcheckResponse subSystemResponse = new SubsystemHealthcheckResponse();
         subSystemResponse.setStatus("UP");
@@ -92,20 +89,13 @@ public class GlobalHealthcheckHandlerTest {
         Mockito.when(restTemplate.exchange(ArgumentMatchers.any(URI.class), ArgumentMatchers.any(HttpMethod.class),
                 ArgumentMatchers.<HttpEntity<?>>any(), ArgumentMatchers.<Class<Object>>any())).thenReturn(r);
 
-        String result = globalhealth.querySubsystemHealth(MsoSubsystems.ASDC);
-        System.out.println(result);
-        assertEquals(HealthcheckStatus.UP.toString(), result);
+        HealthCheckSubsystem result = globalhealth
+                .querySubsystemHealth(new Endpoint(SoSubsystems.ASDC, UriBuilder.fromPath("http://localhost").build()));
+        assertEquals(HealthCheckStatus.UP, result.getStatus());
     }
 
     private Response globalHealthcheck(String status) {
         ReflectionTestUtils.setField(globalhealth, "actuatorContextPath", "/manage");
-        ReflectionTestUtils.setField(globalhealth, "endpointAsdc", "http://localhost:8080");
-        ReflectionTestUtils.setField(globalhealth, "endpointSdnc", "http://localhost:8081");
-        ReflectionTestUtils.setField(globalhealth, "endpointBpmn", "http://localhost:8082");
-        ReflectionTestUtils.setField(globalhealth, "endpointCatalogdb", "http://localhost:8083");
-        ReflectionTestUtils.setField(globalhealth, "endpointOpenstack", "http://localhost:8084");
-        ReflectionTestUtils.setField(globalhealth, "endpointRequestdb", "http://localhost:8085");
-        ReflectionTestUtils.setField(globalhealth, "endpointRequestdbAttsvc", "http://localhost:8086");
 
         SubsystemHealthcheckResponse subSystemResponse = new SubsystemHealthcheckResponse();
 
@@ -116,70 +106,41 @@ public class GlobalHealthcheckHandlerTest {
 
         Mockito.when(requestContext.getProperty(anyString())).thenReturn("1234567890");
         Response response = globalhealth.globalHealthcheck(true, requestContext);
-
         return response;
     }
 
     @Test
-    public void globalHealthcheckAllUPTest() throws JSONException {
+    public void globalHealthcheckAllUPTest() throws JSONException, JsonProcessingException {
+
+        HealthCheckResponse expected = new HealthCheckResponse();
+
+        for (Subsystem system : SoSubsystems.values()) {
+            expected.getSubsystems().add(new HealthCheckSubsystem(system,
+                    UriBuilder.fromUri("http://localhost").build(), HealthCheckStatus.UP));
+        }
+        expected.setMessage("HttpStatus: 200");
         Response response = globalHealthcheck("UP");
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-        HealthcheckResponse root;
-        root = (HealthcheckResponse) response.getEntity();
-        String apistatus = root.getApih();
-        assertTrue(apistatus.equalsIgnoreCase(HealthcheckStatus.UP.toString()));
+        HealthCheckResponse root;
+        root = (HealthCheckResponse) response.getEntity();
+        assertThat(root, sameBeanAs(expected).ignoring("subsystems.uri").ignoring("subsystems.subsystem"));
 
-        String bpmnstatus = root.getBpmn();
-        assertTrue(bpmnstatus.equalsIgnoreCase(HealthcheckStatus.UP.toString()));
-
-        String sdncstatus = root.getSdncAdapter();
-        assertTrue(sdncstatus.equalsIgnoreCase(HealthcheckStatus.UP.toString()));
-
-        String asdcstatus = root.getAsdcController();
-        assertTrue(asdcstatus.equalsIgnoreCase(HealthcheckStatus.UP.toString()));
-
-        String catastatus = root.getCatalogdbAdapter();
-        assertTrue(catastatus.equalsIgnoreCase(HealthcheckStatus.UP.toString()));
-
-        String reqdbstatus = root.getRequestdbAdapter();
-        assertTrue(reqdbstatus.equalsIgnoreCase(HealthcheckStatus.UP.toString()));
-
-        String openstatus = root.getOpenstackAdapter();
-        assertTrue(openstatus.equalsIgnoreCase(HealthcheckStatus.UP.toString()));
-
-        String reqdbattstatus = root.getRequestdbAdapterAttsvc();
-        assertTrue(reqdbattstatus.equalsIgnoreCase(HealthcheckStatus.UP.toString()));
     }
 
     @Test
     public void globalHealthcheckAllDOWNTest() throws JSONException {
+        HealthCheckResponse expected = new HealthCheckResponse();
+
+        for (Subsystem system : SoSubsystems.values()) {
+            expected.getSubsystems().add(new HealthCheckSubsystem(system,
+                    UriBuilder.fromUri("http://localhost").build(), HealthCheckStatus.DOWN));
+        }
+        expected.setMessage("HttpStatus: 200");
         Response response = globalHealthcheck("DOWN");
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-        HealthcheckResponse root;
-        root = (HealthcheckResponse) response.getEntity();
-        String apistatus = root.getApih();
-        assertTrue(apistatus.equalsIgnoreCase(HealthcheckStatus.UP.toString()));
-
-        String bpmnstatus = root.getBpmn();
-        assertTrue(bpmnstatus.equalsIgnoreCase(HealthcheckStatus.DOWN.toString()));
-
-        String sdncstatus = root.getSdncAdapter();
-        assertTrue(sdncstatus.equalsIgnoreCase(HealthcheckStatus.DOWN.toString()));
-
-        String asdcstatus = root.getAsdcController();
-        assertTrue(asdcstatus.equalsIgnoreCase(HealthcheckStatus.DOWN.toString()));
-
-        String catastatus = root.getCatalogdbAdapter();
-        assertTrue(catastatus.equalsIgnoreCase(HealthcheckStatus.DOWN.toString()));
-
-        String reqdbstatus = root.getRequestdbAdapter();
-        assertTrue(reqdbstatus.equalsIgnoreCase(HealthcheckStatus.DOWN.toString()));
-
-        String openstatus = root.getOpenstackAdapter();
-        assertTrue(openstatus.equalsIgnoreCase(HealthcheckStatus.DOWN.toString()));
-
-        String reqdbattstatus = root.getRequestdbAdapterAttsvc();
-        assertTrue(reqdbattstatus.equalsIgnoreCase(HealthcheckStatus.DOWN.toString()));
+        HealthCheckResponse root;
+        root = (HealthCheckResponse) response.getEntity();
+        assertThat(root, sameBeanAs(expected).ignoring("subsystems.uri").ignoring("subsystems.subsystem"));
     }
 
     @Test
@@ -189,40 +150,15 @@ public class GlobalHealthcheckHandlerTest {
         assertEquals(MediaType.APPLICATION_JSON, he.getHeaders().getContentType());
     }
 
-    @Test
-    public void getEndpointUrlForSubsystemEnumTest() {
-        ReflectionTestUtils.setField(globalhealth, "actuatorContextPath", "/manage");
-        ReflectionTestUtils.setField(globalhealth, "endpointAsdc", "http://localhost:8080");
-        ReflectionTestUtils.setField(globalhealth, "endpointSdnc", "http://localhost:8081");
-        ReflectionTestUtils.setField(globalhealth, "endpointBpmn", "http://localhost:8082");
-        ReflectionTestUtils.setField(globalhealth, "endpointCatalogdb", "http://localhost:8083");
-        ReflectionTestUtils.setField(globalhealth, "endpointOpenstack", "http://localhost:8084");
-        ReflectionTestUtils.setField(globalhealth, "endpointRequestdb", "http://localhost:8085");
-        ReflectionTestUtils.setField(globalhealth, "endpointRequestdbAttsvc", "http://localhost:8086");
-
-        String result = globalhealth.getEndpointUrlForSubsystemEnum(MsoSubsystems.ASDC);
-        assertEquals("http://localhost:8080", result);
-        result = globalhealth.getEndpointUrlForSubsystemEnum(MsoSubsystems.SDNC);
-        assertEquals("http://localhost:8081", result);
-        result = globalhealth.getEndpointUrlForSubsystemEnum(MsoSubsystems.BPMN);
-        assertEquals("http://localhost:8082", result);
-        result = globalhealth.getEndpointUrlForSubsystemEnum(MsoSubsystems.CATALOGDB);
-        assertEquals("http://localhost:8083", result);
-        result = globalhealth.getEndpointUrlForSubsystemEnum(MsoSubsystems.OPENSTACK);
-        assertEquals("http://localhost:8084", result);
-        result = globalhealth.getEndpointUrlForSubsystemEnum(MsoSubsystems.REQUESTDB);
-        assertEquals("http://localhost:8085", result);
-        result = globalhealth.getEndpointUrlForSubsystemEnum(MsoSubsystems.REQUESTDBATT);
-        assertEquals("http://localhost:8086", result);
-    }
 
     @Test
     public void processResponseFromSubsystemTest() {
         SubsystemHealthcheckResponse subSystemResponse = new SubsystemHealthcheckResponse();
         subSystemResponse.setStatus("UP");
         ResponseEntity<SubsystemHealthcheckResponse> r = new ResponseEntity<>(subSystemResponse, HttpStatus.OK);
-        String result = globalhealth.processResponseFromSubsystem(r, MsoSubsystems.BPMN);
-        assertEquals("UP", result);
+        Endpoint endpoint = new Endpoint(SoSubsystems.BPMN, UriBuilder.fromUri("http://localhost").build());
+        HealthCheckStatus result = globalhealth.processResponseFromSubsystem(r, endpoint);
+        assertEquals(HealthCheckStatus.UP, result);
     }
 
 }
