4a0888067d8482994bc323eea1006e6d317295de
Arindam Mondal <arind.mondal@samsung.com>
2019-09-11 15:13:04 +0900

    Upgrade swagger in mso-catalog-db-adapter
    Issue-ID: SO-2305
    Change-Id: I25ac20a96cf44a7c8abaa3b78f3b256cb2002d3f
    Signed-off-by: arindamm <arind.mondal@samsung.com>

--- adapters/mso-catalog-db-adapter/src/main/java/org/onap/so/adapters/catalogdb/JerseyConfiguration.java
+++ adapters/mso-catalog-db-adapter/src/main/java/org/onap/so/adapters/catalogdb/JerseyConfiguration.java
@@ -20,6 +20,8 @@
 
 package org.onap.so.adapters.catalogdb;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javax.annotation.PostConstruct;
 import javax.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -30,9 +32,13 @@ import org.onap.so.adapters.catalogdb.rest.ServiceRestImpl;
 import org.onap.so.adapters.catalogdb.rest.VnfRestImpl;
 import org.onap.so.logging.jaxrs.filter.SOAuditLogContainerFilter;
 import org.springframework.context.annotation.Configuration;
-import io.swagger.jaxrs.config.BeanConfig;
-import io.swagger.jaxrs.listing.ApiListingResource;
-import io.swagger.jaxrs.listing.SwaggerSerializers;
+import io.swagger.v3.jaxrs2.integration.JaxrsOpenApiContextBuilder;
+import io.swagger.v3.jaxrs2.integration.resources.AcceptHeaderOpenApiResource;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+import io.swagger.v3.oas.integration.OpenApiConfigurationException;
+import io.swagger.v3.oas.integration.SwaggerConfiguration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 @ApplicationPath("/ecomp/mso/catalog")
@@ -42,18 +48,25 @@ public class JerseyConfiguration extends ResourceConfig {
     public void setUp() {
         System.setProperty(Constants.Property.PARTNER_NAME, ONAPComponents.CATALOG_DB.toString());
         register(CatalogDbAdapterRest.class);
-        register(ApiListingResource.class);
-        register(SwaggerSerializers.class);
         register(SOAuditLogContainerFilter.class);
+        register(OpenApiResource.class);
+        register(AcceptHeaderOpenApiResource.class);
         register(ServiceRestImpl.class);
         register(VnfRestImpl.class);
-        BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("1.0.2");
-        beanConfig.setSchemes(new String[] {"https"});
-        beanConfig.setBasePath("/ecomp/mso/catalog");
-        beanConfig.setResourcePackage("org.onap.so.adapters.catalogdb.rest");
-        beanConfig.setPrettyPrint(true);
-        beanConfig.setScan(true);
+        
+        OpenAPI oas = new OpenAPI();
+        Info info = new Info();
+        info.title("Swagger catalog-db-adapter bootstrap code");
+        info.setVersion("1.0.2");
+
+        SwaggerConfiguration oasConfig = new SwaggerConfiguration().openAPI(oas).prettyPrint(true)
+                .resourcePackages(Stream.of("org.onap.so.adapters.catalogdb.rest").collect(Collectors.toSet()));
+
+        try {
+            new JaxrsOpenApiContextBuilder().application(this).openApiConfiguration(oasConfig).buildContext(true);
+        } catch (OpenApiConfigurationException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
     }
 }
 
