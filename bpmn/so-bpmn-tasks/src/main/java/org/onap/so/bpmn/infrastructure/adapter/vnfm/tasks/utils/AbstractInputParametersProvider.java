b1e3dfbe168feb94ce1daf7e2543e2f10b4a89a7
waqas.ikram <waqas.ikram@est.tech>
2019-10-16 13:59:46 +0100

    Adding preload capability using userParams
    Change-Id: Id107f2c9c2da643ecb57daaef26dfa2bfd89f8aa
    Issue-ID: SO-1635
    Signed-off-by: waqas.ikram <waqas.ikram@est.tech>

---
+++ bpmn/so-bpmn-tasks/src/main/java/org/onap/so/bpmn/infrastructure/adapter/vnfm/tasks/utils/AbstractInputParametersProvider.java
@@ -0,0 +1,66 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2019 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.so.bpmn.infrastructure.adapter.vnfm.tasks.utils;
+
+import static org.onap.so.bpmn.infrastructure.adapter.vnfm.tasks.Constants.ADDITIONAL_PARAMS;
+import static org.onap.so.bpmn.infrastructure.adapter.vnfm.tasks.Constants.EXT_VIRTUAL_LINKS;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import org.onap.vnfmadapter.v1.model.ExternalVirtualLink;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * @author Waqas Ikram (waqas.ikram@ericsson.com)
+ *
+ */
+public abstract class AbstractInputParametersProvider<T> implements InputParametersProvider<T> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractInputParametersProvider.class);
+
+    protected Map<String, String> parseAdditionalParameters(final String additionalParamsString) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final TypeReference<Map<String, String>> typeRef = new TypeReference<Map<String, String>>() {};
+            return mapper.readValue(additionalParamsString, typeRef);
+        } catch (final Exception exception) {
+            LOGGER.error("Unable to parse {} ", ADDITIONAL_PARAMS, exception);
+        }
+        return Collections.emptyMap();
+
+    }
+
+    protected List<ExternalVirtualLink> parseExternalVirtualLinks(final String extVirtualLinksString) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final TypeReference<List<ExternalVirtualLink>> extVirtualLinksStringTypeRef =
+                    new TypeReference<List<ExternalVirtualLink>>() {};
+            return mapper.readValue(extVirtualLinksString, extVirtualLinksStringTypeRef);
+        } catch (final Exception exception) {
+            LOGGER.error("Unable to parse {} ", EXT_VIRTUAL_LINKS, exception);
+        }
+        return Collections.emptyList();
+    }
+
+
+}
