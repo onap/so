adab78c118e6ce0bf28c02ae261b706fdb51d95f
prathamesh <prathamesh_morde@yahoo.ca>
2019-10-21 14:26:08 -0400

    WIP- SO-CDS Generic Building block
    Things done-
    -Added logic to create payload using scope and action.
    -Populate instance parameters from SO-request to CDS payload for VNF.
    -Fixed couple of TODOs.
    -Added ControllerExecutionBB.
    -Implementation of setting orchestration status after CDS completes its operation based on SO scope.
    
    Things in progress-
    -Unit test coverage for updateOrchestrationStatusForCds method.
    -Working on fixing TODOs.
    
    Things to do-
    -Build template-prefix string for VF-Module(config-assign/deploy)
    -Populate instance parameters from SO-request to CDS for VF-Module.
    -Setting orchestration status after CDS completes its operation for VF-Module.
    
    Issue-ID: SO-2312
    Signed-off-by: prathamesh <prathamesh_morde@yahoo.ca>
    Change-Id: I026d9f7909e486c68f274b7e56c8bd7c8fdcd521
    Signed-off-by: prathamesh <prathamesh_morde@yahoo.ca>

---
+++ bpmn/so-bpmn-tasks/src/main/java/org/onap/so/bpmn/infrastructure/flowspecific/tasks/GenericCDSProcessing.java
@@ -0,0 +1,78 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2019 Bell Canada
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.bpmn.infrastructure.flowspecific.tasks;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.onap.so.bpmn.common.BuildingBlockExecution;
+import org.onap.so.bpmn.servicedecomposition.tasks.ExtractPojosForBB;
+import org.onap.so.client.cds.GeneratePayloadForCds;
+import org.onap.so.client.cds.beans.AbstractCDSPropertiesBean;
+import org.onap.so.client.exception.ExceptionBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GenericCDSProcessing {
+
+    private static final Logger logger = LoggerFactory.getLogger(GenericCDSProcessing.class);
+    private final ExceptionBuilder exceptionBuilder;
+    private final ExtractPojosForBB extractPojosForBB;
+
+    @Autowired
+    public GenericCDSProcessing(ExceptionBuilder exceptionBuilder, ExtractPojosForBB extractPojosForBB) {
+        this.exceptionBuilder = exceptionBuilder;
+        this.extractPojosForBB = extractPojosForBB;
+    }
+
+    // Use this method for components which supports BuildingBlock.
+    public void buildPayloadBasedOnScopeAndAction(BuildingBlockExecution buildingBlockExecution) {
+        try {
+            GeneratePayloadForCds configurePayloadForCds = new GeneratePayloadForCds(buildingBlockExecution, extractPojosForBB);
+
+            AbstractCDSPropertiesBean abstractCDSPropertiesBean = configurePayloadForCds.buildCdsPropertiesBean();
+
+            buildingBlockExecution.setVariable("executionObject", abstractCDSPropertiesBean);
+
+        } catch (Exception ex) {
+            logger.error("An exception occurred when creating payload for CDS request", ex);
+            exceptionBuilder.buildAndThrowWorkflowException(buildingBlockExecution, 7000, ex);
+        }
+    }
+
+    // For PNF currently we do not have BuildingBlockExecution therefore we have to pass scope and action through BPMN
+    // process.
+    public void buildPayloadBasedOnScopeAndAction(DelegateExecution delegateExecution, String scope, String action) {
+        try {
+
+            GeneratePayloadForCds configurePayloadForCds = new GeneratePayloadForCds(delegateExecution, scope, action);
+
+            AbstractCDSPropertiesBean abstractCDSPropertiesBean = configurePayloadForCds.buildCdsPropertiesBean();
+
+            delegateExecution.setVariable("executionObject", abstractCDSPropertiesBean);
+
+        } catch (Exception ex) {
+            logger.error("An exception occurred when creating payload for CDS request", ex);
+            exceptionBuilder.buildAndThrowWorkflowException(delegateExecution, 7000, ex);
+        }
+    }
+}
