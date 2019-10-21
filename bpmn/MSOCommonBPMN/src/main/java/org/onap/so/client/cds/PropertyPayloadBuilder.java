c2bc238b847c4c03259a56e713ab2dbbe6649a8c
prathamesh <prathamesh_morde@yahoo.ca>
2019-10-21 14:26:08 -0400

    WIP- SO-CDS Generic Building block
    Things done-
    -Added logic to create payload using scope and action.
    -Unit test coverage for payload creation logic.
    -Populate instance parameters from SO-request to CDS payload.
    -Fixed couple of TODOs
    
    Things to do-
    - Generic implementation of setting orchestration status after CDS completes its operation.
    -Build template-prefix string for VF-Module(config-assign/deploy)
    -Continue working on remaining TODOs.
    -Add ControllerExecutionBB or GenericCDSBB.
    
    Issue-ID: SO-2312
    Signed-off-by: prathamesh <prathamesh_morde@yahoo.ca>
    Change-Id: I026d9f7909e486c68f274b7e56c8bd7c8fdcd521
    Signed-off-by: prathamesh <prathamesh_morde@yahoo.ca>

---
+++ bpmn/MSOCommonBPMN/src/main/java/org/onap/so/client/cds/PropertyPayloadBuilder.java
@@ -0,0 +1,48 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2019 Bell Canada
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.so.client.cds;
+
+import com.google.gson.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import static org.onap.so.client.cds.PayloadConstants.CONFIG;
+import static org.onap.so.client.cds.PayloadConstants.PROPERTIES;
+
+public class PropertyPayloadBuilder {
+
+    private static final Logger logger = LoggerFactory.getLogger(PropertyPayloadBuilder.class);
+
+    /**
+     * Build property payload.
+     * 
+     * @param action - SO action
+     * @param scope - SO scope
+     * @param jsonObject - An object comprises configuration properties for CDS.
+     * @return "config-<action>-<scope></scope>-properties":{ // Configuration properties }
+     */
+    public static String buildConfigProperties(String action, String scope, JsonObject jsonObject) {
+        String propertyBuilder = "\"" + CONFIG + action + "-" + scope + PROPERTIES + "\"" + ":" + jsonObject + "}";
+
+        logger.info("The property payload is {}", propertyBuilder);
+
+        return propertyBuilder;
+    }
+}
+
