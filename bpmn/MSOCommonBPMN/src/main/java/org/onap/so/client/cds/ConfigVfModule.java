c039d2d81f75116210a33e4bfbe7100e4dc23b3f
prathamesh <prathamesh_morde@yahoo.ca>
2019-10-21 14:26:08 -0400

    WIP- SO-CDS Generic Building block
    Things done-
    -Added logic to create payload using scope and action.
    -Populate instance parameters from SO-request to CDS payload for VNF.
    -Fixed couple of TODOs.
    -Added ControllerExecutionBB.
    -Implementation of setting orchestration status after CDS completes its operation based on SO scope.
    
    Things in progress-
    -Unit test coverage for updateOrchestrationStatusForCds method.
    -Working on fixing TODOs.
    
    Things to do-
    -Build template-prefix string for VF-Module(config-assign/deploy)
    -Populate instance parameters from SO-request to CDS for VF-Module.
    -Setting orchestration status after CDS completes its operation for VF-Module.
    
    Issue-ID: SO-2312
    Signed-off-by: prathamesh <prathamesh_morde@yahoo.ca>
    Change-Id: I026d9f7909e486c68f274b7e56c8bd7c8fdcd521
    Signed-off-by: prathamesh <prathamesh_morde@yahoo.ca>

---
+++ bpmn/MSOCommonBPMN/src/main/java/org/onap/so/client/cds/ConfigVfModule.java
@@ -0,0 +1,105 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2019 Bell Canada
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.client.cds;
+
+import com.google.gson.JsonObject;
+import org.onap.so.bpmn.common.BuildingBlockExecution;
+import org.onap.so.bpmn.servicedecomposition.bbobjects.GenericVnf;
+import org.onap.so.bpmn.servicedecomposition.bbobjects.ServiceInstance;
+import org.onap.so.bpmn.servicedecomposition.bbobjects.VfModule;
+import org.onap.so.bpmn.servicedecomposition.entities.ResourceKey;
+import org.onap.so.bpmn.servicedecomposition.tasks.ExtractPojosForBB;
+import static org.onap.so.client.cds.PayloadConstants.VFMODULE_SCOPE;
+import static org.onap.so.client.cds.PayloadConstants.CONFIG;
+import java.util.Optional;
+
+public class ConfigVfModule implements VirtualComponent {
+
+    private JsonObject vfModuleObject;
+    private String blueprintName;
+    private String blueprintVersion;
+    private String action;
+    private BuildingBlockExecution execution;
+    private ExtractPojosForBB extractPojosForBB;
+
+    ConfigVfModule(ExtractPojosForBB extractPojosForBB) {
+        this.extractPojosForBB = extractPojosForBB;
+    }
+
+    @Override
+    public Optional<String> buildRequestPayload(String action) throws Exception {
+        this.action = action;
+
+        vfModuleObject = buildPropertyObjectForVfModule(extractPojosForBB);
+
+        // TODO Need to figure out for vf module how I can build resolution key.
+        String requestBuilder = "{\"" + CONFIG + action + "-" + VFMODULE_SCOPE + "-request\":{" + "\"resolution-key\":"
+                + "\"" + "resolutionKey" + "\"," + "\"template-prefix\":" + "\"" + getTemplatePrefix() + "\","
+                + PropertyPayloadBuilder.buildConfigProperties(action, VFMODULE_SCOPE, vfModuleObject) + '}';
+
+        return Optional.of(requestBuilder);
+    }
+
+    @Override
+    public String getBlueprintName() {
+        return blueprintName;
+    }
+
+    @Override
+    public String getBlueprintVersion() {
+        return blueprintVersion;
+    }
+
+    @Override
+    public <T> void setExecutionObject(T executionObject) {
+        execution = (BuildingBlockExecution) executionObject;
+    }
+
+    private JsonObject buildPropertyObjectForVfModule(ExtractPojosForBB extractPojosForBB) throws Exception {
+        vfModuleObject = new JsonObject();
+
+        ServiceInstance serviceInstance = extractPojosForBB.extractByKey(execution, ResourceKey.SERVICE_INSTANCE_ID);
+        GenericVnf genericVnf = extractPojosForBB.extractByKey(execution, ResourceKey.GENERIC_VNF_ID);
+
+        blueprintName = genericVnf.getBlueprintName();
+        blueprintVersion = genericVnf.getBlueprintVersion();
+
+        VfModule vfModule = extractPojosForBB.extractByKey(execution, ResourceKey.VF_MODULE_ID);
+
+        vfModuleObject.addProperty("service-instance-id", serviceInstance.getServiceInstanceId());
+        vfModuleObject.addProperty("service-model-uuid", serviceInstance.getModelInfoServiceInstance().getModelUuid());
+        vfModuleObject.addProperty("vnf-id", genericVnf.getVnfId());
+        vfModuleObject.addProperty("vnf-name", genericVnf.getVnfName());
+        vfModuleObject.addProperty("vf-module-id", vfModule.getVfModuleId());
+        vfModuleObject.addProperty("vf-module-name", vfModule.getVfModuleName());
+        vfModuleObject.addProperty("vf-module-customization-uuid",
+                vfModule.getModelInfoVfModule().getModelCustomizationUUID());
+
+        // TODO Add instance parameters based on action for VF-Module.
+
+        return vfModuleObject;
+    }
+
+    // TODO Add logic to create template-prefix.
+    private String getTemplatePrefix() {
+        return "template-prefix";
+    }
+}
---
+++ bpmn/MSOCommonBPMN/src/main/java/org/onap/so/client/cds/GeneratePayloadForCds.java
@@ -0,0 +1,105 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2019 TechMahindra.
+ * Copyright (C) 2019 Nokia.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.client.cds;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.onap.so.bpmn.common.BuildingBlockExecution;
+import org.onap.so.bpmn.servicedecomposition.entities.BuildingBlock;
+import org.onap.so.bpmn.servicedecomposition.entities.ExecuteBuildingBlock;
+import org.onap.so.bpmn.servicedecomposition.tasks.ExtractPojosForBB;
+import org.onap.so.client.cds.beans.AbstractCDSPropertiesBean;
+import java.util.Optional;
+import java.util.UUID;
+
+public class GeneratePayloadForCds {
+
+    private static final String ORIGINATOR_ID = "SO";
+    private static final String MODE = "sync";
+    private BuildingBlockExecution execution;
+    private String scope;
+    private String action;
+    private VirtualComponent configuration;
+    private DelegateExecution delegateExecution;
+    private static final String BUILDING_BLOCK = "buildingBlock";
+    private ExtractPojosForBB extractPojosForBB;
+
+    public GeneratePayloadForCds(BuildingBlockExecution execution, ExtractPojosForBB extractPojosForBB) {
+        this.execution = execution;
+
+        ExecuteBuildingBlock executeBuildingBlock = execution.getVariable(BUILDING_BLOCK);
+        BuildingBlock buildingBlock = executeBuildingBlock.getBuildingBlock();
+
+        this.scope = buildingBlock.getBpmnScope();
+        this.action = buildingBlock.getBpmnAction();
+
+        this.extractPojosForBB = extractPojosForBB;
+
+        // Setting scope and action for UpdateAAI BPMN process.
+        execution.setVariable("scope", scope);
+        execution.setVariable("action", action);
+    }
+
+    public GeneratePayloadForCds(DelegateExecution execution, String scope, String action) {
+        this.delegateExecution = execution;
+        this.scope = scope;
+        this.action = action;
+    }
+
+    /**
+     * Build the payload based on SO scope and action.
+     *
+     * @return Payload to push to CDS.
+     */
+    protected Optional<String> generateConfigPropertiesPayload() throws Exception {
+        switch (scope) {
+            case "vnf":
+                configuration = new ConfigVnf(extractPojosForBB);
+                configuration.setExecutionObject(execution);
+                return configuration.buildRequestPayload(action);
+
+            case "vf-module":
+                configuration = new ConfigVfModule(extractPojosForBB);
+                configuration.setExecutionObject(execution);
+                return configuration.buildRequestPayload(action);
+        }
+
+        return Optional.empty();
+    }
+
+    public AbstractCDSPropertiesBean buildCdsPropertiesBean() throws Exception {
+        final AbstractCDSPropertiesBean cdsPropertiesBean = new AbstractCDSPropertiesBean();
+        final String requestPayload =
+                generateConfigPropertiesPayload().orElseThrow(() -> new Exception("Failed to build payload for CDS"));
+        final String requestId = execution.getGeneralBuildingBlock().getRequestContext().getMsoRequestId();
+
+        cdsPropertiesBean.setRequestObject(requestPayload);
+        cdsPropertiesBean.setBlueprintName(configuration.getBlueprintName());
+        cdsPropertiesBean.setBlueprintVersion(configuration.getBlueprintVersion());
+        cdsPropertiesBean.setRequestId(requestId);
+        cdsPropertiesBean.setOriginatorId(ORIGINATOR_ID);
+        cdsPropertiesBean.setSubRequestId(UUID.randomUUID().toString());
+        cdsPropertiesBean.setActionName(action);
+        cdsPropertiesBean.setMode(MODE);
+
+        return cdsPropertiesBean;
+    }
+}
