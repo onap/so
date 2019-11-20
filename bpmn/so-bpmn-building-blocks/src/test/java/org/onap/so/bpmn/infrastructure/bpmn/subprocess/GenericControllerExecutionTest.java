cca9031d05688ddee04d2ab361fe95a04d51445b
eeginux <henry.xie@est.tech>
2019-11-20 13:23:49 +0000

    decision point API
    ControllerRunnable interface:implemented by controller
    ControllerContext:Context for controller execution
    ControllerPreparable interface:used to setup execution context
    ControllerExecutionBB:controller execution for building block
    ControllerExecutionDE:controller execution for camunda
    Sample implementation for CDS, APPC controller.
    Use ControllerExecutionDE for existing PNF configuration.
    Add integration tests for controllerExecutionBB/DE
    Add GenericControllerExecution activity for BuildingBlockExecution based
    Add GenericControllerExecutionDE activity for DelegateExecution based.
    
    TODO 1: Actor selection based on the ingested metadata
    
    Issue-ID: SO-2070
    Change-Id: I4020c2ce21468939690e2cef78bbadbfff4bd3e4
    Signed-off-by: eeginux<henry.xie@est.tech>

---
+++ bpmn/so-bpmn-building-blocks/src/test/java/org/onap/so/bpmn/infrastructure/bpmn/subprocess/GenericControllerExecutionTest.java
@@ -0,0 +1,54 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2019 Nordix
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.bpmn.infrastructure.bpmn.subprocess;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.junit.Test;
+import org.onap.so.bpmn.BaseBPMNTest;
+import org.onap.so.bpmn.common.BuildingBlockExecution;
+
+
+public class GenericControllerExecutionTest extends BaseBPMNTest {
+
+    @Test
+    public void testExecution_validInput_expectedExecution() {
+
+        ProcessInstance pi = runtimeService.startProcessInstanceByKey("GenericControllerExecution", variables);
+        assertThat(pi).isNotNull();
+        assertThat(pi).isStarted().hasPassedInOrder("Start_ControllerExecutionBB", "Call_ControllerExecutionBB",
+                "End_ControllerExecutionBB");
+        assertThat(pi).isEnded();
+    }
+
+    @Test
+    public void testExecution_failedExecution_exceptionThrown() {
+        doThrow(new BpmnError("7000", "TESTING ERRORS")).when(controllerExecutionBB)
+                .execute(any(BuildingBlockExecution.class));
+        ProcessInstance pi = runtimeService.startProcessInstanceByKey("GenericControllerExecution", variables);
+        assertThat(pi).isNotNull();
+        assertThat(pi).isStarted().hasPassedInOrder("Start_ControllerExecutionBB", "Call_ControllerExecutionBB")
+                .hasNotPassed("End_ControllerExecutionBB");
+        assertThat(pi).isEnded();
+    }
+}
---
+++ bpmn/so-bpmn-tasks/src/test/java/org/onap/so/bpmn/infrastructure/decisionpoint/impl/camunda/MockControllerDE.java
@@ -0,0 +1,55 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2019 Nordix
+ *  ================================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *  ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.bpmn.infrastructure.decisionpoint.impl.camunda;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.onap.so.bpmn.common.BuildingBlockExecution;
+import org.onap.so.bpmn.infrastructure.decisionpoint.api.ControllerContext;
+import org.onap.so.bpmn.infrastructure.decisionpoint.api.ControllerRunnable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MockControllerDE implements ControllerRunnable<DelegateExecution> {
+
+    public static final String TEST_ACTOR = "test-controller";
+    public static final String TEST_ACTION = "configuration";
+
+    @Override
+    public Boolean understand(ControllerContext<DelegateExecution> context) {
+        return context.getControllerAction().equalsIgnoreCase(TEST_ACTION)
+                && context.getControllerActor().equalsIgnoreCase(TEST_ACTOR);
+    }
+
+    @Override
+    public Boolean ready(ControllerContext<DelegateExecution> context) {
+        context.getExecution().setVariable("ready", true);
+        return true;
+    }
+
+    @Override
+    public void prepare(ControllerContext<DelegateExecution> context) {
+        context.getExecution().setVariable("prepare", true);
+    }
+
+    @Override
+    public void run(ControllerContext<DelegateExecution> context) {
+        context.getExecution().setVariable("run", true);
+    }
+}
