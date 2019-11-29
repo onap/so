1c33083ee0fc05ee3c914434c0d006e1e3dee5dc
Pooja03 <pm00501616@techmahindra.com>
2019-11-29 20:59:35 +0530

    WIP- Based on Controller Actor calling CDS or APPC(Current Flow)
    Considering the below patch as merged
    https://gerrit.onap.org/r/#/c/so/+/98418/
    
    https://gerrit.onap.org/r/#/c/so/+/97391/
    
    -Added BBNameSelectionReference table having bbName along with
    action,scope,actor
    -Updated CatalogDbClient to use BBNameSelectionReference Table
    -Updated ControllerExecution bmpn to select flow as per Controller Actor
    -Corrected Format Error
    
    TODO:
    -unit test cases
    
    Issue-ID: SO-2316
    Signed-off-by: Pooja03 <pm00501616@techmahindra.com>
    Change-Id: I21fad846249f773308e34abcac134d3ee0694027

--- bpmn/so-bpmn-tasks/src/main/java/org/onap/so/bpmn/infrastructure/flowspecific/tasks/ControllerExecution.java
+++ bpmn/so-bpmn-tasks/src/main/java/org/onap/so/bpmn/infrastructure/flowspecific/tasks/ControllerExecution.java
@@ -1,7 +1,26 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2019  Tech Mahindra
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
 package org.onap.so.bpmn.infrastructure.flowspecific.tasks;
 
 import java.util.Optional;
-
 import org.onap.so.bpmn.common.BuildingBlockExecution;
 import org.onap.so.bpmn.servicedecomposition.bbobjects.GenericVnf;
 import org.onap.so.bpmn.servicedecomposition.entities.BuildingBlock;
@@ -19,55 +38,65 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ControllerExecution {
-	private static final Logger logger = LoggerFactory.getLogger(ControllerExecution.class);
-	private static final String CONTROLLER_ACTOR = "controller_actor";
-	private static final String BUILDING_BLOCK = "buildingBlock";
-	@Autowired
-	private ExceptionBuilder exceptionUtil;
-	@Autowired
-	private CatalogDbClient catalogDbClient;
-	@Autowired
-	private ExtractPojosForBB extractPojosForBB;
-	private String scope;
-	private String action;
+    private static final Logger logger = LoggerFactory.getLogger(ControllerExecution.class);
+    private static final String CONTROLLER_ACTOR = "controller_actor";
+    private static final String BUILDING_BLOCK = "buildingBlock";
+    @Autowired
+    private ExceptionBuilder exceptionUtil;
+    @Autowired
+    private CatalogDbClient catalogDbClient;
+    @Autowired
+    private ExtractPojosForBB extractPojosForBB;
+    private String scope;
+    private String action;
 
-	public void setControllerActorScopeAction(BuildingBlockExecution execution) {
-		try {
-			GenericVnf genericVnf = extractPojosForBB.extractByKey(execution, ResourceKey.GENERIC_VNF_ID);
-			String modleUuid = genericVnf.getModelInfoGenericVnf().getModelCustomizationUuid();
-			VnfResourceCustomization vnfResourceCustomization = catalogDbClient
-					.getVnfResourceCustomizationByModelCustomizationUUID(modleUuid);
 
-			// Fetching Controller Actor at VNF level if null then Actor is set as APPC
-			String controller_actor = Optional.ofNullable(vnfResourceCustomization.getControllerActor()).orElse("APPC");
-			ExecuteBuildingBlock executeBuildingBlock = execution.getVariable(BUILDING_BLOCK);
-			BuildingBlock buildingBlock = executeBuildingBlock.getBuildingBlock();
-			this.scope = buildingBlock.getBpmnScope();
-			this.action = buildingBlock.getBpmnAction();
-			execution.setVariable("scope", scope);
-			execution.setVariable("action", action);
-			execution.setVariable(CONTROLLER_ACTOR, controller_actor);
+    /**
+     * Setting Controller Actor, Scope and Action Variables in BuildingBlockExecution object
+     * 
+     * @param execution
+     */
+    public void setControllerActorScopeAction(BuildingBlockExecution execution) {
+        try {
+            GenericVnf genericVnf = extractPojosForBB.extractByKey(execution, ResourceKey.GENERIC_VNF_ID);
+            String modleUuid = genericVnf.getModelInfoGenericVnf().getModelCustomizationUuid();
+            VnfResourceCustomization vnfResourceCustomization =
+                    catalogDbClient.getVnfResourceCustomizationByModelCustomizationUUID(modleUuid);
 
-		} catch (Exception ex) {
-			logger.error("An exception occurred when fetching Controller Actor from Service ", ex);
-			exceptionUtil.buildAndThrowWorkflowException(execution, 7000, ex);
+            // Fetching Controller Actor at VNF level if null then Actor is set as APPC
+            String controller_actor = Optional.ofNullable(vnfResourceCustomization.getControllerActor()).orElse("APPC");
+            ExecuteBuildingBlock executeBuildingBlock = execution.getVariable(BUILDING_BLOCK);
+            BuildingBlock buildingBlock = executeBuildingBlock.getBuildingBlock();
+            this.scope = buildingBlock.getBpmnScope();
+            this.action = buildingBlock.getBpmnAction();
+            execution.setVariable("scope", scope);
+            execution.setVariable("action", action);
+            execution.setVariable(CONTROLLER_ACTOR, controller_actor);
 
-		}
-	}
+        } catch (Exception ex) {
+            logger.error("An exception occurred when fetching Controller Actor from Service ", ex);
+            exceptionUtil.buildAndThrowWorkflowException(execution, 7000, ex);
+        }
+    }
 
-	public void selectBB(BuildingBlockExecution execution) {
-		try {
+    /**
+     * Selecting bbName from BBNameSelectionReference and setting the value in a variable of BuildingBlockExecution
+     * 
+     * @param execution
+     */
+    public void selectBB(BuildingBlockExecution execution) {
+        try {
 
-			String actor = execution.getVariable(CONTROLLER_ACTOR);
-			BBNameSelectionReference bbNameSelectionReference = catalogDbClient.getBBNameSelectionReference(actor,
-					scope, action);
-			String bbName = bbNameSelectionReference.getBB_NAME();
-			execution.setVariable("bbName", bbName);
-		} catch (Exception ex) {
-			logger.error("An exception occurred while getting bbname from catalogdb ", ex);
-			exceptionUtil.buildAndThrowWorkflowException(execution, 7000, ex);
+            String actor = execution.getVariable(CONTROLLER_ACTOR);
+            BBNameSelectionReference bbNameSelectionReference =
+                    catalogDbClient.getBBNameSelectionReference(actor, scope, action);
+            String bbName = bbNameSelectionReference.getBB_NAME();
+            execution.setVariable("bbName", bbName);
+        } catch (Exception ex) {
+            logger.error("An exception occurred while getting bbname from catalogdb ", ex);
+            exceptionUtil.buildAndThrowWorkflowException(execution, 7000, ex);
 
-		}
+        }
 
-	}
+    }
 }
