66368717a849eb7071f67eb14f5427a24e1dec26
Pooja03 <pm00501616@techmahindra.com>
2019-12-03 16:22:25 +0530

    WIP- Based on Controller Actor calling CDS or APPC(Current Flow)
    Considering the below patch as merged
    https://gerrit.onap.org/r/#/c/so/+/98418/
    
    https://gerrit.onap.org/r/#/c/so/+/97391/
    
    -Added BBNameSelectionReference table having bbName along with
    action,scope,actor
    -Updated CatalogDbClient to use BBNameSelectionReference Table
    -Updated ControllerExecution bmpn to select flow as per Controller Actor
    -Corrected Format Error
    
    TODO:
    -unit test cases
    
    Issue-ID: SO-2316
    Signed-off-by: Pooja03 <pm00501616@techmahindra.com>
    Change-Id: I21fad846249f773308e34abcac134d3ee0694027

---
+++ mso-catalog-db/src/main/java/org/onap/so/db/catalog/beans/BBNameSelectionReference.java
@@ -0,0 +1,106 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2019  Tech Mahindra
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.db.catalog.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+@Entity
+@Table(name = "BBNameSelectionReference")
+public class BBNameSelectionReference {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "ID", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer ID;
+
+    @Column(name = "ACTOR")
+    private String ControllerActor;
+
+    @Column(name = "SCOPE")
+    private String scope;
+
+    @Column(name = "ACTION")
+    private String action;
+
+    @Column(name = "BB_NAME")
+    private String bb_name;
+
+    public String getControllerActor() {
+        return ControllerActor;
+    }
+
+    public void setControllerActor(String controllerActor) {
+        ControllerActor = controllerActor;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getBB_NAME() {
+        return bb_name;
+    }
+
+    public void setBB_NAME(String bB_NAME) {
+        bb_name = bB_NAME;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(ControllerActor).append(bb_name).append(scope).append(action).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof BBNameSelectionReference)) {
+            return false;
+        }
+        BBNameSelectionReference castOther = (BBNameSelectionReference) other;
+        return new EqualsBuilder().append(bb_name, castOther.bb_name).isEquals();
+    }
+
+
+
+}
