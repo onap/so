5a590c735f11de2a36400b4ca77cf1e2c19c3265
Pooja03 <pm00501616@techmahindra.com>
2019-12-03 16:22:25 +0530

    WIP- Based on Controller Actor calling CDS or APPC(Current Flow)
    Considering the below patch as merged
    https://gerrit.onap.org/r/#/c/so/+/98418/
    
    https://gerrit.onap.org/r/#/c/so/+/97391/
    
    -Added BBNameSelectionReference table having bbName along with
    action,scope,actor
    -Updated CatalogDbClient to use BBNameSelectionReference Table
    -Updated ControllerExecution bmpn to select flow as per Controller Actor
    -Corrected Format Error
    
    TODO:
    -unit test cases
    
    Issue-ID: SO-2316
    Signed-off-by: Pooja03 <pm00501616@techmahindra.com>
    Change-Id: I21fad846249f773308e34abcac134d3ee0694027

---
+++ adapters/mso-catalog-db-adapter/src/main/resources/db/migration/V8.1__AddBBNameSelectionReference.sql
@@ -0,0 +1,16 @@
+use catalogdb;
+
+CREATE TABLE IF NOT EXISTS `BBNameSelectionReference` (
+  `ID` INT(11) NOT NULL AUTO_INCREMENT,
+  `ACTOR` varchar(200) NOT NULL,
+  `SCOPE` varchar(200) NOT NULL,
+  `ACTION` varchar(200) NOT NULL,
+  `BB_NAME` varchar(200) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO BBNameSelectionReference (ACTOR,SCOPE,ACTION,BB_NAME)
+VALUES
+('APPC', 'VfModule', 'HealthCheck','GenericVnfHealthCheckBB'),
+('APPC', 'VfModule', 'ScaleOut','ConfigurationScaleOutBB'),
+('APPC', 'Vnf', 'HealthCheck','GenericVnfHealthCheckBB');
