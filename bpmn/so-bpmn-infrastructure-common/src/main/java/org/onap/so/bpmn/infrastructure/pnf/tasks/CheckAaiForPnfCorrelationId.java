1dc7f38c53c7b8c0eb00330239eef108997d2898
Remigiusz Janeczek <remigiusz.janeczek@nokia.com>
2020-01-09 12:23:18 +0100

    Align AssignPnfBB with Service Macro Create 	-move and align logic of BB delegates with tests 	-remove unnecessary classes 	-add tests cases for Pnfs in ExtractPojosForBBTests
    Issue-ID: SO-2568
    Signed-off-by: Remigiusz Janeczek <remigiusz.janeczek@nokia.com>
    Change-Id: Ic48e2c1c6ab852c33837091da3e7916b71c26466

---
+++ bpmn/so-bpmn-infrastructure-common/src/main/java/org/onap/so/bpmn/infrastructure/pnf/tasks/CheckAaiForPnfCorrelationId.java
@@ -0,0 +1,50 @@
+package org.onap.so.bpmn.infrastructure.pnf.tasks;
+
+import org.onap.so.bpmn.common.BuildingBlockExecution;
+import org.onap.so.bpmn.infrastructure.pnf.management.PnfManagement;
+import org.onap.so.bpmn.servicedecomposition.bbobjects.Pnf;
+import org.onap.so.bpmn.servicedecomposition.entities.ResourceKey;
+import org.onap.so.bpmn.servicedecomposition.tasks.ExtractPojosForBB;
+import org.onap.so.client.exception.BBObjectNotFoundException;
+import org.onap.so.client.exception.ExceptionBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+import static org.onap.so.bpmn.infrastructure.pnf.delegate.ExecutionVariableNames.AAI_CONTAINS_INFO_ABOUT_PNF;
+import static org.onap.so.bpmn.infrastructure.pnf.delegate.ExecutionVariableNames.PNF_CORRELATION_ID;
+
+@Component
+public class CheckAaiForPnfCorrelationId extends PnfBaseTasks {
+    private static final Logger logger = LoggerFactory.getLogger(CheckAaiForPnfCorrelationId.class);
+
+    @Override
+    public void execute(BuildingBlockExecution execution) {
+        try {
+            String pnfCorrelationId = extractPnf(execution).getPnfName();
+            checkIfPnfCorrelationIdPresent(execution, pnfCorrelationId);
+            checkIfPnfExistsInAai(execution, pnfCorrelationId);
+        } catch (BBObjectNotFoundException e) {
+            exceptionUtil.buildAndThrowWorkflowException(execution, 7000, e);
+        }
+
+    }
+
+    private void checkIfPnfCorrelationIdPresent(BuildingBlockExecution execution, String pnfCorrelationId) {
+        if (pnfCorrelationId == null) {
+            exceptionUtil.buildAndThrowWorkflowException(execution, 500, PNF_CORRELATION_ID + " is not set");
+        }
+    }
+
+    private void checkIfPnfExistsInAai(BuildingBlockExecution execution, String pnfCorrelationId) {
+        try {
+            boolean isEntry = pnfManagement.getEntryFor(pnfCorrelationId).isPresent();
+            logger.debug("AAI entry is found for pnf correlation id {}: {}", PNF_CORRELATION_ID, isEntry);
+            execution.setVariable(AAI_CONTAINS_INFO_ABOUT_PNF, isEntry);
+        } catch (IOException e) {
+            logger.error("Exception in check AAI for pnf_correlation_id execution", e);
+            exceptionUtil.buildAndThrowWorkflowException(execution, 9999, e);
+        }
+    }
+}
