b1698c4efad8b45ef99995dcb5c93417cd20e4c0
Piotr Borelowski <p.borelowski@partner.samsung.com>
2020-01-15 17:58:27 +0100

    Added the communication with AAI
    Ve-Vnfm (SOL002) Adapter project
    
    Issue-ID: SO-2574
    Signed-off-by: Piotr Borelowski <p.borelowski@partner.samsung.com>
    Change-Id: I32a1a560d2787b966f1634b3679368412af19520

---
+++ adapters/mso-ve-vnfm-adapter/src/test/java/org/onap/so/adapters/vevnfm/subscription/SubscribeSenderTest.java
@@ -0,0 +1,77 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SO
+ * ================================================================================
+ * Copyright (C) 2020 Samsung. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.adapters.vevnfm.subscription;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.onap.so.adapters.vevnfm.service.StartupService;
+import org.onap.so.adapters.vnfmadapter.extclients.vnfm.model.LccnSubscriptionRequest;
+import org.onap.so.rest.service.HttpRestServiceProvider;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SubscribeSenderTest {
+
+    @Mock
+    private HttpRestServiceProvider restProvider;
+
+    @InjectMocks
+    private SubscribeSender sender;
+
+    @Test
+    public void testSuccess() {
+        final ResponseEntity<String> response = ResponseEntity.status(HttpStatus.CREATED).body("{}");
+        assertTrue(testingSend(response));
+    }
+
+    @Test
+    public void testFailure() {
+        final ResponseEntity<String> response = new ResponseEntity(HttpStatus.BAD_REQUEST);
+        assertFalse(testingSend(response));
+    }
+
+    private boolean testingSend(final ResponseEntity<String> response) {
+        // given
+        when(restProvider.postHttpRequest(any(), anyString(), eq(String.class))).thenReturn(response);
+
+        final LccnSubscriptionRequest request = new LccnSubscriptionRequest();
+        final Map vnfm = new LinkedHashMap<String, String>();
+        vnfm.put(StartupService.URL, "lh");
+
+        // when
+        final boolean done = sender.send(vnfm, request);
+
+        // then
+        verify(restProvider, times(1)).postHttpRequest(any(), anyString(), eq(String.class));
+
+        return done;
+    }
+}
