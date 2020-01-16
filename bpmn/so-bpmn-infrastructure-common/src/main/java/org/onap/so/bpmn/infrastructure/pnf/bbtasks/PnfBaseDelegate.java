e02546eb0acc253c136822e46009f115b8eb22b4
Remigiusz Janeczek <remigiusz.janeczek@nokia.com>
2020-01-16 12:02:45 +0100

    Change AssignPnfBB to use JavaDelegates
    Issue-ID: SO-2568
    Signed-off-by: Remigiusz Janeczek <remigiusz.janeczek@nokia.com>
    Change-Id: I179c102189fc638b9a27660509c6984d59c09810

--- bpmn/so-bpmn-infrastructure-common/src/main/java/org/onap/so/bpmn/infrastructure/pnf/bbtasks/PnfBaseDelegate.java
+++ bpmn/so-bpmn-infrastructure-common/src/main/java/org/onap/so/bpmn/infrastructure/pnf/bbtasks/PnfBaseDelegate.java
@@ -1,5 +1,7 @@
-package org.onap.so.bpmn.infrastructure.pnf.tasks;
+package org.onap.so.bpmn.infrastructure.pnf.bbtasks;
 
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.onap.so.bpmn.common.BuildingBlockExecution;
 import org.onap.so.bpmn.infrastructure.pnf.management.PnfManagement;
 import org.onap.so.bpmn.servicedecomposition.bbobjects.Pnf;
@@ -9,19 +11,26 @@ import org.onap.so.client.exception.BBObjectNotFoundException;
 import org.onap.so.client.exception.ExceptionBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public abstract class PnfBaseTasks {
+public abstract class PnfBaseDelegate implements JavaDelegate {
     protected PnfManagement pnfManagement;
     @Autowired
     protected ExceptionBuilder exceptionUtil;
     @Autowired
     protected ExtractPojosForBB extractPojosForBB;
 
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        BuildingBlockExecution gBuildingBlockExecution =
+                (BuildingBlockExecution) delegateExecution.getVariable("gBuildingBlockExecution");
+        execute(gBuildingBlockExecution);
+    }
+
+    protected abstract void execute(BuildingBlockExecution gBuildingBlockExecution) throws Exception;
+
     @Autowired
     public void setPnfManagement(PnfManagement pnfManagement) {
         this.pnfManagement = pnfManagement;
     }
-
-    public abstract void execute(BuildingBlockExecution execution) throws Exception;
 
     protected Pnf extractPnf(BuildingBlockExecution execution) throws BBObjectNotFoundException {
         return extractPojosForBB.extractByKey(execution, ResourceKey.PNF);
