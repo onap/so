2d1b0e0341dd68681c7af2fb39194c3e638e0c31
Joanna Jeremicz <joanna.jeremicz@nokia.com>
2020-01-21 13:30:45 +0100

    AssignPnfBB connects PNF and service instance in AAI
    Change-Id: I92ab4b45acb120d647ea3de7a455e2e873c12a72
    Issue-ID: SO-2605
    Signed-off-by: Joanna Jeremicz <joanna.jeremicz@nokia.com>

--- bpmn/so-bpmn-tasks/src/main/java/org/onap/so/client/aai/mapper/AAIObjectMapper.java
+++ bpmn/so-bpmn-tasks/src/main/java/org/onap/so/client/aai/mapper/AAIObjectMapper.java
@@ -4,6 +4,8 @@
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ * Modifications Copyright (c) 2020 Nokia
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -85,6 +87,10 @@ public class AAIObjectMapper {
         }
 
         return modelMapper.map(vnf, org.onap.aai.domain.yang.GenericVnf.class);
+    }
+
+    public org.onap.aai.domain.yang.Pnf mapPnf(Pnf pnf) {
+        return modelMapper.map(pnf, org.onap.aai.domain.yang.Pnf.class);
     }
 
     public org.onap.aai.domain.yang.VfModule mapVfModule(VfModule vfModule) {
