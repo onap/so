86bbcec53197dc5ba3788d208b90c1d55cbcf401
Piotr Borelowski <p.borelowski@partner.samsung.com>
2020-01-21 09:23:23 +0100

    Ve-Vnfm (SOL002) Adapter fix
    Changed the <parent> in pom.xml to use org.onap.so:adapters
    
    Issue-ID: SO-2574
    Signed-off-by: Piotr Borelowski <p.borelowski@partner.samsung.com>
    Change-Id: Ia049de7a757126df1fba8cc3adc1913ef2b927fd

--- adapters/mso-ve-vnfm-adapter/src/test/java/org/onap/so/adapters/vevnfm/controller/NotificationControllerTest.java
+++ adapters/mso-ve-vnfm-adapter/src/test/java/org/onap/so/adapters/vevnfm/controller/NotificationControllerTest.java
@@ -24,7 +24,6 @@ import org.junit.Test;
 import org.onap.so.adapters.vnfmadapter.extclients.vnfm.lcn.model.VnfLcmOperationOccurrenceNotification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import static org.junit.Assert.assertEquals;
 
 public class NotificationControllerTest {
@@ -33,8 +32,7 @@ public class NotificationControllerTest {
 
     @Test
     public void testReceiveNotification() {
-        final VnfLcmOperationOccurrenceNotification notification
-                = new VnfLcmOperationOccurrenceNotification();
+        final VnfLcmOperationOccurrenceNotification notification = new VnfLcmOperationOccurrenceNotification();
         final ResponseEntity response = controller.receiveNotification(notification);
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
