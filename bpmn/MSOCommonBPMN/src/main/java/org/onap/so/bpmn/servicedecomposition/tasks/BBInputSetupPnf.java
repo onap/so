5d1206c2a5b9d3af542d54199a4651c4b7a552a5
Joanna Jeremicz <joanna.jeremicz@nokia.com>
2020-02-04 13:21:31 +0100

    AssignPnfBB - store model related PNF parameters in AAI
    Issue-ID: SO-2640
    Change-Id: Ie30c928e730ab858e9fe4ac5a4b14cebf6c0f806
    Signed-off-by: Joanna Jeremicz <joanna.jeremicz@nokia.com>

---
+++ bpmn/MSOCommonBPMN/src/main/java/org/onap/so/bpmn/servicedecomposition/tasks/BBInputSetupPnf.java
@@ -0,0 +1,44 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2020 Nokia Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+
+package org.onap.so.bpmn.servicedecomposition.tasks;
+
+import org.onap.so.bpmn.servicedecomposition.bbobjects.Pnf;
+import org.onap.so.bpmn.servicedecomposition.bbobjects.ServiceInstance;
+import org.onap.so.bpmn.servicedecomposition.modelinfo.ModelInfoPnf;
+import org.onap.so.db.catalog.beans.OrchestrationStatus;
+import org.onap.so.serviceinstancebeans.Pnfs;
+
+final class BBInputSetupPnf {
+    static Pnf populatePnf(Pnfs pnfs, String pnfId, ServiceInstance serviceInstance) {
+        Pnf pnf = new Pnf();
+        pnf.setPnfId(pnfId);
+        pnf.setPnfName(pnfs.getInstanceName());
+        pnf.setModelInfoPnf(new ModelInfoPnf());
+        pnf.getModelInfoPnf().setModelCustomizationUuid(pnfs.getModelInfo().getModelCustomizationId());
+        pnf.getModelInfoPnf().setModelInvariantUuid(pnfs.getModelInfo().getModelInvariantId());
+        pnf.getModelInfoPnf().setModelUuid(pnfs.getModelInfo().getModelVersionId());
+        pnf.setOrchestrationStatus(OrchestrationStatus.PRECREATED);
+
+        serviceInstance.getPnfs().add(pnf);
+        return pnf;
+    }
+}
---
+++ bpmn/MSOCommonBPMN/src/test/java/org/onap/so/bpmn/servicedecomposition/tasks/BBInputSetupPnfTest.java
@@ -0,0 +1,70 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2020 Nokia Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+
+package org.onap.so.bpmn.servicedecomposition.tasks;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.onap.so.bpmn.servicedecomposition.bbobjects.Pnf;
+import org.onap.so.bpmn.servicedecomposition.bbobjects.ServiceInstance;
+import org.onap.so.db.catalog.beans.OrchestrationStatus;
+import org.onap.so.serviceinstancebeans.ModelInfo;
+import org.onap.so.serviceinstancebeans.Pnfs;
+import static org.mockito.Mockito.doReturn;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BBInputSetupPnfTest {
+
+    @Mock
+    private Pnfs pnfs;
+
+    @Mock
+    private ModelInfo modelInfo;
+
+    @Test
+    public void populatePnfShouldSetRequiredFields() {
+        final String pnfId = "PNF_id1";
+        final String pnfName = "PNF_name1";
+        final String modelCustomizationId = "8421fe03-fd1b-4bf7-845a-c3fe91edb031";
+        final String modelInvariantId = "3360a2a5-22ff-44c7-8935-08c8e5ecbd06";
+        final String modelVersionId = "b80c3a52-abd4-436c-a22e-9c5da768781a";
+
+        doReturn(modelCustomizationId).when(modelInfo).getModelCustomizationId();
+        doReturn(modelInvariantId).when(modelInfo).getModelInvariantId();
+        doReturn(modelVersionId).when(modelInfo).getModelVersionId();
+        doReturn(pnfName).when(pnfs).getInstanceName();
+        doReturn(modelInfo).when(pnfs).getModelInfo();
+
+        ServiceInstance serviceInstance = new ServiceInstance();
+        Pnf pnf = BBInputSetupPnf.populatePnf(pnfs, pnfId, serviceInstance);
+
+        assertEquals(pnf.getPnfId(), pnfId);
+        assertEquals(pnf.getPnfName(), pnfName);
+        assertEquals(pnf.getModelInfoPnf().getModelCustomizationUuid(), modelCustomizationId);
+        assertEquals(pnf.getModelInfoPnf().getModelInvariantUuid(), modelInvariantId);
+        assertEquals(pnf.getModelInfoPnf().getModelUuid(), modelVersionId);
+        assertEquals(pnf.getOrchestrationStatus(), OrchestrationStatus.PRECREATED);
+        assertEquals(serviceInstance.getPnfs().get(0), pnf);
+    }
+}
