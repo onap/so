948d7716b9c6c2d66ebeffa78080273d87ed1577
Tomasz Wrobel <tomasz.wrobel@nokia.com>
2020-02-06 08:46:30 +0100

    Fix Sonar issue - throw generic exception.
    Issue-ID: SO-1841
    Signed-off-by: Tomasz Wrobel <tomasz.wrobel@nokia.com>
    Change-Id: Ia24bdb36f03a1b96a356ef27fe1dca95e48adb7c

--- bpmn/so-bpmn-tasks/src/main/java/org/onap/so/client/orchestration/VnfAdapterVolumeGroupResources.java
+++ bpmn/so-bpmn-tasks/src/main/java/org/onap/so/client/orchestration/VnfAdapterVolumeGroupResources.java
@@ -6,6 +6,8 @@
  * ================================================================================
  * Modifications Copyright (c) 2019 Samsung
  * ================================================================================
+ * Modifications Copyright (c) 2020 Nokia
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -32,10 +34,9 @@ import org.onap.so.bpmn.servicedecomposition.generalobjects.OrchestrationContext
 import org.onap.so.bpmn.servicedecomposition.generalobjects.RequestContext;
 import org.onap.so.client.adapter.vnf.VnfVolumeAdapterClientImpl;
 import org.onap.so.client.adapter.vnf.mapper.VnfAdapterObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import java.io.IOException;
 
 @Component
 public class VnfAdapterVolumeGroupResources {
@@ -48,13 +49,13 @@ public class VnfAdapterVolumeGroupResources {
 
     public CreateVolumeGroupRequest createVolumeGroupRequest(RequestContext requestContext, CloudRegion cloudRegion,
             OrchestrationContext orchestrationContext, ServiceInstance serviceInstance, GenericVnf genericVnf,
-            VolumeGroup volumeGroup, String sdncVfModuleQueryResponse) throws Exception {
+            VolumeGroup volumeGroup, String sdncVfModuleQueryResponse) throws IOException {
         return vnfAdapterObjectMapper.createVolumeGroupRequestMapper(requestContext, cloudRegion, orchestrationContext,
                 serviceInstance, genericVnf, volumeGroup, sdncVfModuleQueryResponse);
     }
 
     public DeleteVolumeGroupRequest deleteVolumeGroupRequest(RequestContext requestContext, CloudRegion cloudRegion,
-            ServiceInstance serviceInstance, VolumeGroup volumeGroup) throws Exception {
+            ServiceInstance serviceInstance, VolumeGroup volumeGroup) throws IOException {
         return vnfAdapterObjectMapper.deleteVolumeGroupRequestMapper(requestContext, cloudRegion, serviceInstance,
                 volumeGroup);
     }
