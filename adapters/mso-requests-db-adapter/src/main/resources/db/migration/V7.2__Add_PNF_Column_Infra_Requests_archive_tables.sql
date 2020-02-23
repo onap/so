54e2d3f3f76613dc3a8a79160b00c119e4ed8713
Benjamin, Max <max.benjamin@att.com>
2020-02-22 20:52:22 -0500

    Rename migration script
    renamed script because you cannot change flyway scripts
    
    Issue-ID: SO-2675
    Signed-off-by: Benjamin, Max (mb388a) <mb388a@att.com>
    Change-Id: Ie60f6b081b4af8d21353268d3f73137c8776f597

--- adapters/mso-requests-db-adapter/src/main/resources/db/migration/V7.2__Add_PNF_Column_Infra_Requests_archive_tables.sql
+++ adapters/mso-requests-db-adapter/src/main/resources/db/migration/V7.2__Add_PNF_Column_Infra_Requests_archive_tables.sql
@@ -1,4 +1,4 @@
 use requestdb;
 
-ALTER TABLE infra_active_requests ADD COLUMN IF NOT EXISTS PNF_NAME varchar(45);
-ALTER TABLE archived_infra_requests ADD COLUMN IF NOT EXISTS PNF_NAME varchar(45);
+ALTER TABLE infra_active_requests ADD COLUMN IF NOT EXISTS PNF_ID varchar(45);
+ALTER TABLE archived_infra_requests ADD COLUMN IF NOT EXISTS PNF_ID varchar(45);
