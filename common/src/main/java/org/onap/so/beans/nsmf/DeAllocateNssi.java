42e12b38705bbbe16a1cd0099dd63d955539727f
jananib <janani.b@huawei.com>
2020-02-25 13:57:55 +0530

    Deallocate Request Bean
    Bean addition for NSSMF adapter
    
    Change-Id: I7966b60a04dcacd074420e44ea7d67a21ba18a42
    Issue-ID: SO-2544
    Signed-off-by: jananib <janani.b@huawei.com>

---
+++ common/src/main/java/org/onap/so/beans/nsmf/DeAllocateNssi.java
@@ -0,0 +1,91 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2020 Huawei Technologies Co., Ltd. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.beans.nsmf;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DeAllocateNssi {
+
+    public final static String URL = "/api/rest/provMns/v1/NSS" + "/SliceProfiles/%s";
+
+    private String nsiId;
+
+    private String nssiId;
+
+    private List<String> snssaiList;
+
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    private int terminateNssiOption;
+
+    private String scriptName;
+
+    private String extension;
+
+    public String getNsiId() {
+        return nsiId;
+    }
+
+    public void setNsiId(String nsiId) {
+        this.nsiId = nsiId;
+    }
+
+    public String getNssiId() {
+        return nssiId;
+    }
+
+    public void setNssiId(String nssiId) {
+        this.nssiId = nssiId;
+    }
+
+    public List<String> getSnssaiList() {
+        return snssaiList;
+    }
+
+    public void setSnssaiList(List<String> snssaiList) {
+        this.snssaiList = snssaiList;
+    }
+
+    public int getTerminateNssiOption() {
+        return terminateNssiOption;
+    }
+
+    public void setTerminateNssiOption(int terminateNssiOption) {
+        this.terminateNssiOption = terminateNssiOption;
+    }
+
+    public String getScriptName() {
+        return scriptName;
+    }
+
+    public void setScriptName(String scriptName) {
+        this.scriptName = scriptName;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+}
