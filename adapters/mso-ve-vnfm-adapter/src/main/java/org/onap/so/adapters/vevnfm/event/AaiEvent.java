54a4057295ea8883dcf7b06aa390d6de2f69b20f
Piotr Borelowski <p.borelowski@partner.samsung.com>
2020-02-28 13:59:45 +0100

    Basic ETSI -> DMaaP translation added
    Ve-Vnfm (SOL002) Adapter project
    
    Issue-ID: SO-2574
    Signed-off-by: Piotr Borelowski <p.borelowski@partner.samsung.com>
    Change-Id: Ia838887fad65099d37c37fc6b71f0f2f092e4a10

---
+++ adapters/mso-ve-vnfm-adapter/src/main/java/org/onap/so/adapters/vevnfm/event/AaiEvent.java
@@ -0,0 +1,44 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SO
+ * ================================================================================
+ * Copyright (C) 2020 Samsung. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.adapters.vevnfm.event;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class AaiEvent {
+
+    private final boolean vserverIsClosedLoopDisabled;
+    private final String genericVnfVnfId;
+
+    public AaiEvent(final boolean cld, final String id) {
+        this.vserverIsClosedLoopDisabled = cld;
+        this.genericVnfVnfId = id;
+    }
+
+    @JsonProperty("vserver.is-closed-loop-disabled")
+    public boolean isVserverIsClosedLoopDisabled() {
+        return vserverIsClosedLoopDisabled;
+    }
+
+    @JsonProperty("generic-vnf.vnf-id")
+    public String getGenericVnfVnfId() {
+        return genericVnfVnfId;
+    }
+}
