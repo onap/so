695d6ddacfec85cb541fefb7b293b24a51910b92
hetengjiao <hetengjiao@chinamobile.com>
2020-03-04 10:43:27 +0800

    Add scripts for ActivateSliceService workflow
    Issue-ID: SO-2368
    
    Change-Id: I537dc0be5b4a483ef67ff838d9659d809f244225
    Signed-off-by: hetengjiao <hetengjiao@chinamobile.com>

---
+++ common/src/main/java/org/onap/so/beans/nsmf/NSSI.java
@@ -0,0 +1,60 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ # Copyright (c) 2019, CMCC Technologies Co., Ltd.
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License")
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #       http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.beans.nsmf;
+
+public class NSSI {
+
+	private String nssiId;
+
+	private String modelInvariantId;
+
+	private String modelVersionId;
+
+	public NSSI(String nssiId, String modelInvariantId, String modelVersionId) {
+		this.nssiId = nssiId;
+		this.modelInvariantId = modelInvariantId;
+		this.modelVersionId = modelVersionId;
+	}
+
+	public String getNssiId() {
+		return nssiId;
+	}
+
+	public void setNssiId(String nssiId) {
+		this.nssiId = nssiId;
+	}
+
+	public String getModelInvariantId() {
+		return modelInvariantId;
+	}
+
+	public void setModelInvariantId(String modelInvariantId) {
+		this.modelInvariantId = modelInvariantId;
+	}
+
+	public String getModelVersionId() {
+		return modelVersionId;
+	}
+
+	public void setModelVersionId(String modelVersionId) {
+		this.modelVersionId = modelVersionId;
+	}
+}
