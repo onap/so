e868a5559666b458cca129be7d840b9c585cfd76
Piotr Borelowski <p.borelowski@partner.samsung.com>
2020-03-27 14:41:15 +0100

    Notification filter
    Ve-Vnfm (SOL002) Adapter project
    - checking if VNF Id is registered in AAI
    - checking if list from ESR is not null
    
    Issue-ID: SO-2574
    Signed-off-by: Piotr Borelowski <p.borelowski@partner.samsung.com>
    Change-Id: Iad2c5d9bc2921221fcf0304db73baee6bbaa0c77

---
+++ adapters/mso-ve-vnfm-adapter/src/test/java/org/onap/so/adapters/vevnfm/service/VnfAaiCheckerTest.java
@@ -0,0 +1,88 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * SO
+ * ================================================================================
+ * Copyright (C) 2020 Samsung. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.adapters.vevnfm.service;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.endsWith;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.onap.so.adapters.vevnfm.aai.AaiConnection;
+import org.onap.so.adapters.vevnfm.constant.VnfNotificationFilterType;
+
+@RunWith(MockitoJUnitRunner.class)
+public class VnfAaiCheckerTest {
+
+    private static final String VNF_ID = "t5h78w";
+
+    @Mock
+    private AaiConnection aaiConnection;
+
+    @InjectMocks
+    private VnfAaiChecker checker;
+
+    @Test
+    public void testAll() {
+        // when
+        final boolean response = checker.vnfCheck(VnfNotificationFilterType.ALL, VNF_ID);
+
+        // then
+        assertTrue(response);
+    }
+
+    @Test
+    public void testAaiCheckedPresent() {
+        // given
+        when(aaiConnection.receiveGenVnf(eq(VNF_ID))).thenReturn(VNF_ID);
+
+        // when
+        final boolean response = checker.vnfCheck(VnfNotificationFilterType.AAI_CHECKED, VNF_ID);
+
+        // then
+        assertTrue(response);
+    }
+
+    @Test
+    public void testAaiCheckedAbsent() {
+        // given
+        when(aaiConnection.receiveGenVnf(eq(VNF_ID))).thenReturn(null);
+
+        // when
+        final boolean response = checker.vnfCheck(VnfNotificationFilterType.AAI_CHECKED, VNF_ID);
+
+        // then
+        assertFalse(response);
+    }
+
+    @Test
+    public void testNone() {
+        // when
+        final boolean response = checker.vnfCheck(VnfNotificationFilterType.NONE, VNF_ID);
+
+        // then
+        assertFalse(response);
+    }
+}
