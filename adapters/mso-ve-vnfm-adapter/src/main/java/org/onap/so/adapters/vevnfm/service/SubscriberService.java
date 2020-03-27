0f1aa5c1a7b975b1e0c9f768247114378beff269
Piotr Borelowski <p.borelowski@partner.samsung.com>
2020-03-27 14:41:15 +0100

    Notification and Subscription filters
    Ve-Vnfm (SOL002) Adapter project
    - checking if VNF Id is registered in AAI
    - checking if list from ESR is not null
    - sending the filter in subscription
    
    Issue-ID: SO-2574
    Signed-off-by: Piotr Borelowski <p.borelowski@partner.samsung.com>
    Change-Id: Iad2c5d9bc2921221fcf0304db73baee6bbaa0c77

--- adapters/mso-ve-vnfm-adapter/src/main/java/org/onap/so/adapters/vevnfm/service/SubscriberService.java
+++ adapters/mso-ve-vnfm-adapter/src/main/java/org/onap/so/adapters/vevnfm/service/SubscriberService.java
@@ -20,6 +20,7 @@
 
 package org.onap.so.adapters.vevnfm.service;
 
+import com.google.gson.Gson;
 import com.squareup.okhttp.Credentials;
 import java.util.Collections;
 import org.apache.logging.log4j.util.Strings;
@@ -29,12 +30,18 @@ import org.onap.so.adapters.vevnfm.provider.AuthorizationHeadersProvider;
 import org.onap.so.adapters.vnfmadapter.extclients.vnfm.model.LccnSubscriptionRequest;
 import org.onap.so.adapters.vnfmadapter.extclients.vnfm.model.SubscriptionsAuthentication;
 import org.onap.so.adapters.vnfmadapter.extclients.vnfm.model.SubscriptionsAuthenticationParamsBasic;
+import org.onap.so.adapters.vnfmadapter.extclients.vnfm.model.SubscriptionsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SubscriberService {
+
+    private static final Gson gson = new Gson();
+
+    @Value("${vevnfmadapter.vnf-filter-json}")
+    private String vnfFilter;
 
     @Value("${vevnfmadapter.endpoint}")
     private String endpoint;
@@ -91,6 +98,7 @@ public class SubscriberService {
 
     private LccnSubscriptionRequest createRequest() {
         final LccnSubscriptionRequest request = new LccnSubscriptionRequest();
+        request.filter(getFilter());
         request.callbackUri(getCallbackUri());
         final SubscriptionsAuthenticationParamsBasic paramsBasic = new SubscriptionsAuthenticationParamsBasic();
         final SubscriptionsAuthentication authentication = new SubscriptionsAuthentication();
@@ -101,6 +109,10 @@ public class SubscriberService {
         request.authentication(authentication);
 
         return request;
+    }
+
+    private SubscriptionsFilter getFilter() {
+        return gson.fromJson(vnfFilter, SubscriptionsFilter.class);
     }
 
     private String getCallbackUri() {
