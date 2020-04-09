b7c9cb537a3d78a26d8fc1210f774c0bc8c8b19b
Sebastien Premont-Tendland <sebastien.premont@bell.ca>
2020-04-09 10:07:56 -0400

    Fix transition states for the BB workflow.
    Deleted the versioned flyway file in favor of the repeatable
    one for MacroData. I know deleting a versioned file could mess up
    upgrade but given no release has been done with those new versioned files
    it should be ok and cleaner.
    
    This is a list of transition states that were added internally at Bell
    when working with the BB workflow and post instantiation.
    
    Those are to support Rollback and Delete even if the VNF is in the status
    ConfigAssigned, Configure or Configured.
    The missing transition states was causing the Rollback or Delete to fail.
    
    Issue-ID: SO-2798
    Signed-off-by: Sebastien Premont-Tendland <sebastien.premont@bell.ca>
    Change-Id: I21c11298a456a472cedc7a8733c6dbc3a071f3a6
    (cherry picked from commit b1e92221cbed0131b0788e993ee224a42283a8a2)

--- adapters/mso-catalog-db-adapter/src/main/resources/db/migration/V9.0__AddConfiguredForVnfOrchestrationStatus.sql
+++ adapters/mso-catalog-db-adapter/src/main/resources/db/migration/V9.0__AddConfiguredForVnfOrchestrationStatus.sql
@@ -1,2 +1,5 @@
+<<<<<<< HEAD   (f360ba resolve problem CreateCommunicationService SST is null)
 INSERT INTO orchestration_status_state_transition_directive(RESOURCE_TYPE, ORCHESTRATION_STATUS, TARGET_ACTION, FLOW_DIRECTIVE)
 VALUES ('VNF', 'CONFIGURED', 'ACTIVATE', 'CONTINUE');
+=======
+>>>>>>> CHANGE (b1e922 Fix transition states for the BB workflow.)
