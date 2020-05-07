1fd109aa953886adb4cf34748216025f37926483
Rupinder <rupinsi1@in.ibm.com>
2020-05-07 11:38:41 +0530

    removed unused imports lowered code smells
    Issue-ID: SO-2804
    Change-Id: Id2c98451cf522e8c4ffde592b12340e8c8bab577
    Signed-off-by: Rupinder <rupinsi1@in.ibm.com>

--- mso-api-handlers/mso-requests-db/src/main/java/org/onap/so/db/request/beans/OperationalEnvDistributionStatus.java
+++ mso-api-handlers/mso-requests-db/src/main/java/org/onap/so/db/request/beans/OperationalEnvDistributionStatus.java
@@ -22,14 +22,9 @@ package org.onap.so.db.request.beans;
 
 import java.io.Serializable;
 import java.util.Date;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
