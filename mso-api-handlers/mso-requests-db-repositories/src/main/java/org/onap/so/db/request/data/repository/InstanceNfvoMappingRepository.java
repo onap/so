f38320d8de9db861a6ba2d60b4feda0d1205f257
Rupinder <rupinsi1@in.ibm.com>
2020-05-07 11:30:47 +0530

    removed unused imports
    Issue-ID: SO-2804
    Change-Id: I24397aa0cd1f9481b92f3adfbfa56897c49e2b5c
    Signed-off-by: Rupinder <rupinsi1@in.ibm.com>

--- mso-api-handlers/mso-requests-db-repositories/src/main/java/org/onap/so/db/request/data/repository/InstanceNfvoMappingRepository.java
+++ mso-api-handlers/mso-requests-db-repositories/src/main/java/org/onap/so/db/request/data/repository/InstanceNfvoMappingRepository.java
@@ -18,6 +18,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
 @RepositoryRestResource(collectionResourceRel = "instanceNfvoMapping", path = "instanceNfvoMapping")
 public interface InstanceNfvoMappingRepository extends JpaRepository<InstanceNfvoMapping, String> {
 
--- mso-api-handlers/mso-requests-db-repositories/src/main/java/org/onap/so/db/request/data/repository/SiteStatusRepository.java
+++ mso-api-handlers/mso-requests-db-repositories/src/main/java/org/onap/so/db/request/data/repository/SiteStatusRepository.java
@@ -23,7 +23,6 @@ package org.onap.so.db.request.data.repository;
 import org.onap.so.db.request.beans.SiteStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 
 @RepositoryRestResource(collectionResourceRel = "siteStatus", path = "siteStatus")
 public interface SiteStatusRepository extends JpaRepository<SiteStatus, String> {
