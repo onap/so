38df5bc712f77abd0b16dd15faf9563bec0e3a67
Brittany Plummer (bp896r) <bp896r@att.com>
2020-05-14 18:39:43 -0400

    Added primary annotation to asyncExecutor so that executor
    Issue-ID: SO-2913
    Change-Id: I7418f1279ff105cd2a47593dea0d26c0fed69cc9
    Signed-off-by: Brittany Plummer (bp896r) <bp896r@att.com>

--- adapters/mso-openstack-adapters/src/main/java/org/onap/so/adapters/openstack/MsoOpenstackAdaptersApplication.java
+++ adapters/mso-openstack-adapters/src/main/java/org/onap/so/adapters/openstack/MsoOpenstackAdaptersApplication.java
@@ -27,6 +27,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -63,6 +64,7 @@ public class MsoOpenstackAdaptersApplication {
     }
 
     @Bean
+    @Primary
     public Executor asyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setTaskDecorator(new MDCTaskDecorator());
