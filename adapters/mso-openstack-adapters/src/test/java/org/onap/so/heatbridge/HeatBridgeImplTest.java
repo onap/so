a06fb5e6ecc13c0b9742819c710f762fed2b79ac
Lukasz Rajewski <lukasz.rajewski@orange.com>
2020-05-26 16:57:47 +0200

    Add missing relationship in heatbridge
    Added missing relationship creation
    between v-server and generic-vnf
    
    Issue-ID: SO-2956
    Signed-off-by: Lukasz Rajewski <lukasz.rajewski@orange.com>
    Change-Id: I8c74f078d7073041492b8c03ce6adb26823dec51

--- adapters/mso-openstack-adapters/src/test/java/org/onap/so/heatbridge/HeatBridgeImplTest.java
+++ adapters/mso-openstack-adapters/src/test/java/org/onap/so/heatbridge/HeatBridgeImplTest.java
@@ -320,15 +320,15 @@ public class HeatBridgeImplTest {
 
         org.onap.aai.domain.yang.RelationshipList relList = aaiHelper.getVserverRelationshipList(CLOUD_OWNER, REGION_ID,
                 "test-genericVnf-id", "test-vfModule-id", server1);
-        assertEquals(2, relList.getRelationship().size());
+        assertEquals(3, relList.getRelationship().size());
 
         org.onap.aai.domain.yang.RelationshipList relList2 = aaiHelper.getVserverRelationshipList(CLOUD_OWNER,
                 REGION_ID, "test-genericVnf-id", "test-vfModule-id", server2);
-        assertEquals(1, relList2.getRelationship().size());
+        assertEquals(2, relList2.getRelationship().size());
 
         org.onap.aai.domain.yang.RelationshipList relList3 = aaiHelper.getVserverRelationshipList(CLOUD_OWNER,
                 REGION_ID, "test-genericVnf-id", "test-vfModule-id", server3);
-        assertEquals(1, relList3.getRelationship().size());
+        assertEquals(2, relList3.getRelationship().size());
     }
 
     @Test
