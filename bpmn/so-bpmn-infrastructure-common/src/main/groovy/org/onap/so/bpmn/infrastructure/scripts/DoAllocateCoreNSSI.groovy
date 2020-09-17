c7caf208e96a9f1f7d9fd43bb3c3d2cb1347a399
aleemraja <ar00500721@techmahindra.com>
2020-09-17 13:41:56 +0530

    Implement Allocate Core NSSMF workflow
    Issue-ID: SO-3234
    Signed-off-by: aleemraja <ar00500721@techmahindra.com>
    Change-Id: I37ce7d21eaba8ea60a91a7034509696b2cf3a987

---
+++ bpmn/so-bpmn-infrastructure-common/src/main/groovy/org/onap/so/bpmn/infrastructure/scripts/DoAllocateCoreNSSI.groovy
@@ -0,0 +1,157 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2020  Tech Mahindra
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.bpmn.infrastructure.scripts
+import org.apache.commons.collections.map.HashedMap
+import org.camunda.bpm.engine.delegate.BpmnError
+import org.camunda.bpm.engine.delegate.DelegateExecution
+import org.onap.so.beans.nsmf.SliceTaskParams
+import org.onap.so.bpmn.common.scripts.AbstractServiceTaskProcessor
+import org.onap.so.bpmn.common.scripts.CatalogDbUtils
+import org.onap.so.bpmn.common.scripts.CatalogDbUtilsFactory
+import org.onap.so.bpmn.common.scripts.ExceptionUtil
+import org.onap.so.bpmn.core.domain.ServiceDecomposition
+import org.onap.so.bpmn.core.domain.ServiceProxy
+import org.onap.so.bpmn.core.json.JsonUtils
+import org.onap.so.db.request.beans.OperationStatus
+import org.onap.so.db.request.beans.ResourceOperationStatus
+import org.onap.so.serviceinstancebeans.ModelInfo
+import org.onap.so.bpmn.core.UrnPropertiesReader
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import static org.apache.commons.lang3.StringUtils.*;
+import com.fasterxml.jackson.databind.ObjectMapper
+import groovy.json.JsonSlurper
+import javax.ws.rs.core.Response
+import org.onap.so.bpmn.common.scripts.OofUtils
+import org.onap.so.bpmn.common.scripts.RequestDBUtil
+
+class DoAllocateCoreNSSI extends AbstractServiceTaskProcessor {
+    String Prefix="DACNSSI_"
+    private static final Logger logger = LoggerFactory.getLogger( DoAllocateCoreNSSI.class);
+    ExceptionUtil exceptionUtil = new ExceptionUtil()
+    CatalogDbUtils catalogDbUtils = new CatalogDbUtilsFactory().create()
+    JsonUtils jsonUtil = new JsonUtils()
+    RequestDBUtil requestDBUtil = new RequestDBUtil()
+    ObjectMapper mapper = new ObjectMapper()
+    OofUtils oofUtils = new OofUtils()
+    void preProcessRequest(DelegateExecution execution) {
+        logger.debug(Prefix+" **** Enter DoAllocateCoreNSSI ::: preProcessRequest ****")
+        execution.setVariable("prefix", Prefix)
+        String msg = ""
+        //Get SliceProfile from sliceParams JSON
+        String sliceProfile = jsonUtil.getJsonValue(execution.getVariable("sliceParams"), "sliceProfile")
+        if (isBlank(sliceProfile)) {
+            msg = "Slice Profile is null"
+            exceptionUtil.buildAndThrowWorkflowException(execution, 500, msg)
+        } else {
+            execution.setVariable("sliceProfile", sliceProfile)
+        }
+        String coreServiceInstanceId = UUID.randomUUID().toString()
+        execution.setVariable("coreServiceInstanceId", coreServiceInstanceId)
+        logger.debug(Prefix+" **** Exit DoAllocateCoreNSSI ::: preProcessRequest ****")
+    }
+    /**
+     * Query NSST name from CatalogDB
+     * @param execution
+     */
+    void getNSSTName(DelegateExecution execution){
+        logger.debug(Prefix+" **** Enter DoAllocateCoreNSSI ::: getNSSTName ****")
+        String nsstModelInvariantUuid = execution.getVariable("modelInvariantUuid")
+        try{
+            String json = catalogDbUtils.getServiceResourcesByServiceModelInvariantUuidString(execution, nsstModelInvariantUuid)
+            logger.debug("***** JSON Response is: "+json)
+            String nsstName = jsonUtil.getJsonValue(json, "serviceResources.modelInfo.modelName") ?: ""
+            String networkServiceModelInfo = jsonUtil.getJsonValue(json, "serviceResources.serviceProxy.modelInfo") ?: ""
+
+            execution.setVariable("networkServiceModelInfo", networkServiceModelInfo)
+            logger.debug("***** nsstName is: "+ nsstName)
+            execution.setVariable("nsstName",nsstName)
+        }catch(BpmnError e){
+            throw e
+        } catch (Exception ex){
+            String msg = "Exception in preProcessRequest " + ex.getMessage()
+            logger.debug(msg)
+            exceptionUtil.buildAndThrowWorkflowException(execution, 7000, msg)
+        }
+        logger.debug(Prefix+" **** Exit DoAllocateCoreNSSI ::: getNSSTName ****")
+    }
+    void prepareOOFRequest(DelegateExecution execution){
+        logger.debug(Prefix+" **** Enter DoAllocateCoreNSSI ::: prepareOOFRequest ****")
+        //API Path
+        String apiPath =  "/api/oof/selection/nssi/v1"
+        logger.debug("API path for DoAllocateCoreNSSI: "+apiPath)
+        execution.setVariable("NSSI_apiPath", apiPath)
+        //Setting correlator as requestId
+        String requestId = execution.getVariable("msoRequestId")
+        execution.setVariable("NSSI_correlator", requestId)
+        //Setting messageType for all Core slice as cn
+        String messageType = "cn"
+        execution.setVariable("NSSI_messageType", messageType)
+        //Is there any specific timeout we have to set or else we don't need to send
+        //if blank will be set default value in DoHandleOofRequest
+        String timeout = "PT30M"
+        execution.setVariable("NSSI_timeout", timeout)
+        Map<String, Object> profileInfo = mapper.readValue(execution.getVariable("sliceProfile"), Map.class)
+        String nsstModelUuid = execution.getVariable("modelUuid")
+        String nsstModelInvariantUuid = execution.getVariable("modelInvariantUuid")
+        String nsstName = execution.getVariable("nsstName")
+        String oofRequest = oofUtils.buildSelectNSSIRequest(requestId, messageType, nsstModelUuid, nsstModelInvariantUuid, nsstName, profileInfo)
+        logger.debug("**** OOfRequest for Core Slice: "+oofRequest)
+        execution.setVariable("NSSI_oofRequest", oofRequest)
+        logger.debug(Prefix+" **** Exit DoAllocateCoreNSSI ::: prepareOOFRequest ****")
+    }
+
+    void processOOFAsyncResponse(DelegateExecution execution) {
+        logger.debug(Prefix+ " **** Enter DoAllocateCoreNSSI ::: processOOFAsyncResponse ****")
+        String OOFResponse = execution.getVariable("NSSI_asyncCallbackResponse")
+        String requestStatus = jsonUtil.getJsonValue(OOFResponse, "requestStatus")
+        logger.debug("NSSI OOFResponse is: " + OOFResponse)
+        execution.setVariable("OOFResponse", OOFResponse)
+        String solutions =""
+        if(requestStatus.equals("completed")) {
+            solutions = jsonUtil.getJsonValue(OOFResponse, "solutions")
+        } else {
+            String statusMessage = jsonUtil.getJsonValue(OOFResponse, "statusMessage")
+            logger.error("received failed status from oof "+ statusMessage)
+            exceptionUtil.buildAndThrowWorkflowException(execution, 7000,"Received a failed Async Response from OOF : "+statusMessage)
+        }
+        execution.setVariable("solutions", solutions)
+        logger.debug(Prefix+" **** Exit DoAllocateCoreNSSI ::: processOOFAsyncResponse ****")
+    }
+
+    void prepareFailedOperationStatusUpdate(DelegateExecution execution){
+        logger.debug(Prefix + " **** Enter DoAllocateCoreNSSI ::: prepareFailedOperationStatusUpdate ****")
+        String serviceId = execution.getVariable("nssiId")
+        String jobId = execution.getVariable("jobId")
+        String nsiId = execution.getVariable("nsiId")
+        String operationType = execution.getVariable("operationType")
+        ResourceOperationStatus resourceOperationStatus = new ResourceOperationStatus()
+        resourceOperationStatus.setServiceId(serviceId)
+        resourceOperationStatus.setOperationId(jobId)
+        resourceOperationStatus.setResourceTemplateUUID(nsiId)
+        resourceOperationStatus.setOperType(operationType)
+        resourceOperationStatus.setProgress(0)
+        resourceOperationStatus.setStatus("failed")
+        resourceOperationStatus.setStatusDescription("Core NSSI Allocate Failed")
+        requestDBUtil.prepareUpdateResourceOperationStatus(execution, resourceOperationStatus)
+        logger.debug(Prefix + " **** Exit DoAllocateCoreNSSI ::: prepareFailedOperationStatusUpdate ****")
+    }
+}
---
+++ bpmn/so-bpmn-infrastructure-common/src/test/groovy/org/onap/so/bpmn/infrastructure/scripts/DoAllocateCoreNSSITest.groovy
@@ -0,0 +1,103 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2020  Tech Mahindra
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.bpmn.infrastructure.scripts
+
+import static org.junit.Assert.*
+
+import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.Mockito
+import org.onap.so.bpmn.common.scripts.MsoGroovyTest
+
+import static org.mockito.Mockito.when
+import static org.mockito.Mockito.times
+import static org.mockito.ArgumentMatchers.eq
+
+class DoAllocateCoreNSSITest extends MsoGroovyTest {
+
+    @Before
+    void init() throws IOException {
+        super.init("DoAllocateCoreNSSI")
+    }
+
+    @Captor
+    static ArgumentCaptor<ExecutionEntity> captor = ArgumentCaptor.forClass(ExecutionEntity.class)
+
+    @Test
+    public void testPreProcessRequest() {
+
+        String sliceParams="""{
+    "sliceProfile": {
+        "snssaiList": [
+            "001-100001"
+        ],
+        "sliceProfileId": "ab9af40f13f721b5f13539d87484098",
+        "plmnIdList": [
+            "460-00",
+            "460-01"
+        ],
+        "perfReq": {
+            "perfReqEmbbList ": [{
+                "activityFactor": 50
+            }]
+        },
+        "maxNumberofUEs": 200,
+        "coverageAreaTAList": [
+            "1",
+            "2",
+            "3",
+            "4"
+        ],
+        "latency": 2,
+        "resourceSharingLevel": "non-shared"
+    },
+    "endPoints": [{
+            "nodeId": "",
+            "additionalInfo": {
+                "xxx": "xxx"
+            }
+        },
+        {
+            "nodeId": "",
+            "additionalInfo": {
+                "xxx": "xxx"
+            }
+        }
+    ],
+    "nsiInfo": {
+        "nsiId": "NSI-M-001-HDBNJ-NSMF-01-A-ZX",
+        "nsiName": "eMBB-001"
+    },
+    "scriptName": "AN1"
+}"""
+        String expected = """{"plmnIdList":["460-00","460-01"],"sliceProfileId":"ab9af40f13f721b5f13539d87484098","maxNumberofUEs":200,"latency":2,"snssaiList":["001-100001"],"perfReq":{"perfReqEmbbList ":[{"activityFactor":50}]},"coverageAreaTAList":["1","2","3","4"],"resourceSharingLevel":"non-shared"}"""
+        when(mockExecution.getVariable("sliceParams")).thenReturn(sliceParams)
+        DoAllocateCoreNSSI allocateNssi = new DoAllocateCoreNSSI()
+        allocateNssi.preProcessRequest(mockExecution)
+        Mockito.verify(mockExecution, times(1)).setVariable(eq("sliceProfile"), captor.capture())
+        def sliceProfile = captor.getValue()
+        assertEquals(expected, sliceProfile)
+        Mockito.verify(mockExecution, times(3)).setVariable(captor.capture() as String, captor.capture())
+    }
+}
