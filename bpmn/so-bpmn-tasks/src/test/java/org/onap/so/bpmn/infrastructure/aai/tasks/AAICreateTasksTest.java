2e66d01a3a75439ab077fe15b1830ba4bc2a1d6b
Mateusz Gołuchowski <mateusz.goluchowski@nokia.com>
2020-09-21 14:45:29 +0200

    Sonar fixes
    New test for createOwningEntity method.
    
    Issue-ID: SO-1841
    Change-Id: I96f7793ee74d7871908f8dbbc1e4cd3a028498d5
    Signed-off-by: Mateusz Goluchowski <mateusz.goluchowski@nokia.com>

--- bpmn/so-bpmn-tasks/src/test/java/org/onap/so/bpmn/infrastructure/aai/tasks/AAICreateTasksTest.java
+++ bpmn/so-bpmn-tasks/src/test/java/org/onap/so/bpmn/infrastructure/aai/tasks/AAICreateTasksTest.java
@@ -9,9 +9,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,6 +37,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
+import com.google.common.base.Strings;
 import org.camunda.bpm.engine.delegate.BpmnError;
 import org.junit.Before;
 import org.junit.Rule;
@@ -300,6 +301,22 @@ public class AAICreateTasksTest extends BaseTaskTest {
         verify(aaiServiceInstanceResources, times(1)).existsOwningEntity(serviceInstance.getOwningEntity());
         verify(aaiServiceInstanceResources, times(1))
                 .createOwningEntityandConnectServiceInstance(serviceInstance.getOwningEntity(), serviceInstance);
+    }
+
+    @Test
+    public void createOwningEntityShouldThrowExceptionWhenNameAndIDAreNull() {
+        boolean catchedBpmnError = false;
+        serviceInstance.getOwningEntity().setOwningEntityName(null);
+        serviceInstance.getOwningEntity().setOwningEntityId(null);
+
+        try {
+            aaiCreateTasks.createOwningEntity(execution);
+        } catch (BpmnError err) {
+            catchedBpmnError = true;
+        }
+
+        assertTrue(catchedBpmnError);
+        assertEquals(execution.getVariable("ErrorCreateOEAAI"), aaiCreateTasks.EXCEPTION_NAME_AND_ID_ARE_NULL);
     }
 
     @Test
