ef600de63de247e0f176be8fb6a038c6a5fc0d7e
Lukasz Muszkieta <lukasz.muszkieta@nokia.com>
2020-10-21 19:15:14 +0200

    pnf service instantiation fix - IndexOutOfBoundsException
    Issue-ID: SO-3322
    Signed-off-by: Lukasz Muszkieta <lukasz.muszkieta@nokia.com>
    Change-Id: I6c978cbe788281eac25fa7be6f4ff24c4bff9810

--- bpmn/so-bpmn-tasks/src/main/java/org/onap/so/bpmn/infrastructure/workflow/tasks/WorkflowAction.java
+++ bpmn/so-bpmn-tasks/src/main/java/org/onap/so/bpmn/infrastructure/workflow/tasks/WorkflowAction.java
@@ -30,6 +30,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -1675,6 +1676,7 @@ public class WorkflowAction {
                 }
             }
         }
+        listToExecute.sort(Comparator.comparingInt(OrchestrationFlow::getSequenceNumber));
         return listToExecute;
     }
 
