c2abdb9b843ec50fd2f514e8bfb07fe355af7dbb
hetengjiao <hetengjiao@chinamobile.com>
2020-10-26 20:16:37 +0800

    add request logger annotation
    Issue-ID: SO-2963
    
    Signed-off-by: hetengjiao <hetengjiao@chinamobile.com>
    Change-Id: Ie7d888fb216f61979a093b8688640ebbaf7c6f28

--- adapters/mso-nssmf-adapter/src/main/java/org/onap/so/adapters/nssmf/interceptor/LoggerInterceptor.java
+++ adapters/mso-nssmf-adapter/src/main/java/org/onap/so/adapters/nssmf/interceptor/LoggerInterceptor.java
@@ -19,18 +19,28 @@
  */
 package org.onap.so.adapters.nssmf.interceptor;
 
+import lombok.Data;
+import lombok.ToString;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.onap.so.adapters.nssmf.annotation.RequestLogger;
 import org.onap.so.adapters.nssmf.annotation.ServiceLogger;
+import org.onap.so.adapters.nssmf.exceptions.ApplicationException;
 import org.onap.so.adapters.nssmf.util.NssmfAdapterUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * support to print logger of service method
@@ -88,5 +98,134 @@ public class LoggerInterceptor {
             logger.error(e.getMessage(), e);
         }
         return null;
+    }
+
+    @Pointcut("execution(* org.onap.so.adapters.nssmf.controller..*(..))")
+    public void controllerLogger() {
+
+    }
+
+    @Around("controllerLogger()")
+    public Object doAroundRequest(ProceedingJoinPoint point) throws Throwable {
+        MethodSignature signature = (MethodSignature) point.getSignature();
+        Method method = signature.getMethod();
+        Class<?> targetClass = method.getDeclaringClass();
+
+        RequestLogger classAnnotation = targetClass.getAnnotation(RequestLogger.class);
+        RequestLogger methodAnnotation = method.getAnnotation(RequestLogger.class);
+
+        if ((classAnnotation == null && methodAnnotation == null)
+                || (classAnnotation != null && classAnnotation.ignore())
+                || (methodAnnotation != null && methodAnnotation.ignore())) {
+            return point.proceed();
+        }
+
+        long start = System.currentTimeMillis();
+        ServletRequestAttributes attributes
+                = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        assert attributes != null;
+        HttpServletRequest request = attributes.getRequest();
+        Object result = point.proceed();
+        RequestInfo requestInfo = new RequestInfo();
+        requestInfo.setIp(request.getRemoteAddr());
+        requestInfo.setUrl(request.getRequestURL().toString());
+        requestInfo.setHttpMethod(request.getMethod());
+        requestInfo.setClassMethod(String.format("%s.%s", signature.getDeclaringTypeName(),
+                signature.getName()));
+        requestInfo.setRequestParams(getRequestParamsByProceedingJoinPoint(point));
+        requestInfo.setResult(result);
+        requestInfo.setTimeCost(System.currentTimeMillis() - start);
+        logger.info("Request Info      : {}", NssmfAdapterUtil.marshal(requestInfo));
+        return result;
+    }
+
+    @AfterThrowing(pointcut = "controllerLogger()", throwing = "e")
+    public void doAfterRequestThrow(JoinPoint joinPoint, RuntimeException e) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        Class<?> targetClass = method.getDeclaringClass();
+
+        RequestLogger classAnnotation = targetClass.getAnnotation(RequestLogger.class);
+        RequestLogger methodAnnotation = method.getAnnotation(RequestLogger.class);
+
+        if ((classAnnotation == null && methodAnnotation == null)
+                || (classAnnotation != null && classAnnotation.ignore())
+                || (methodAnnotation != null && methodAnnotation.ignore())) {
+            return;
+        }
+        ServletRequestAttributes attributes
+                = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        assert attributes != null;
+        HttpServletRequest request = attributes.getRequest();
+        RequestErrorInfo requestErrorInfo = new RequestErrorInfo();
+        requestErrorInfo.setIp(request.getRemoteAddr());
+        requestErrorInfo.setUrl(request.getRequestURL().toString());
+        requestErrorInfo.setHttpMethod(request.getMethod());
+        requestErrorInfo.setClassMethod(String.format("%s.%s", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName()));
+        requestErrorInfo.setRequestParams(getRequestParamsByJoinPoint(joinPoint));
+        requestErrorInfo.setException(e);
+        String res;
+        try {
+            res = NssmfAdapterUtil.marshal(requestErrorInfo);
+            logger.info("Error Request Info      : {}", res);
+        } catch (ApplicationException ex) {
+            logger.info("Error Request Info      : {}", requestErrorInfo);
+        }
+    }
+
+    private Map<String, Object> getRequestParamsByJoinPoint(JoinPoint joinPoint) {
+        String[] paramNames = ((MethodSignature)joinPoint.getSignature()).getParameterNames();
+        Object[] paramValues = joinPoint.getArgs();
+
+        return buildRequestParam(paramNames, paramValues);
+    }
+
+
+    private Map<String, Object> getRequestParamsByProceedingJoinPoint(ProceedingJoinPoint proceedingJoinPoint) {
+        String[] paramNames = ((MethodSignature)proceedingJoinPoint.getSignature()).getParameterNames();
+        Object[] paramValues = proceedingJoinPoint.getArgs();
+
+        return buildRequestParam(paramNames, paramValues);
+    }
+
+    private Map<String, Object> buildRequestParam(String[] paramNames, Object[] paramValues) {
+        Map<String, Object> requestParams = new HashMap<>();
+        for (int i = 0; i < paramNames.length; i++) {
+            Object value = paramValues[i];
+
+            if (value instanceof MultipartFile) {
+                MultipartFile file = (MultipartFile) value;
+                value = file.getOriginalFilename();
+            }
+
+            requestParams.put(paramNames[i], value);
+        }
+
+        return requestParams;
+    }
+
+    @Data
+    @ToString
+    private class RequestInfo {
+        private String ip;
+        private String url;
+        private String httpMethod;
+        private String classMethod;
+        private Object requestParams;
+        private Object result;
+        private Long timeCost;
+    }
+
+    @Data
+    @ToString
+    private class RequestErrorInfo {
+        private String ip;
+        private String url;
+        private String httpMethod;
+        private String classMethod;
+        private Object requestParams;
+        private RuntimeException exception;
     }
 }
