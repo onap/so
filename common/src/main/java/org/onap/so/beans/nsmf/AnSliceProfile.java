343b4a10635e3898817c8aa781389c07effaf6bf
hetengjiao <hetengjiao@chinamobile.com>
2020-11-13 15:18:59 +0800

    fixed cs profile trans to ss profile
    Issue-ID: SO-2963
    
    Signed-off-by: hetengjiao <hetengjiao@chinamobile.com>
    Change-Id: Ia5a40ac9126641479047a0a76c7d2fefc1d6f07d

--- common/src/main/java/org/onap/so/beans/nsmf/AnSliceProfile.java
+++ common/src/main/java/org/onap/so/beans/nsmf/AnSliceProfile.java
@@ -24,6 +24,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.ToString;
+
 import java.io.Serializable;
 import java.util.List;
 
@@ -46,14 +47,12 @@ public class AnSliceProfile implements Serializable {
     @JsonProperty(value = "perfReq", required = true)
     private AnPerfReq perfReq;
 
-    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     @JsonProperty(value = "maxNumberofUEs")
     private long maxNumberofUEs;
 
     @JsonProperty(value = "coverageAreaTAList")
-    private List<Integer> coverageAreaTAList;
+    private List<String> coverageAreaTAList;
 
-    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
     @JsonProperty(value = "latency")
     private int latency;
 
