25b54d688aa6b1bba38cdc7efc31c77bd4773e34
hetengjiao <hetengjiao@chinamobile.com>
2020-11-20 10:11:12 +0800

    add serializable to fixed cn nssi processing error
    Issue-ID: SO-2963
    
    Signed-off-by: hetengjiao <hetengjiao@chinamobile.com>
    Change-Id: I0c50d7ea14298aa7b7a261faef9d01c811363633

--- common/src/main/java/org/onap/so/beans/nsmf/ResponseHistory.java
+++ common/src/main/java/org/onap/so/beans/nsmf/ResponseHistory.java
@@ -21,10 +21,13 @@
 package org.onap.so.beans.nsmf;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import java.io.Serializable;
 import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ResponseHistory {
+public class ResponseHistory implements Serializable {
+
+    private static final long serialVersionUID = -1005342539496792450L;
 
     private int progress;
 
