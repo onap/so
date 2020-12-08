ebc47bbcefd16b266a31ad2ae9d513ff47bd2523
pwielebs <piotr.wielebski@nokia.com>
2020-12-08 08:46:38 +0100

    SO WorkflowAction refactor
    - extract traverseUserParamsService() method from WorkflowAction class to UserParamsServiceTraversal class
    - move methods used only in traverseUserParamsService() method to UserParamsServiceTraversal class
    - change method signature from traverseUserParamsService to getResourceListFromUserParams()
    - create WorkFlowActionCommon for duplicated code
    
    Issue-ID: SO-3422
    Signed-off-by: pwielebs <piotr.wielebski@nokia.com>
    Change-Id: Ic6a567eb7b323939e47335aa7938b83498f7a8f8

---
+++ bpmn/so-bpmn-tasks/src/main/java/org/onap/so/bpmn/infrastructure/workflow/tasks/WorkflowActionConstants.java
@@ -0,0 +1,35 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Modifications Copyright (c) 2019 Samsung
+ * ================================================================================
+ * Modifications Copyright (c) 2020 Nokia
+ * ================================================================================
+ * Modifications Copyright (c) 2020 Tech Mahindra
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.bpmn.infrastructure.workflow.tasks;
+
+public final class WorkflowActionConstants {
+
+    static final String USER_PARAM_SERVICE = "service";
+    static final String CREATE_INSTANCE = "createInstance";
+    static final String FABRIC_CONFIGURATION = "FabricConfiguration";
+    static final String WORKFLOW_ACTION_ERROR_MESSAGE = "WorkflowActionErrorMessage";
+}
