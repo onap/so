fb9f8b33a7df071d29e83f02f7b152122ed56ddf
Kalkere Ramesh, Sharan (sk720x) <sk720x@att.com>
2021-04-15 13:26:34 -0400

    Added logging statements for skip post inst conf class
    Issue-ID: SO-3626
    Change-Id: I618b6b6b6049f94bc6c9bfe6be893ac022f2449e
    Signed-off-by: Kalkere Ramesh, Sharan (sk720x) <sk720x@att.com>

--- adapters/mso-catalog-db-adapter/src/test/java/org/onap/so/db/catalog/client/CatalogDbClientTest.java
+++ adapters/mso-catalog-db-adapter/src/test/java/org/onap/so/db/catalog/client/CatalogDbClientTest.java
@@ -197,8 +197,8 @@ public class CatalogDbClientTest extends CatalogDbAdapterBaseTest {
         assertNotNull(vnfResourceCustomization.getVnfResources());
         assertNotNull(vnfResourceCustomization.getVfModuleCustomizations());
         assertEquals("vSAMP10a", vnfResourceCustomization.getVnfResources().getModelName());
-        assertTrue("skip post instantiation configuration",
-                vnfResourceCustomization.isSkipPostInstConf().booleanValue());
+        assertFalse("skip post instantiation configuration",
+                vnfResourceCustomization.getSkipPostInstConf().booleanValue());
     }
 
     @Test
