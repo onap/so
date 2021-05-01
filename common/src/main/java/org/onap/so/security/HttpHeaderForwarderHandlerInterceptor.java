8fbc249e03d0f982f391a542d217d828cad8dad4
sheel.bajpai <sheel.bajpai@orange.com>
2021-05-01 09:31:30 +0000

    ONAP Service Mesh compliant modification
    Issue-ID: OOM-2232
    Signed-off-by: sheel.bajpai <sheel.bajpai@orange.com>
    Change-Id: Ife60fc13d9723b54daaad1706d56d90aeb021ce9

--- common/src/main/java/org/onap/so/security/HttpHeaderForwarderHandlerInterceptor.java
+++ common/src/main/java/org/onap/so/security/HttpHeaderForwarderHandlerInterceptor.java
@@ -27,7 +27,6 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import com.google.common.collect.ImmutableSet;
