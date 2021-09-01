8186b41544ec5aff18b619e1fb183916fad1cc2d
deepikasatheesh <deepika.s84@wipro.com>
2021-09-01 19:12:27 +0000

    Fix issues in NSMF terminate flow
    Issue-ID: SO-3759
    
    Signed-off-by: deepikasatheesh <deepika.s84@wipro.com>
    Change-Id: Ic27f53b8b8e739849f6eb5ab4a1cee1d2cc0969c

--- bpmn/so-bpmn-infrastructure-common/src/main/groovy/org/onap/so/bpmn/infrastructure/scripts/DeAllocateSliceSubnet.groovy
+++ bpmn/so-bpmn-infrastructure-common/src/main/groovy/org/onap/so/bpmn/infrastructure/scripts/DeAllocateSliceSubnet.groovy
@@ -139,7 +139,8 @@ class DeAllocateSliceSubnet extends AbstractServiceTaskProcessor {
         ResourceOperationStatus initStatus = new ResourceOperationStatus()
         initStatus.setServiceId(nsiId)
         initStatus.setOperationId(jobId)
-        initStatus.setResourceTemplateUUID(modelUuid)
+        //initStatus.setResourceTemplateUUID(modelUuid)
+        initStatus.setResourceInstanceID(nssiId)
         initStatus.setOperType("Deallocate")
         requestDBUtil.prepareInitResourceOperationStatus(execution, initStatus)
 
