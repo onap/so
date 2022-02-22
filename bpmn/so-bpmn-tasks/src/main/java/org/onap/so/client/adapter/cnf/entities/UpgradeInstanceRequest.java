5263c78735c193fe3ab0fe36e7aab730f2d990a3
sanket12345 <SX00562924@techmahindra.com>
2022-02-22 20:39:50 +0530

    CNF - Upgrade Functionality - CNF Adapter
    -Added new endpoint in CNF Adapter for CNF-Upgrade.
    
    Issue-ID: SO-3858
    Change-Id: I68f32f215d232d723f44f1a103acf5431d6c7abe
    Signed-off-by: sanket12345 <SX00562924@techmahindra.com>

---
+++ bpmn/so-bpmn-tasks/src/main/java/org/onap/so/client/adapter/cnf/entities/UpgradeInstanceRequest.java
@@ -0,0 +1,100 @@
+
+package org.onap.so.client.adapter.cnf.entities;
+
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UpgradeInstanceRequest {
+
+    @JsonProperty("modelInvariantId")
+    private String modelInvariantId;
+
+    @JsonProperty("modelCustomizationId")
+    private String modelCustomizationId;
+
+    @JsonProperty("k8sRBProfileName")
+    private String profileName;
+
+    @JsonProperty("k8sRBInstanceStatusCheck")
+    private Boolean statusCheck = false;
+
+    @JsonProperty("cloudRegionId")
+    private String cloudRegion;
+
+    @JsonProperty("vfModuleUUID")
+    private String vfModuleUUID;
+
+    @JsonProperty("labels")
+    private Map<String, String> labels;
+
+    @JsonProperty("override-values")
+    private Map<String, String> overrideValues;
+
+    public String getModelInvariantId() {
+        return modelInvariantId;
+    }
+
+    public void setModelInvariantId(String modelInvariantId) {
+        this.modelInvariantId = modelInvariantId;
+    }
+
+    public String getModelCustomizationId() {
+        return modelCustomizationId;
+    }
+
+    public void setModelCustomizationId(String modelCustomizationId) {
+        this.modelCustomizationId = modelCustomizationId;
+    }
+
+    public String getProfileName() {
+        return profileName;
+    }
+
+    public void setProfileName(String profileName) {
+        this.profileName = profileName;
+    }
+
+    public Boolean getStatusCheck() {
+        return statusCheck;
+    }
+
+    public void setStatusCheck(Boolean statusCheck) {
+        this.statusCheck = statusCheck;
+    }
+
+    public String getCloudRegion() {
+        return cloudRegion;
+    }
+
+    public void setCloudRegion(String cloudRegion) {
+        this.cloudRegion = cloudRegion;
+    }
+
+    public Map<String, String> getLabels() {
+        return labels;
+    }
+
+    public void setLabels(Map<String, String> labels) {
+        this.labels = labels;
+    }
+
+    public String getVfModuleUUID() {
+        return vfModuleUUID;
+    }
+
+    public void setVfModuleUUID(String vfModuleUUID) {
+        this.vfModuleUUID = vfModuleUUID;
+    }
+
+    public Map<String, String> getOverrideValues() {
+        return overrideValues;
+    }
+
+    public void setOverrideValues(Map<String, String> overrideValues) {
+        this.overrideValues = overrideValues;
+    }
+
+}
