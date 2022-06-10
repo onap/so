9267b3ae36ce0ade8d7f7bcd44ff648e78d3a14e
waqas.ikram <waqas.ikram@est.tech>
2022-06-10 15:43:31 +0100

    [SO] Adding logic to invoke cnfm
    Change-Id: I2c0e625724a394fe091a0ed1c6efabcd98e606ff
    Issue-ID: SO-3905
    Signed-off-by: waqas.ikram <waqas.ikram@est.tech>

---
+++ bpmn/so-bpmn-tasks/src/main/java/org/onap/so/bpmn/infrastructure/adapter/cnfm/tasks/CnfmHttpServiceProvider.java
@@ -0,0 +1,36 @@
+/*-
+ * ============LICENSE_START=======================================================
+ *  Copyright (C) 2022 Nordix Foundation.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.so.bpmn.infrastructure.adapter.cnfm.tasks;
+
+import java.util.Optional;
+import org.onap.so.cnfm.lcm.model.AsInstance;
+import org.onap.so.cnfm.lcm.model.CreateAsRequest;
+
+
+/**
+ * @author Sagar Shetty (sagar.shetty@est.tech)
+ * @author Waqas Ikram (waqas.ikram@est.tech)
+ *
+ */
+public interface CnfmHttpServiceProvider {
+
+    Optional<AsInstance> invokeCreateAsRequest(final CreateAsRequest createAsRequest);
+
+}
