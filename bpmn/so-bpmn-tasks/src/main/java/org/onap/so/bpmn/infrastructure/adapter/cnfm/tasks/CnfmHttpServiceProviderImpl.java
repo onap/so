61a3844b94f073926b0ad191a79caba816b4bda4
SagarS <sagar.shetty@est.tech>
2022-06-16 13:22:13 +0100

    [SO] CNF Invoke AS instance updates
    Change-Id: I221df282a8cf1b9b4b0c579517393d2818eafaf2
    Signed-off-by: SagarS <sagar.shetty@est.tech>
    Issue-ID: SO-3905

--- bpmn/so-bpmn-tasks/src/main/java/org/onap/so/bpmn/infrastructure/adapter/cnfm/tasks/CnfmHttpServiceProviderImpl.java
+++ bpmn/so-bpmn-tasks/src/main/java/org/onap/so/bpmn/infrastructure/adapter/cnfm/tasks/CnfmHttpServiceProviderImpl.java
@@ -5,15 +5,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
@@ -34,6 +34,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.onap.so.cnfm.lcm.model.InstantiateAsRequest;
 
 @Service
 public class CnfmHttpServiceProviderImpl implements CnfmHttpServiceProvider {
@@ -78,6 +79,30 @@ public class CnfmHttpServiceProviderImpl implements CnfmHttpServiceProvider {
             LOGGER.error("Unexpected error while processing create and instantiation request", httpInvocationException);
             return Optional.empty();
         }
+    }
+
+    @Override
+    public void invokeInstantiateAsRequest(InstantiateAsRequest createAsRequest, String asInstanceId) {
+        try {
+
+            final String url = cnfmUrlProvider.getInstantiateAsRequestUrl(asInstanceId);
+            final ResponseEntity<AsInstance> response =
+                    httpServiceProvider.postHttpRequest(createAsRequest, url, AsInstance.class);
+
+            final HttpStatus httpStatus = response.getStatusCode();
+            if (httpStatus.is2xxSuccessful()) {
+                if (!response.hasBody()) {
+                    LOGGER.error("Received response without body: {}", response);
+                }
+
+            }
+            LOGGER.error("Unable to invoke HTTP POST using URL: {}, Response Code: {}", url, httpStatus.value());
+
+        } catch (final RestProcessingException | InvalidRestRequestException
+                | HttpResouceNotFoundException httpInvocationException) {
+            LOGGER.error("Unexpected error while processing instantiation request", httpInvocationException);
+        }
+
     }
 
 }
