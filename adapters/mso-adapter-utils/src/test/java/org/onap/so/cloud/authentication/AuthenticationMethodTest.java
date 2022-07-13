2dd06a074f8de1069f9a7326123184e6023a5371
Ganesh <ganesh.c@samsung.com>
2022-07-13 08:40:07 +0530

    sonar issue, AuthenticationMethodTest use assertNotEquals instead
    Signed-off-by: Ganesh <ganesh.c@samsung.com>
    Change-Id: I737a0cfe04bcb2018120d897267c4fc8fb695fd2
    Issue-ID: SO-3966

--- adapters/mso-adapter-utils/src/test/java/org/onap/so/cloud/authentication/AuthenticationMethodTest.java
+++ adapters/mso-adapter-utils/src/test/java/org/onap/so/cloud/authentication/AuthenticationMethodTest.java
@@ -3,13 +3,12 @@
  * ONAP - SO
  * ================================================================================
  * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2022 Samsung Electronics
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,8 +20,9 @@
 package org.onap.so.cloud.authentication;
 
 import static com.shazam.shazamcrest.matcher.Matchers.sameBeanAs;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -61,7 +61,7 @@ public class AuthenticationMethodTest {
         ci.setMsoId("test");
 
         Authentication auth = authenticationMethodFactory.getAuthenticationFor(ci);
-        assertTrue(RackspaceAuthentication.class.equals(auth.getClass()));
+        assertEquals(RackspaceAuthentication.class, auth.getClass());
 
     }
 
@@ -73,7 +73,7 @@ public class AuthenticationMethodTest {
         ci.setMsoId("someuser");
 
         Authentication auth = authenticationMethodFactory.getAuthenticationFor(ci);
-        assertTrue(UsernamePassword.class.equals(auth.getClass()));
+        assertEquals(UsernamePassword.class, auth.getClass());
 
     }
 
@@ -85,7 +85,7 @@ public class AuthenticationMethodTest {
         ci.setMsoId("test");
 
         Authentication auth = authenticationMethodFactory.getAuthenticationFor(ci);
-        assertTrue(RackspaceAuthentication.class.equals(auth.getClass()));
+        assertEquals(RackspaceAuthentication.class, auth.getClass());
     }
 
     @Test
@@ -96,7 +96,7 @@ public class AuthenticationMethodTest {
         ci.setMsoId("someuser");
 
         Authentication auth = authenticationMethodFactory.getAuthenticationFor(ci);
-        assertTrue(UsernamePassword.class.equals(auth.getClass()));
+        assertEquals(UsernamePassword.class, auth.getClass());
 
     }
 
