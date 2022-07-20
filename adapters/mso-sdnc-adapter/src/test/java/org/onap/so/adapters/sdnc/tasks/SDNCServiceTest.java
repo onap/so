c8ed3429b76cc507e36cfd15de25db8dc9701d02
Ganesh <ganesh.c@samsung.com>
2022-07-20 18:34:10 +0530

    SDNCResponse Test, remove unnessary global variable
    Signed-off-by: Ganesh <ganesh.c@samsung.com>
    Change-Id: I559d0b2dab7e952c64bf51d4818a5ce854b078b2
    Issue-ID: SO-3966

--- adapters/mso-sdnc-adapter/src/test/java/org/onap/so/adapters/sdnc/tasks/SDNCServiceTest.java
+++ adapters/mso-sdnc-adapter/src/test/java/org/onap/so/adapters/sdnc/tasks/SDNCServiceTest.java
@@ -1,3 +1,18 @@
+/*
+ * ============LICENSE_START======================================================= ONAP : SO
+ * Copyright (c) 2022, Samsung Electronics. All rights reserved.
+ * ================================================================================ Licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
 package org.onap.so.adapters.sdnc.tasks;
 
 import java.io.IOException;
@@ -26,8 +41,6 @@ import org.springframework.web.client.RestTemplate;
 @RunWith(MockitoJUnitRunner.class)
 public class SDNCServiceTest {
 
-    private String RESOURCE_PATH = "src/test/resources";
-
     @Mock
     private ExternalTask mockExternalTask;
 
@@ -52,6 +65,7 @@ public class SDNCServiceTest {
 
     @Test
     public void testExecutePostTask() throws IOException, GeneralSecurityException {
+        String RESOURCE_PATH = "src/test/resources";
         String payload = new String(Files.readAllBytes(Paths.get(RESOURCE_PATH + "/sdncRequest.json")));
 
         Mockito.when(env.getProperty("org.onap.so.adapters.sdnc.sdncHost")).thenReturn("sdncHost");
@@ -65,7 +79,7 @@ public class SDNCServiceTest {
 
         sdncService.executePostTask(mockExternalTask, mockExternalTaskService);
 
-        Mockito.verify(mockExternalTaskService).complete(Mockito.eq(mockExternalTask), Mockito.eq(new HashMap<>()));
+        Mockito.verify(mockExternalTaskService).complete(mockExternalTask, new HashMap<>());
 
     }
 
