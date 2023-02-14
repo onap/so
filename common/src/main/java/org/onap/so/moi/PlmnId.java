c7755f98b6be6384b0a8d08de2c50e13d8691eda
Patil <Abhishek.Patil@t-systems.com>
2023-02-14 18:46:28 +0530

    Code changes in SO api-handler for RAN Slice
    Issue-ID: SO-4038
    Change-Id: Ifad4e0a65dc810a753d30741a84c08081bcfd258
    Signed-off-by: Patil <Abhishek.Patil@t-systems.com>

---
+++ common/src/main/java/org/onap/so/moi/PlmnId.java
@@ -0,0 +1,68 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2023 DTAG Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.so.moi;
+
+import com.fasterxml.jackson.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"mcc", "mnc"})
+public class PlmnId {
+
+    @JsonProperty("mcc")
+    private Integer mcc;
+    @JsonProperty("mnc")
+    private Integer mnc;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("mcc")
+    public Integer getMcc() {
+        return mcc;
+    }
+
+    @JsonProperty("mcc")
+    public void setMcc(Integer mcc) {
+        this.mcc = mcc;
+    }
+
+    @JsonProperty("mnc")
+    public Integer getMnc() {
+        return mnc;
+    }
+
+    @JsonProperty("mnc")
+    public void setMnc(Integer mnc) {
+        this.mnc = mnc;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
