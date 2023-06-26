cb5e24fc0e7bab88f4c8c3ae0a8e9ffa28ba9670
thmsdt <thomas.kulik@telekom.de>
2023-06-26 14:48:22 +0200

    fix docs config files
    Issue-ID: DOC-811
    
    Signed-off-by: thmsdt <thomas.kulik@telekom.de>
    Change-Id: I33c1757630cffe2b6449fd74304cd8825b568229

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 project = "onap"
-release = "master"
-version = "master"
+release = "london"
+version = "london"
 
 doc_url = 'https://docs.onap.org/projects'
 
@@ -32,11 +32,10 @@ extensions = [
 # Change to {releasename} after you have created the new 'doc' branch.
 #
 
-branch = 'latest'
+branch = 'london'
 
 intersphinx_mapping = {}
 intersphinx_mapping['onap-usecase-ui'] = ('{}/onap-usecase-ui/en/%s'.format(doc_url) % branch, None)
-intersphinx_mapping['onap-vid'] = ('{}/onap-vid/en/%s'.format(doc_url) % branch, None)
 doc_url = 'https://docs.onap.org/projects'
 master_doc = 'index'
 
--- docs/developer_info/instantiate/instantiation/vid/index.rst
+++
@@ -1,9 +0,0 @@
-.. This work is licensed under a Creative Commons Attribution 4.0
-.. International License. http://creativecommons.org/licenses/by/4.0
-.. Copyright 2019 ONAP Contributors.  All rights reserved.
-
-
-A La Carte mode Service Instantiation via ONAP VID Portal
-=========================================================
-
-VID documentation, see :ref:`VID <onap-vid:instantiate>` for details.
