291269d0b6c39b46d3bde922ee7f4da7e2c51412
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-02-14 15:01:19 +0100

    Reenable tests for the mso-requests-db-repositories module
    - use mariadb in tests to ensure functionality for the db that is actually
      used in production (instead of h2)
    
    Issue-ID: SO-4139
    Change-Id: I959a667f1df4a775e32b97ce77bd79d1a48e2c0b
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

--- mso-api-handlers/mso-requests-db-repositories/src/test/java/org/onap/so/BaseTest.java
+++ mso-api-handlers/mso-requests-db-repositories/src/test/java/org/onap/so/BaseTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,15 +20,30 @@
 
 package org.onap.so;
 
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.MariaDBContainer;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public abstract class BaseTest {
+
+    @ClassRule
+    public static final MariaDBContainer<?> mariaDBContainer = new MariaDBContainer<>("mariadb:11-ubi")
+            .withDatabaseName("testdb").withUsername("user").withPassword("password");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        System.setProperty("spring.datasource.url", mariaDBContainer.getJdbcUrl());
+        System.setProperty("spring.datasource.username", mariaDBContainer.getUsername());
+        System.setProperty("spring.datasource.password", mariaDBContainer.getPassword());
+    }
 
 }
