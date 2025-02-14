e98b5b19d8f7ccfb3881993f32534f263cf31123
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-02-14 15:01:19 +0100

    Reenable tests for the mso-requests-db-repositories module
    - use mariadb in tests to ensure functionality for the db that is actually
      used in production (instead of h2)
    
    Issue-ID: SO-4139
    Change-Id: I959a667f1df4a775e32b97ce77bd79d1a48e2c0b
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

--- mso-api-handlers/mso-requests-db-repositories/src/test/java/org/onap/so/db/request/OperationStatusTest.java
+++ mso-api-handlers/mso-requests-db-repositories/src/test/java/org/onap/so/db/request/OperationStatusTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,20 +27,19 @@ import java.util.Date;
 import javax.transaction.Transactional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.onap.so.BaseTest;
 import org.onap.so.TestApplication;
 import org.onap.so.db.request.beans.OperationStatus;
 import org.onap.so.db.request.beans.OperationStatusId;
 import org.onap.so.db.request.data.repository.OperationStatusRepository;
 import org.onap.so.db.request.exceptions.NoEntityFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = TestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
-public class OperationStatusTest {
+public class OperationStatusTest extends BaseTest {
 
     @Autowired
     private OperationStatusRepository repository;
