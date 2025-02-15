cc29bcbf81ff509dfa3226082c8af47eb6c6d32c
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-02-15 15:14:39 +0100

    Reenable tests for most of the top-level modules
    - mso-requests-db-adapter
    - aai-client
    - so-optimization-clients
    - so-sdn-clients
    
    Issue-ID: SO-4141
    Change-Id: I59264e394de71eea1a091bd08d32b181a44713d5
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

--- so-sdn-clients/src/test/java/org/onap/so/client/sdnc/SDNCClientIT.java
+++ so-sdn-clients/src/test/java/org/onap/so/client/sdnc/SDNCClientIT.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -55,7 +55,7 @@ public class SDNCClientIT extends BaseIntegrationTest {
         String responseJson =
                 new String(Files.readAllBytes(Paths.get(JSON_FILE_LOCATION + "SDNCClientPut404Response.json")));
 
-        String queryLink = "/restconf/operations/GENERIC-RESOURCE-API:network-topology-operation/";
+        String queryLink = "/restconf/operations/GENERIC-RESOURCE-API:network-topology-operation";
 
         wireMockServer.stubFor(post(urlMatching(queryLink)).willReturn(
                 aResponse().withStatus(200).withHeader("Content-Type", "application/json").withBody(responseJson)));
