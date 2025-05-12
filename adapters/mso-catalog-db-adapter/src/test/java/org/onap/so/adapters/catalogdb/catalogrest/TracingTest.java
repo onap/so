4105abcb69017488c8305c4f7ef3b7614305e868
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-05-12 13:08:24 +0200

    Add tracing for catalog-db-adapter
    - add sleuth tracing dependencies
    - make tracing disabled by default
    - make it configurable via environment variables
    - bump snapshot version to 1.15.1-SNAPSHOT
    
    Issue-ID: SO-4150
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
    Change-Id: Id8c32584a5ae49417f0d5ddb7a828cc189dc2789

---
+++ adapters/mso-catalog-db-adapter/src/test/java/org/onap/so/adapters/catalogdb/catalogrest/TracingTest.java
@@ -0,0 +1,57 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright © 2025 Deutsche Telekom AG Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.so.adapters.catalogdb.catalogrest;
+
+import org.junit.Test;
+import org.onap.so.adapters.catalogdb.CatalogDbAdapterBaseTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import com.github.tomakehurst.wiremock.client.WireMock;
+
+@DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
+@AutoConfigureWireMock(port = 0)
+@TestPropertySource(
+        properties = {"spring.sleuth.enabled=true", "spring.zipkin.base-url=http://localhost:${wiremock.server.port}"})
+public class TracingTest extends CatalogDbAdapterBaseTest {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Test
+    public void thatTracesAreExported() throws InterruptedException {
+        WireMock.stubFor(WireMock.post(WireMock.urlEqualTo("/api/v2/spans"))
+                .willReturn(WireMock.aResponse().withStatus(HttpStatus.OK.value())));
+
+        try {
+            restTemplate.getForObject("http://localhost:" + port + "/foo", String.class);
+        } catch (RestClientException e) {
+            // this provokes a 404. For the test it's not important what is returned here
+        }
+        Thread.sleep(1000);
+        WireMock.verify(WireMock.postRequestedFor(WireMock.urlEqualTo("/api/v2/spans")));
+    }
+
+}
