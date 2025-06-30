d7c176dc5472103f7b67aba1169bc47f45c94fa2
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-06-30 13:12:55 +0200

    Reduce SO log noise
    - introduce logging.request-status.exclusions property to allow
      disabling request logging for certain paths (i.e liveness probes)
      in catalog-db and request-db-adapter
    - disable logging for the scheduled performDnsLookup
    - disable logging for the scheduled checkAllClientsActive
    
    Issue-ID: SO-4192
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
    Change-Id: I491aa4b6e9d4ec2ce02e3559026edfad9bfa229c

--- adapters/mso-catalog-db-adapter/src/main/java/org/onap/so/adapters/catalogdb/WebMvcConfig.java
+++ adapters/mso-catalog-db-adapter/src/main/java/org/onap/so/adapters/catalogdb/WebMvcConfig.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,6 +24,7 @@ package org.onap.so.adapters.catalogdb;
 import org.onap.logging.filter.spring.LoggingInterceptor;
 import org.onap.logging.filter.spring.StatusLoggingInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -33,6 +34,9 @@ import org.springframework.web.servlet.handler.MappedInterceptor;
 @Configuration
 @ComponentScan(basePackages = {"org.onap.logging.filter"})
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Value("${logging.request-status.exclusions:}")
+    private String[] excludedPaths = new String[0];
 
     @Autowired
     private LoggingInterceptor loggingInterceptor;
@@ -47,6 +51,8 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public MappedInterceptor mappedStatusLoggingInterceptor() {
-        return new MappedInterceptor(new String[] {"/**"}, statusLoggingInterceptor);
+        return excludedPaths != null && excludedPaths.length > 0
+                ? new MappedInterceptor(new String[] {"/**"}, excludedPaths, statusLoggingInterceptor)
+                : new MappedInterceptor(new String[] {"/**"}, statusLoggingInterceptor);
     }
 }
