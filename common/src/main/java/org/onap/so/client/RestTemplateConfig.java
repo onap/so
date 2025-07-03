d4dc2ba7f1974761704189695da6cf848125800d
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-07-03 09:27:30 +0200

    Use a dependency-injected RestTemplateBuilder to create RestTemplates
    - RestTemplate t = new RestTemplate() breaks tracing
    - use dependency-injected RestTemplateBuilder to create RestTemplate
    
    Issue-ID: SO-4194
    Change-Id: Id7884b28ad6e930d6007bf41b5dbd7017b93dd4f
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

--- common/src/main/java/org/onap/so/client/RestTemplateConfig.java
+++ common/src/main/java/org/onap/so/client/RestTemplateConfig.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,27 +23,46 @@ package org.onap.so.client;
 import org.onap.logging.filter.spring.SpringClientPayloadFilter;
 import org.onap.so.configuration.rest.HttpComponentsClientConfiguration;
 import org.onap.so.logging.jaxrs.filter.SOSpringClientFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.jackson.JsonComponentModule;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.kotlin.KotlinModule;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class RestTemplateConfig {
 
     public static final String CONFIGURABLE_REST_TEMPLATE = "configurableRestTemplate";
+    private final HttpComponentsClientConfiguration httpComponentsClientConfiguration;
 
-    @Autowired
-    private HttpComponentsClientConfiguration httpComponentsClientConfiguration;
+    private MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(new JsonComponentModule());
+        converter.setObjectMapper(mapper);
+        return converter;
+    }
 
     @Bean
     @Primary
-    public RestTemplate restTemplate() {
-        final RestTemplate restTemplate = new RestTemplate();
+    RestTemplate restTemplate(RestTemplateBuilder builder) {
+        RestTemplate restTemplate = builder.build();
+        restTemplate.getMessageConverters().add(0, mappingJackson2HttpMessageConverter());
         restTemplate
                 .setRequestFactory(new BufferingClientHttpRequestFactory(new HttpComponentsClientHttpRequestFactory()));
         restTemplate.getInterceptors().add(new SOSpringClientFilter());
@@ -53,11 +72,11 @@ public class RestTemplateConfig {
 
     @Bean
     @Qualifier(CONFIGURABLE_REST_TEMPLATE)
-    public RestTemplate configurableRestTemplate() {
+    RestTemplate configurableRestTemplate(RestTemplateBuilder builder) {
         final HttpComponentsClientHttpRequestFactory clientHttpRequestFactory =
                 httpComponentsClientConfiguration.httpComponentsClientHttpRequestFactory();
         final RestTemplate restTemplate =
-                new RestTemplate(new BufferingClientHttpRequestFactory(clientHttpRequestFactory));
+                builder.requestFactory(() -> new BufferingClientHttpRequestFactory(clientHttpRequestFactory)).build();
         restTemplate.getInterceptors().add(new SOSpringClientFilter());
         restTemplate.getInterceptors().add((new SpringClientPayloadFilter()));
         return restTemplate;
