21dea00d647fab57ae3de516fc803e0707d1c2c7
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-07-03 09:27:30 +0200

    Use a dependency-injected RestTemplateBuilder to create RestTemplates
    - RestTemplate t = new RestTemplate() breaks tracing
    - use dependency-injected RestTemplateBuilder to create RestTemplate
    
    Issue-ID: SO-4194
    Change-Id: Id7884b28ad6e930d6007bf41b5dbd7017b93dd4f
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

--- common/src/main/java/org/onap/so/security/HttpHeaderForwarderConfig.java
+++ common/src/main/java/org/onap/so/security/HttpHeaderForwarderConfig.java
@@ -5,15 +5,15 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * SPDX-License-Identifier: Apache-2.0
  * ============LICENSE_END=========================================================
  */
@@ -21,6 +21,7 @@ package org.onap.so.security;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -38,8 +39,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Profile("serviceMesh")
 class HttpHeaderForwarderConfig implements WebMvcConfigurer {
     @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        RestTemplate restTemplate = builder.build();
 
         List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
         if (CollectionUtils.isEmpty(interceptors))
