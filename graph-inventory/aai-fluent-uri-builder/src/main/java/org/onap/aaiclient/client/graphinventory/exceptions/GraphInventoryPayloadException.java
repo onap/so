a67bb7a42d9d532a24e6b57fcf5630567e44ed0a
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-07-18 17:37:57 +0200

    Move AAI uri building into dedicated module
    - move uri-building related classes from aai-client module
      into a dedicated aai-fluent-uri-builder module
    - this prepares the introduction of an alternative aai-client
      implementation that is much simpler and will support tracing
    
    Issue-ID: AAI-4189
    Change-Id: I6af926f9f04c50440bfd818aacd5114876fed9bb
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

---
+++ graph-inventory/aai-fluent-uri-builder/src/main/java/org/onap/aaiclient/client/graphinventory/exceptions/GraphInventoryPayloadException.java
@@ -0,0 +1,40 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.aaiclient.client.graphinventory.exceptions;
+
+public class GraphInventoryPayloadException extends Exception {
+
+    private static final long serialVersionUID = -5712783905947711065L;
+
+    public GraphInventoryPayloadException(Throwable t) {
+        super(t);
+    }
+
+    public GraphInventoryPayloadException(String s, Throwable t) {
+        super(s, t);
+    }
+
+    public GraphInventoryPayloadException(String s) {
+        super(s);
+    }
+
+
+}
