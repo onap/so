55ed85f92dee3922b5f3f28328196620d3139526
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-07-18 17:37:57 +0200

    Move AAI uri building into dedicated module
    - move uri-building related classes from aai-client module
      into a dedicated aai-fluent-uri-builder module
    - this prepares the introduction of an alternative aai-client
      implementation that is much simpler and will support tracing
    
    Issue-ID: AAI-4189
    Change-Id: I6af926f9f04c50440bfd818aacd5114876fed9bb
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

--- graph-inventory/aai-client/src/main/java/org/onap/aaiclient/client/aai/entities/Relationships.java
+++ graph-inventory/aai-client/src/main/java/org/onap/aaiclient/client/aai/entities/Relationships.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,7 +28,8 @@ import org.onap.aaiclient.client.aai.entities.uri.AAIFluentTypeReverseLookup;
 import org.onap.aaiclient.client.aai.entities.uri.AAIResourceUri;
 import org.onap.aaiclient.client.aai.entities.uri.AAIUriFactory;
 import org.onap.aaiclient.client.graphinventory.GraphInventoryObjectName;
-import org.onap.aaiclient.client.graphinventory.entities.GraphInventoryRelationships;
+import org.onap.aaiclient.client.graphinventory.GraphInventoryRelationships;
+
 
 public class Relationships extends GraphInventoryRelationships<AAIResultWrapper, AAIResourceUri, AAIObjectType> {
 
@@ -39,7 +40,7 @@ public class Relationships extends GraphInventoryRelationships<AAIResultWrapper,
     @Deprecated
     /**
      * Use getRelatedUris instead
-     * 
+     *
      * @return
      */
     public List<AAIResourceUri> getRelatedAAIUris() {
@@ -49,7 +50,7 @@ public class Relationships extends GraphInventoryRelationships<AAIResultWrapper,
     @Deprecated
     /**
      * Use getRelatedUris instead
-     * 
+     *
      * @return
      */
     public List<AAIResourceUri> getRelatedAAIUris(GraphInventoryObjectName type) {
