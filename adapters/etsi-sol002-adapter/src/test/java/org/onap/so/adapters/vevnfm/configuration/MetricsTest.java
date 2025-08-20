d19e5fc23f5f0a3a6f5432e37705945e7fb8b6f1
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2025-08-20 10:22:57 +0200

    Enable prometheus metrics
    Issue-ID: SO-4231
    Change-Id: I10e841c1ef93ea5253292b33363e5959945a9bb1
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

---
+++ adapters/etsi-sol002-adapter/src/test/java/org/onap/so/adapters/vevnfm/configuration/MetricsTest.java
@@ -0,0 +1,48 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2025 Deutsche Telekom. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.so.adapters.vevnfm.configuration;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class MetricsTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void prometheusEndpointShouldReturnMetrics() throws Exception {
+        mockMvc.perform(get("/actuator/prometheus"))
+               .andExpect(status().isOk())
+               .andExpect(content().string(containsString("# HELP")));
+    }
+}
