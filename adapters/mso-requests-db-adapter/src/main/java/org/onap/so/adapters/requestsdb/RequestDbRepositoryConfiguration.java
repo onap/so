2c9d3c109eef28a4b7aff3bd4ba467f201d0cb98
Fiete Ostkamp <fiete.ostkamp@telekom.de>
2026-02-23 10:10:52 +0100

    Upgrade spring-boot to 2.5
    - upgrade spring boot (2.4.13 -> 2.5.15)
    
    Issue-ID: SO-4198
    Change-Id: Ife8ced8005e746049ee372d32e4cb8580fbf22c2
    Signed-off-by: Fiete Ostkamp <fiete.ostkamp@telekom.de>

--- adapters/mso-requests-db-adapter/src/main/java/org/onap/so/adapters/requestsdb/RequestDbRepositoryConfiguration.java
+++ adapters/mso-requests-db-adapter/src/main/java/org/onap/so/adapters/requestsdb/RequestDbRepositoryConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 public class RequestDbRepositoryConfiguration implements RepositoryRestConfigurer {
@@ -14,7 +15,7 @@ public class RequestDbRepositoryConfiguration implements RepositoryRestConfigure
     private EntityManager entityManager;
 
     @Override
-    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(e -> e.getJavaType())
                 .collect(Collectors.toList()).toArray(new Class[0]));
     }
