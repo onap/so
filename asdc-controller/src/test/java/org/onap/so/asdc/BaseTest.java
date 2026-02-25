c481eec0d2688afaec577e50cea81b25f4c0b78c
Fiete Ostkamp <fiete.ostkamp@telekom.de>
2026-02-25 15:56:45 +0100

    Spring-boot 2.6 upgrade
    Issue-ID: SO-4244
    Change-Id: I1a087b617932ebceea975fa92c3fcbca70f775a0
    Signed-off-by: Fiete Ostkamp <fiete.ostkamp@telekom.de>

--- asdc-controller/src/test/java/org/onap/so/asdc/BaseTest.java
+++ asdc-controller/src/test/java/org/onap/so/asdc/BaseTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,7 +30,7 @@ import org.onap.so.asdc.tenantIsolation.WatchdogDistribution;
 import org.onap.so.spring.SpringContextHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
@@ -44,7 +44,7 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@ContextConfiguration(classes = SpringContextHelper.class, initializers = ConfigFileApplicationContextInitializer.class)
+@ContextConfiguration(classes = SpringContextHelper.class, initializers = ConfigDataApplicationContextInitializer.class)
 @AutoConfigureWireMock(port = 0)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class BaseTest {
