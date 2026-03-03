fc0844824c88c248f0ea457f6687cf9ad478d6d7
Ajaj Ahmad <ajaj.ahmad@techmahindra.com>
2026-03-03 16:20:13 +0530

    Resume/Pause/Abort API logic for Camunda process
    - Implemented logic to restart process from where it stopped.
    - Picked up the previous process info from Camunda history.
    - Implemented logic to Pause/Abort Camunda process.
    - Pause will Stop all process instances currently InProgress state.
    - Paused process can be later Resumed, using the Request ID
    - Abort cancel/terminates all process instances currently InProgress state.
    
    Issue-ID: SO-4171
    Change-ID: I73f97f256a817d403f92f8d905dcd0047b8a0778
    Signed-off-by: Ajaj Ahmad <ajaj.ahmad@techmahindra.com>

---
+++ mso-api-handlers/mso-api-handler-common/src/main/java/org/onap/so/db/camunda/client/CamundaDBClient.java
@@ -0,0 +1,87 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2026 Deutsche Telekom AG.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.so.db.camunda.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+@Component
+public class CamundaDBClient {
+    @Autowired
+    @Qualifier("camundaEntityManagerFactory")
+    private EntityManager entityManager;
+
+    protected static Logger logger = LoggerFactory.getLogger(CamundaDBClient.class);
+
+    // Correct query: Finds the latest started BB that has no corresponding End_
+    String sqlQuery = "SELECT start.ACT_ID_ AS resume_from " + "FROM ACT_HI_ACTINST start "
+            + "LEFT JOIN ACT_HI_ACTINST end ON end.ACT_ID_ = REPLACE(start.ACT_ID_, 'Start_', 'End_') "
+            + "AND end.PROC_INST_ID_ = start.PROC_INST_ID_ " + "WHERE start.ACT_ID_ LIKE 'Start_%BB' "
+            + "AND start.ACT_ID_ != 'Start_WorkflowActionBB' " + "AND start.PROC_INST_ID_ IN ( "
+            + "  SELECT PROC_INST_ID_ FROM ACT_HI_PROCINST WHERE BUSINESS_KEY_ = ?1" + ") " + "AND end.ACT_ID_ IS NULL "
+            + "ORDER BY start.START_TIME_ ASC LIMIT 1";
+
+    String sql = "select PROC_INST_ID_ from ACT_HI_PROCINST where BUSINESS_KEY_ =?1 AND STATE_='SUSPENDED'";
+
+    public String findResumeFromBB(String requestId) {
+        String bbName = null;
+        try {
+            logger.trace("****** In Try Block to fetch resumeFrom ****** CamundaDBClient");
+
+            Query query = entityManager.createNativeQuery(sqlQuery);
+            String resumeFrom = query.setParameter(1, requestId).getSingleResult().toString();
+            logger.info("****** Found resumeFrom Building block in Camunda DB: {} ******", resumeFrom);
+
+            // Clean prefix to get only BB name
+            bbName = stripStartPrefix(resumeFrom);
+        } catch (Exception e) {
+            logger.error("****** Error querying resumeFrom from Camunda DB: {} ******", e.getMessage());
+        }
+        return bbName;
+    }
+
+    public String findProcessInstanceId(String requestId) {
+        String processInstanceId = null;
+        try {
+            logger.trace("****** In Try Block to fetch processInstanceId ****** CamundaDBClient");
+
+            Query query = entityManager.createNativeQuery(sql);
+            processInstanceId = query.setParameter(1, requestId).getSingleResult().toString();
+            logger.info("****** Found processInstanceId in Camunda DB: {} ******", processInstanceId);
+        } catch (Exception e) {
+            logger.error("****** Error querying from Camunda DB: {} ******", e.getMessage());
+        }
+        return processInstanceId;
+    }
+
+    protected String stripStartPrefix(String resumeFrom) {
+        String bbName = "";
+        if (resumeFrom != null && resumeFrom.startsWith("Start_")) {
+            bbName = resumeFrom.substring(6);
+            logger.info("****** Cleaned resumeFrom Building block: {} ******", bbName);
+        }
+        return bbName;
+    }
+}
---
+++ mso-api-handlers/mso-api-handler-infra/src/main/java/org/onap/so/apihandlerinfra/InfraActiveRequestsService.java
@@ -0,0 +1,43 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP - SO
+ * ================================================================================
+ * Copyright (C) 2026 Deutsche Telekom AG.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.so.apihandlerinfra;
+
+import lombok.RequiredArgsConstructor;
+import org.onap.so.db.request.beans.InfraActiveRequests;
+import org.onap.so.db.request.data.repository.InfraActiveRequestsRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class InfraActiveRequestsService {
+    private static final Logger logger = LoggerFactory.getLogger(InfraActiveRequestsService.class);
+
+    @Autowired
+    private final InfraActiveRequestsRepository infraActiveRequestsRepository;
+
+    protected List<InfraActiveRequests> getListOfRequestsInProgress() {
+        logger.info("InfraActiveRequestsService is returning list of in progress request:");
+        return infraActiveRequestsRepository.getListOfRequestsInProgress();
+    }
+}
