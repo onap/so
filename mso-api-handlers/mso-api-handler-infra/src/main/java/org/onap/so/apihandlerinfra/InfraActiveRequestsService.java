bd5632135dd1f75dd7a98bc2ddbd904e61e72da7
Ajaj Ahmad <ajaj.ahmad@techmahindra.com>
2026-03-03 16:20:13 +0530

    Resume/Pause/Abort API logic for Camunda process
    - Implemented logic to restart process from where it stopped.
    - Picked up the previous process info from Camunda history.
    - Implemented logic to Pause/Abort Camunda process.
    - Pause will Stop all process instances currently InProgress state.
    - Paused process can be later Resumed, using the Request ID
    - Abort cancel/terminates all process instances currently InProgress state.
    
    Issue-ID: SO-4171
    Change-ID: I73f97f256a817d403f92f8d905dcd0047b8a0778
    Signed-off-by: Ajaj Ahmad <ajaj.ahmad@techmahindra.com>

--- mso-api-handlers/mso-api-handler-infra/src/main/java/org/onap/so/apihandlerinfra/InfraActiveRequestsService.java
+++ mso-api-handlers/mso-api-handler-infra/src/main/java/org/onap/so/apihandlerinfra/InfraActiveRequestsService.java
@@ -2,14 +2,14 @@
  * ============LICENSE_START=======================================================
  * ONAP - SO
  * ================================================================================
- * Copyright (C) 2017 AT&T Intellectual Property. All rights reserved.
+ * Copyright (C) 2026 Deutsche Telekom AG.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,26 +17,24 @@
  * limitations under the License.
  * ============LICENSE_END=========================================================
  */
+package org.onap.so.apihandlerinfra;
 
-package org.onap.so.constants;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.onap.so.db.request.beans.InfraActiveRequests;
+import org.onap.so.db.request.data.repository.InfraActiveRequestsRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class InfraActiveRequestsService {
 
-/*
- * Enum for Status values returned by API Handler to Tail-F
- */
-public enum Status {
-    PENDING,
-    IN_PROGRESS,
-    WAIT_COMPLETION_NOTIF,
-    COMPLETE,
-    COMPLETED,
-    FAILED,
-    TIMEOUT,
-    UNLOCKED,
-    PENDING_MANUAL_TASK,
-    ABORTED,
-    ROLLED_BACK,
-    ROLLED_BACK_TO_ASSIGNED,
-    ROLLED_BACK_TO_CREATED,
-    PAUSED
+    private final InfraActiveRequestsRepository infraActiveRequestsRepository;
+
+    protected List<InfraActiveRequests> getListOfRequestsInProgress() {
+        log.debug("InfraActiveRequestsService is returning list of in progress request:");
+        return infraActiveRequestsRepository.getListOfRequestsInProgress();
+    }
 }
