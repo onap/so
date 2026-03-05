b2098fc5f6e58c5c7444d70e09c27ba9e9783a9a
Fiete Ostkamp <fiete.ostkamp@telekom.de>
2026-03-05 08:19:45 +0100

    Spring-boot 2.6 upgrade
    Issue-ID: SO-4244
    Change-Id: I0449f128f1ac80823a456667bce21d4c70e17bf9
    Signed-off-by: Fiete Ostkamp <fiete.ostkamp@telekom.de>

--- adapters/mso-adapter-utils/src/main/java/org/onap/so/openstack/utils/MsoTenantUtilsFactory.java
+++ adapters/mso-adapter-utils/src/main/java/org/onap/so/openstack/utils/MsoTenantUtilsFactory.java
@@ -9,9 +9,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,6 +29,7 @@ import org.onap.so.openstack.exceptions.MsoCloudSiteNotFound;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -37,8 +38,10 @@ public class MsoTenantUtilsFactory {
     protected static Logger logger = LoggerFactory.getLogger(MsoTenantUtilsFactory.class);
     @Autowired
     protected CloudConfig cloudConfig;
+    @Lazy
     @Autowired
     protected MsoKeystoneUtils keystoneUtils;
+    @Lazy
     @Autowired
     protected MsoKeystoneV3Utils keystoneV3Utils;
 
--- mso-api-handlers/mso-api-handler-infra/src/test/java/org/onap/so/apihandlerinfra/GlobalHealthcheckHandlerTest.java
+++ mso-api-handlers/mso-api-handler-infra/src/test/java/org/onap/so/apihandlerinfra/GlobalHealthcheckHandlerTest.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,7 +35,7 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.onap.so.apihandlerinfra.HealthCheckConfig.Endpoint;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.HttpEntity;
@@ -52,7 +52,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {GenericStringConverter.class, HealthCheckConverter.class},
-        initializers = {ConfigFileApplicationContextInitializer.class})
+        initializers = {ConfigDataApplicationContextInitializer.class})
 @ActiveProfiles("test")
 @EnableConfigurationProperties({HealthCheckConfig.class})
 public class GlobalHealthcheckHandlerTest {
